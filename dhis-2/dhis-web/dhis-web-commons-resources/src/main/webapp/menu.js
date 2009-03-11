1c633705246cc9fd82283eb6479a0bd99f9070a7
Saptarshi <sunbiz@gmail.com>
2009-03-11 11:45:48 +0530

    revno: 40 fixes bug: https://launchpad.net/bugs/339807 message:   Removed speed from the menu appearance and got to default. Fixes crashes in Webkit based browsers like Chrome, Safari, Konqueror
    committer: Saptarshi <sunbiz@gmail.com>
    branch nick: trunk
    timestamp: Wed 2009-03-11 11:45:48 +0530

--- dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/menu.js
+++ dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/menu.js
@@ -1,44 +1,34 @@
-
 var menuTimeout = 500;
 var closeTimer = null;
 var dropDownId = null;
 
-function showDropDown( id )
-{
-  cancelHideDropDownTimeout();
+function showDropDown( id ){
+    cancelHideDropDownTimeout();
+    var newDropDownId = "#" + id;
   
-  var newDropDownId = "#" + id;
-  
-  if ( dropDownId != newDropDownId )
-  {
-    if ( dropDownId )
+    if ( dropDownId != newDropDownId )
     {
-      hideDropDown();
+        if ( dropDownId ){
+            hideDropDown();
+        }
+
+        dropDownId = newDropDownId;
+        $( dropDownId ).show();
     }
-
-    dropDownId = newDropDownId;
-  
-    $( dropDownId ).show( "fast" );
-  }
 }
 
-function hideDropDown()
-{
-  $( dropDownId ).hide( "fast" );
-  
-  dropDownId = null;
+function hideDropDown(){
+    $( dropDownId ).hide();
+    dropDownId = null;
 }
 
-function hideDropDownTimeout()
-{
-  closeTimer = window.setTimeout( hideDropDown, menuTimeout );
+function hideDropDownTimeout(){
+    closeTimer = window.setTimeout( hideDropDown, menuTimeout );
 }
 
-function cancelHideDropDownTimeout()
-{
-  if ( closeTimer )
-  {
-    window.clearTimeout( closeTimer );
-    closeTimer = null;
-  }
+function cancelHideDropDownTimeout(){
+    if ( closeTimer ){
+        window.clearTimeout( closeTimer );
+        closeTimer = null;
+    }
 }
