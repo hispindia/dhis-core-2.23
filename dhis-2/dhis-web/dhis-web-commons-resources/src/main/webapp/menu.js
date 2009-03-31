3eab409c5f444f8bdeeb5faa7fd4341324d9e19a
Lars Helge Oeverland <larshelge@gmail.com>
2009-03-31 12:41:50 +0200

    revno: 134 message:   dhis-web-commons-resources: Enabled the animated menu again. This was removed in order to make it work in webkit enabled browsers, but this is not a good solution as these effects are used throughout the system. At least we know the source of the problem and can alert jquery about it and wait for a fix
    committer: Lars Helge Oeverland <larshelge@gmail.com>
    branch nick: trunk
    timestamp: Tue 2009-03-31 12:41:50 +0200

--- dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/menu.js
+++ dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/menu.js
@@ -2,33 +2,43 @@ var menuTimeout = 500;
 var closeTimer = null;
 var dropDownId = null;
 
-function showDropDown( id ){
+function showDropDown( id )
+{
     cancelHideDropDownTimeout();
+    
     var newDropDownId = "#" + id;
   
     if ( dropDownId != newDropDownId )
     {
-        if ( dropDownId ){
+        if ( dropDownId )
+        {
             hideDropDown();
         }
 
         dropDownId = newDropDownId;
-        $( dropDownId ).show();
+        
+        $( dropDownId ).show( 'fast' );
     }
 }
 
-function hideDropDown(){
-    $( dropDownId ).hide();
+function hideDropDown()
+{
+    $( dropDownId ).hide( 'fast' );
+    
     dropDownId = null;
 }
 
-function hideDropDownTimeout(){
+function hideDropDownTimeout()
+{
     closeTimer = window.setTimeout( hideDropDown, menuTimeout );
 }
 
-function cancelHideDropDownTimeout(){
-    if ( closeTimer ){
+function cancelHideDropDownTimeout()
+{
+    if ( closeTimer )
+    {
         window.clearTimeout( closeTimer );
+        
         closeTimer = null;
     }
 }
