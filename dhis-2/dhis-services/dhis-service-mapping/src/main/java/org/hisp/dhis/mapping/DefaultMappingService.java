cb4117d534edeb87f164e96c6a76baed68373258
Jan Henrik Overland <janhenrik.overland@gmail.com>
2009-04-16 16:10:22 +0200

    revno: 182 message:   Moving methods from map adapter to map service.
    committer: Jan Henrik Overland <janhenrik.overland@gmail.com>
    branch nick: trunk
    timestamp: Thu 2009-04-16 16:10:22 +0200

--- dhis-2/dhis-services/dhis-service-mapping/src/main/java/org/hisp/dhis/mapping/DefaultMappingService.java
+++ dhis-2/dhis-services/dhis-service-mapping/src/main/java/org/hisp/dhis/mapping/DefaultMappingService.java
@@ -29,6 +29,7 @@ package org.hisp.dhis.mapping;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Set;
 
 import org.hisp.dhis.organisationunit.OrganisationUnit;
 import org.hisp.dhis.organisationunit.OrganisationUnitLevel;
@@ -46,6 +47,13 @@ public class DefaultMappingService
     // Dependencies
     // -------------------------------------------------------------------------
 
+    private MappingService mappingService;
+
+    public void setMappingService( MappingService mappingService )
+    {
+        this.mappingService = mappingService;
+    }
+    
     private MappingStore mappingStore;
 
     public void setMappingStore( MappingStore mappingStore )
@@ -103,6 +111,62 @@ public class DefaultMappingService
         return mappingStore.getMapsAtLevel( organisationUnitLevel );
     }
     
+    public int addMap( String mapLayerPath, int organisationUnitId, int organisationUnitLevelId, String uniqueColumn,
+        String nameColumn, String longitude, String latitude, int zoom )
+    {
+        OrganisationUnit organisationUnit = organisationUnitService.getOrganisationUnit( organisationUnitId );
+
+        OrganisationUnitLevel organisationUnitLevel = organisationUnitService
+            .getOrganisationUnitLevel( organisationUnitLevelId );
+
+        Set<String> staticMapLayerPaths = null;
+
+        Map map = new Map( mapLayerPath, organisationUnit, organisationUnitLevel, uniqueColumn, nameColumn, longitude,
+            latitude, zoom, staticMapLayerPaths );
+
+        return mappingService.addMap( map );
+    }
+
+    public void addOrUpdateMap( String mapLayerPath, int organisationUnitId, int organisationUnitLevelId,
+        String uniqueColumn, String nameColumn, String longitude, String latitude, int zoom )
+    {
+        Map map = mappingService.getMapByMapLayerPath( mapLayerPath );
+
+        OrganisationUnit organisationUnit = organisationUnitService.getOrganisationUnit( organisationUnitId );
+
+        OrganisationUnitLevel organisationUnitLevel = organisationUnitService
+            .getOrganisationUnitLevel( organisationUnitLevelId );
+
+        Set<String> staticMapLayerPaths = null;
+
+        if ( map != null )
+        {
+            map.setOrganisationUnit( organisationUnit );
+            map.setOrganisationUnitLevel( organisationUnitLevel );
+            map.setUniqueColumn( uniqueColumn );
+            map.setNameColumn( nameColumn );
+            map.setLongitude( longitude );
+            map.setLatitude( latitude );
+            map.setZoom( zoom );
+
+            mappingService.updateMap( map );
+        }
+        else
+        {
+            map = new Map( mapLayerPath, organisationUnit, organisationUnitLevel, uniqueColumn, nameColumn, longitude,
+                latitude, zoom, staticMapLayerPaths );
+
+            mappingService.addMap( map );
+        }
+    }
+    
+    public void deleteMapByMapLayerPath( String mapLayerPath )
+    {
+        Map map = mappingService.getMapByMapLayerPath( mapLayerPath );
+        
+        mappingService.deleteMap( map );
+    }
+    
     // -------------------------------------------------------------------------
     // MapOrganisationUnitRelation
     // -------------------------------------------------------------------------
@@ -156,5 +220,47 @@ public class DefaultMappingService
         }
         
         return relations;
+    }
+    
+    public int addMapOrganisationUnitRelation( String mapLayerPath, int organisationUnitId, String featureId )
+    {
+        Map map = mappingService.getMapByMapLayerPath( mapLayerPath );
+
+        OrganisationUnit organisationUnit = organisationUnitService.getOrganisationUnit( organisationUnitId );
+
+        MapOrganisationUnitRelation mapOrganisationUnitRelation = new MapOrganisationUnitRelation( map,
+            organisationUnit, featureId );
+
+        return mappingService.addMapOrganisationUnitRelation( mapOrganisationUnitRelation );
+    }
+
+    public void addOrUpdateMapOrganisationUnitRelation( String mapLayerPath, int organisationUnitId, String featureId )
+    {
+        Map map = mappingService.getMapByMapLayerPath( mapLayerPath );
+
+        OrganisationUnit organisationUnit = organisationUnitService.getOrganisationUnit( organisationUnitId );
+
+        MapOrganisationUnitRelation mapOrganisationUnitRelation = mappingService.getMapOrganisationUnitRelation( map,
+            organisationUnit );
+
+        if ( mapOrganisationUnitRelation != null )
+        {
+            mapOrganisationUnitRelation.setFeatureId( featureId );
+
+            mappingService.updateMapOrganisationUnitRelation( mapOrganisationUnitRelation );
+        }
+        else
+        {
+            mapOrganisationUnitRelation = new MapOrganisationUnitRelation( map, organisationUnit, featureId );
+
+            mappingService.addMapOrganisationUnitRelation( mapOrganisationUnitRelation );
+        }
+    }
+
+    public Collection<MapOrganisationUnitRelation> getAvailableMapOrganisationUnitRelations( String mapLayerPath )
+    {
+        Map map = mappingService.getMapByMapLayerPath( mapLayerPath );
+
+        return mappingService.getAvailableMapOrganisationUnitRelations( map );
     }
 }
