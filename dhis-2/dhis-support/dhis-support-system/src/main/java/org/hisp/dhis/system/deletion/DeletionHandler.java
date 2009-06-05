301a573525163f67acaf90e859ce4cd8e4788a9f
Lars Helge Oeverland <larshelge@gmail.com>
2009-06-05 22:49:17 +0200

    revno: 358 message:   Minor fix
    committer: Lars Helge Oeverland <larshelge@gmail.com>
    branch nick: trunk
    timestamp: Fri 2009-06-05 22:49:17 +0200

--- dhis-2/dhis-support/dhis-support-system/src/main/java/org/hisp/dhis/system/deletion/DeletionHandler.java
+++ dhis-2/dhis-support/dhis-support-system/src/main/java/org/hisp/dhis/system/deletion/DeletionHandler.java
@@ -51,6 +51,7 @@ import org.hisp.dhis.indicator.IndicatorGroup;
 import org.hisp.dhis.indicator.IndicatorType;
 import org.hisp.dhis.mapping.Map;
 import org.hisp.dhis.mapping.MapLegendSet;
+import org.hisp.dhis.mapping.MapOrganisationUnitRelation;
 import org.hisp.dhis.mapping.MapView;
 import org.hisp.dhis.minmax.MinMaxDataElement;
 import org.hisp.dhis.olap.OlapURL;
@@ -440,6 +441,15 @@ public abstract class DeletionHandler
     }
     
     public boolean allowDeleteMapLegendSet( MapLegendSet mapLegendSet )
+    {
+        return true;
+    }
+    
+    public void deleteMapOrganisationUnitRelation( MapOrganisationUnitRelation mapOrganisationUnitRelation )
+    {   
+    }
+    
+    public boolean allowDeleteMapOrganisationUnitRelation( MapOrganisationUnitRelation mapOrganisationUnitRelation )
     {
         return true;
     }
