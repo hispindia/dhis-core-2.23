c3970ba168d624df69068e6a37c2d53e19ff5087
Jan Henrik Overland <janhenrik.overland@gmail.com>
2009-08-01 00:22:28 +0700

    revno: 469 message:   Checkbox behaviour fixed.
    committer: Jan Henrik Overland <janhenrik.overland@gmail.com>
    branch nick: trunk
    timestamp: Sat 2009-08-01 00:22:28 +0700

--- gis/dhis-web-mapping/src/main/webapp/mapping/geostat/geostat.js
+++ gis/dhis-web-mapping/src/main/webapp/mapping/geostat/geostat.js
@@ -39,6 +39,107 @@ Ext.onReady(function()
         onShow: Ext.form.TriggerField.superclass.onShow,
         onHide: Ext.form.TriggerField.superclass.onHide
     });
+    
+    Ext.override(Ext.form.Checkbox, {
+	onRender: function(ct, position){
+		Ext.form.Checkbox.superclass.onRender.call(this, ct, position);
+		if(this.inputValue !== undefined){
+			this.el.dom.value = this.inputValue;
+		}
+		//this.el.addClass('x-hidden');
+		this.innerWrap = this.el.wrap({
+			//tabIndex: this.tabIndex,
+			cls: this.baseCls+'-wrap-inner'
+		});
+		this.wrap = this.innerWrap.wrap({cls: this.baseCls+'-wrap'});
+		this.imageEl = this.innerWrap.createChild({
+			tag: 'img',
+			src: Ext.BLANK_IMAGE_URL,
+			cls: this.baseCls
+		});
+		if(this.boxLabel){
+			this.labelEl = this.innerWrap.createChild({
+				tag: 'label',
+				htmlFor: this.el.id,
+				cls: 'x-form-cb-label',
+				html: this.boxLabel
+			});
+		}
+		//this.imageEl = this.innerWrap.createChild({
+			//tag: 'img',
+			//src: Ext.BLANK_IMAGE_URL,
+			//cls: this.baseCls
+		//}, this.el);
+		if(this.checked){
+			this.setValue(true);
+		}else{
+			this.checked = this.el.dom.checked;
+		}
+		this.originalValue = this.checked;
+	},
+	afterRender: function(){
+		Ext.form.Checkbox.superclass.afterRender.call(this);
+		//this.wrap[this.checked ? 'addClass' : 'removeClass'](this.checkedCls);
+		this.imageEl[this.checked ? 'addClass' : 'removeClass'](this.checkedCls);
+	},
+	initCheckEvents: function(){
+		//this.innerWrap.removeAllListeners();
+		this.innerWrap.addClassOnOver(this.overCls);
+		this.innerWrap.addClassOnClick(this.mouseDownCls);
+		this.innerWrap.on('click', this.onClick, this);
+		//this.innerWrap.on('keyup', this.onKeyUp, this);
+	},
+	onFocus: function(e) {
+		Ext.form.Checkbox.superclass.onFocus.call(this, e);
+		//this.el.addClass(this.focusCls);
+		this.innerWrap.addClass(this.focusCls);
+	},
+	onBlur: function(e) {
+		Ext.form.Checkbox.superclass.onBlur.call(this, e);
+		//this.el.removeClass(this.focusCls);
+		this.innerWrap.removeClass(this.focusCls);
+	},
+	onClick: function(e){
+		if (e.getTarget().htmlFor != this.el.dom.id) {
+			if (e.getTarget() !== this.el.dom) {
+				this.el.focus();
+			}
+			if (!this.disabled && !this.readOnly) {
+				this.toggleValue();
+			}
+		}
+		//e.stopEvent();
+	},
+	onEnable: Ext.form.Checkbox.superclass.onEnable,
+	onDisable: Ext.form.Checkbox.superclass.onDisable,
+	onKeyUp: undefined,
+	setValue: function(v) {
+		var checked = this.checked;
+		this.checked = (v === true || v === 'true' || v == '1' || String(v).toLowerCase() == 'on');
+		if(this.rendered){
+			this.el.dom.checked = this.checked;
+			this.el.dom.defaultChecked = this.checked;
+			//this.wrap[this.checked ? 'addClass' : 'removeClass'](this.checkedCls);
+			this.imageEl[this.checked ? 'addClass' : 'removeClass'](this.checkedCls);
+		}
+		if(checked != this.checked){
+			this.fireEvent("check", this, this.checked);
+			if(this.handler){
+				this.handler.call(this.scope || this, this, this.checked);
+			}
+		}
+	},
+	getResizeEl: function() {
+		//if(!this.resizeEl){
+			//this.resizeEl = Ext.isSafari ? this.wrap : (this.wrap.up('.x-form-element', 5) || this.wrap);
+		//}
+		//return this.resizeEl;
+		return this.wrap;
+	}
+    });
+    Ext.override(Ext.form.Radio, {
+        checkedCls: 'x-form-radio-checked'
+    });
 
     myMap: null;
     map = new OpenLayers.Map($('olmap'));
@@ -1366,23 +1467,50 @@ Ext.onReady(function()
                             
                             Ext.Ajax.request(
                             {
-                                url: path + 'setMapSourceTypeUserSetting' + type,
+                                url: path + 'getMapSourceTypeUserSetting' + type,
                                 method: 'POST',
-                                params: { mapSourceType: msv },
 
                                 success: function( responseObject )
                                 {
-                                    Ext.messageBlack.msg('Map source', msg_highlight_start + msrw + msg_highlight_end + ' was saved as map source.');
+                                    if (Ext.util.JSON.decode(responseObject.responseText).mapSource == msv)
+                                    {
+                                        Ext.messageRed.msg('Map source', msg_highlight_start + msrw + msg_highlight_end + ' is already selected.');
+                                    }
+                                    else
+                                    {
+                                        Ext.Ajax.request(
+                                        {
+                                            url: path + 'setMapSourceTypeUserSetting' + type,
+                                            method: 'POST',
+                                            params: { mapSourceType: msv },
 
-                                    MAPSOURCE = msv;
-                                    Ext.getCmp('map_cb').getStore().reload();
-                                    Ext.getCmp('maps_cb').getStore().reload();
+                                            success: function( responseObject )
+                                            {
+                                                Ext.messageBlack.msg('Map source', msg_highlight_start + msrw + msg_highlight_end + ' is saved as map source.');
+
+                                                MAPSOURCE = msv;
+                                                
+                                                Ext.getCmp('map_cb').getStore().reload();
+                                                Ext.getCmp('maps_cb').getStore().reload();
+                                                Ext.getCmp('mapview_cb').getStore().reload();
+
+                                                Ext.getCmp('map_cb').reset();
+                                                Ext.getCmp('mapview_cb').reset();
+                                            },
+                                            failure: function()
+                                            {
+                                                alert( 'Status', 'Error while saving data' );
+                                            }
+                                        });
+                                    }
                                 },
                                 failure: function()
                                 {
                                     alert( 'Status', 'Error while saving data' );
                                 }
                             });
+                            
+                            
                         }
                     }
                 }
