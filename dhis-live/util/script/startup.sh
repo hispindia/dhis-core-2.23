c128fc3297a7954caafbd969b2b5ca33e5f92b09
Lars Helge Oeverland <larshelge@gmail.com>
2009-09-11 21:14:49 +0200

    revno: 684 message:   Removed check for JAVA_HOME in startup.sh in dhis-live.
    committer: Lars Helge Oeverland <larshelge@gmail.com>
    branch nick: trunk
    timestamp: Fri 2009-09-11 21:14:49 +0200

--- dhis-live/util/script/startup.sh
+++ dhis-live/util/script/startup.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
-#if [ "${JAVA_HOME:+1}" = "1" ]
-if true
-then
-  echo "Starting DHIS 2..."
-  java -jar dhis2-lite.jar
-else
-  echo "DHIS 2 requires a Java Runtime Environment to be installed"
-fi
+echo "Starting DHIS 2..."
+java -jar dhis2-lite.jar
