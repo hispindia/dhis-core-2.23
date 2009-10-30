85049a81e349f740de70da12c59526ffee7c97b7
Bob Jolliffe <bobj@bobj-laptop>
2009-10-30 18:20:30 +0000

    revno: 942 message:   First steps at dxf2 parser.  Rough outline but compiles ...   The v2objects shouldnt be under version control as they are auto-generated
    committer: Bob Jolliffe <bobj@bobj-laptop>
    branch nick: trunk
    timestamp: Fri 2009-10-30 18:20:30 +0000

--- dhis-2/dhis-services/dhis-service-importexport/src/main/java/org/hisp/dhis/importexport/dxf/importer/DefaultDXFImportService.java
+++ dhis-2/dhis-services/dhis-service-importexport/src/main/java/org/hisp/dhis/importexport/dxf/importer/DefaultDXFImportService.java
@@ -158,6 +158,7 @@ import org.hisp.dhis.reporttable.ReportTableService;
 import org.hisp.dhis.source.Source;
 import org.hisp.dhis.system.util.StreamUtils;
 import org.hisp.dhis.validation.ValidationRuleService;
+import org.hisp.dhis.importexport.dxf.importer.V2Parser;
 
 /**
  * @author Lars Helge Overland
@@ -349,20 +350,29 @@ public class DefaultDXFImportService
 
         // move to root element
         reader.next();
-
+	
         if ( reader.isStartElement( ROOT_NAME ) )
-        {
-            if ( reader.getXmlStreamReader().getNamespaceURI() == DXF2_NAMESPACE_URI )
-            {
-                dxfVersion = 2;
-                log.info( "parsing dxf version " + dxfVersion );
-            }
-            else
-            {
-                dxfVersion = 1;
-                parseDXFv1( params, reader );
-            }
-        }
+	    {
+		if (reader.getXmlStreamReader().getNamespaceURI() ==  DXF2_NAMESPACE_URI)
+		    {
+			dxfVersion = 2;
+			log.info("parsing dxf version "+dxfVersion);
+			try {
+			    V2Parser v2parser = new V2Parser();
+			    
+			    v2parser.getMetadata(reader.getXmlStreamReader());
+			    v2parser.getDataValues(reader.getXmlStreamReader());
+			} catch (javax.xml.bind.JAXBException ex) {
+			    log.info("Parsing error: " + ex);
+			    // report something to GUI ....
+			}
+		    }
+		else
+		    {
+			dxfVersion = 1;
+			parseDXFv1(params, reader);
+		    }
+	    }
 
         // setMessage( "import_process_done" );
 
