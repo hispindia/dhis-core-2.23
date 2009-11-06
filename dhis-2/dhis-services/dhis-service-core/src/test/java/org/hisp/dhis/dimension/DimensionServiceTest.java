3dced7e8c14683a0eb579459938b34374f7a4c72
Lars Helge Oeverland <larshelge@gmail.com>
2009-11-06 13:10:19 +0100

    revno: 990 message:   Quickly fixed.
    committer: Lars Helge Oeverland <larshelge@gmail.com>
    branch nick: trunk
    timestamp: Fri 2009-11-06 13:10:19 +0100

--- dhis-2/dhis-services/dhis-service-core/src/test/java/org/hisp/dhis/dimension/DimensionServiceTest.java
+++ dhis-2/dhis-services/dhis-service-core/src/test/java/org/hisp/dhis/dimension/DimensionServiceTest.java
@@ -27,15 +27,16 @@ package org.hisp.dhis.dimension;
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
+
 import java.util.Collection;
 
 import org.hisp.dhis.DhisSpringTest;
-import org.hisp.dhis.dimension.Dimension;
+import org.hisp.dhis.dataelement.DataElement;
 import org.hisp.dhis.dataelement.DataElementGroupSet;
 import org.hisp.dhis.dataelement.DataElementService;
 import org.junit.Test;
-
-import static junit.framework.Assert.*;
 
 /**
  * @author Lars Helge Overland
@@ -49,6 +50,8 @@ public class DimensionServiceTest
     private DataElementGroupSet dataElementGroupSetA;
     private DataElementGroupSet dataElementGroupSetB;
     
+    private DataElement dataElementA;
+    
     @Override
     public void setUpTest()
     {
@@ -60,17 +63,22 @@ public class DimensionServiceTest
         dataElementGroupSetB = new DataElementGroupSet( "DataElementGroupSetB" );
         
         dataElementService.addDataElementGroupSet( dataElementGroupSetA );
-        dataElementService.addDataElementGroupSet( dataElementGroupSetB );        
+        dataElementService.addDataElementGroupSet( dataElementGroupSetB );
+        
+        dataElementA = createDataElement( 'A' );
+        
+        dataElementA.getGroupSets().add( dataElementGroupSetA );
+        dataElementA.getGroupSets().add( dataElementGroupSetA );
+        
+        dataElementService.addDataElement( dataElementA );
     }
     
     @Test
     public void getDimensions()
     {
-        Collection<Dimension> dimensions = dimensionService.getAllDimensions();
+        Collection<DimensionSet> dimensionSets = dimensionService.getAllDimensionSets();
         
-        assertNotNull( dimensions );
-        assertEquals( 3, dimensions.size() ); // Including default
-        assertTrue( dimensions.contains( dataElementGroupSetA ) );
-        assertTrue( dimensions.contains( dataElementGroupSetB ) );
+        assertNotNull( dimensionSets );
+        assertTrue( dimensionSets.contains( dataElementA ) );
     }
 }
