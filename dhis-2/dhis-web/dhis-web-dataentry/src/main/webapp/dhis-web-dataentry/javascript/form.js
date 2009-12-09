bc4895bdf798a3c24748822c22797cde73129f4e
Lars Helge Oeverland <larshelge@gmail.com>
2009-12-09 13:01:02 +0100

    revno: 1191 message:   Merged out/reverted revision 1184
    committer: Lars Helge Oeverland <larshelge@gmail.com>
    branch nick: trunk
    timestamp: Wed 2009-12-09 13:01:02 +0100

--- dhis-2/dhis-web/dhis-web-dataentry/src/main/webapp/dhis-web-dataentry/javascript/form.js
+++ dhis-2/dhis-web/dhis-web-dataentry/src/main/webapp/dhis-web-dataentry/javascript/form.js
@@ -303,7 +303,6 @@ function registerReceived( messageElement )
 	document.getElementById( "undoButton" ).disabled = false;
     document.getElementById( "dateField" ).disabled = true;
     document.getElementById( "dateDiv" ).style.display = "none";
-	disableInputField();
 }
 
 function undoCompleteDataSet()
@@ -326,25 +325,4 @@ function undoReceived( messageElement )
     document.getElementById( "undoButton" ).disabled = true;
     document.getElementById( "dateField" ).disabled = false;
     document.getElementById( "dateDiv" ).style.display = "inline";
-	enableInputField();
-}
-
-function disableInputField()
-{
-	var inputList = byId("dataEntryForm").getElementsByTagName("input");
-	
-	for(var i=0;i<inputList.length;i++){
-		var input = inputList.item(i);
-		input.disabled = true;
-	}		
-}
-
-function enableInputField()
-{
-	var inputList = byId("dataEntryForm").getElementsByTagName("input");
-	
-	for(var i=0;i<inputList.length;i++){
-		var input = inputList.item(i);
-		input.disabled = false;
-	}	
 }
