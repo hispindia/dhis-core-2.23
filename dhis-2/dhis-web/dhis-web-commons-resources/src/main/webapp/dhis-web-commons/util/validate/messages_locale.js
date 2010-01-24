84c36acc858dd65aa01d5feb065b2cfcbf5ac57e
Viet Nguyen <phamquocviet@gmail.com>
2010-01-25 05:22:09 +0530

    revno: 1329 message:   Apply jquery validation plugin for page add user.
    committer: Viet Nguyen <phamquocviet@gmail.com>
    branch nick: trunk
    timestamp: Mon 2010-01-25 05:22:09 +0530

---
+++ dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/util/validate/messages_locale.js
@@ -0,0 +1,34 @@
+/*
+ * Translated default messages for the jQuery validation plugin.
+ * 
+ */
+en_GB = {
+		 required: "This field is required."
+		,remote: "Please fix this field."
+		,email: "Please enter a valid email address."
+		,url: "Please enter a valid URL."
+		,date: "Please enter a valid date."
+		,dateISO: "Please enter a valid date (like 1990-01-01)."
+		,number: "Please enter a valid number."
+		,digits: "Please enter only digits"
+		,creditcard: "Please enter a valid credit card number."
+		,equalTo: "Mismatch: Please enter the equal values again."
+		,accept: "Please enter a value with a valid extension."
+		,firstletteralphabet: "The first character must be alphabetical."
+		,maxlength: $.validator.format("Please enter no more than {0} characters.")
+		,minlength: $.validator.format("Please enter at least {0} characters.")
+		,rangelength: $.validator.format("Please enter a value between {0} and {1} characters long.")
+		,range: $.validator.format("Please enter a value between {0} and {1}.")
+		,max: $.validator.format("Please enter a value less than or equal to {0}.")
+		,min: $.validator.format("Please enter a value greater than or equal to {0}.")
+		,notequalto: "Please enter a different value to above."
+        ,decimals: "Digits/Decimal only please"
+		,alphanumeric: "Please Letters, numbers, spaces and underscores only"
+        ,alphanumericwithbasicpuncspaces: "Please Letters, numbers, spaces or some special chars \n like .,-,(,) only, Check the value."
+        ,letterswithbasicspecialchars: "Please Letters, numbers, spaces or some special chars \n like .,-,%,(,) only, Check the value."
+        ,lessthanequal: $.validator.format("Please enter a value less than or equal to {1}")
+        ,greaterthanequal: $.validator.format("Please enter a value greater than or equal to {1}")
+        ,unique: "Please do not enter same values"
+        ,letterswithbasicpunc: "Letters or punctuation only please"
+};
+
