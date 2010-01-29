22d659837b6eb0c07b5e5f81b88ddac005f7546f
hieu <hieu.hispvietnam@gmail.com>
2010-01-29 16:03:26 +0700

    revno: 1369 message:   Function Excel Management - Added missing file ...
    committer: hieu <hieu.hispvietnam@gmail.com>
    branch nick: trunk
    timestamp: Fri 2010-01-29 16:03:26 +0700

---
+++ dhis-2/dhis-web/dhis-web-excel-reporting/src/main/java/org/hisp/dhis/reportexcel/filemanager/action/UploadExcelTemplateAction.java
@@ -0,0 +1,114 @@
+package org.hisp.dhis.reportexcel.filemanager.action;
+
+/*
+ * Copyright (c) 2004-2007, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * * Neither the name of the HISP project nor the names of its contributors may
+ *   be used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import java.io.File;
+
+import org.hisp.dhis.options.SystemSettingManager;
+import org.hisp.dhis.reportexcel.action.ActionSupport;
+import org.hisp.dhis.reportexcel.state.SelectionManager;
+import org.hisp.dhis.system.util.StreamUtils;
+
+/**
+ * @author Dang Duy Hieu
+ * @version $Id
+ * @since 2010-01-28
+ */
+
+public class UploadExcelTemplateAction
+    extends ActionSupport
+{
+    // -------------------------------------------
+    // Dependency
+    // -------------------------------------------
+
+    private SystemSettingManager systemSettingManager;
+
+    public void setSystemSettingManager( SystemSettingManager systemSettingManager )
+    {
+        this.systemSettingManager = systemSettingManager;
+    }
+
+    private SelectionManager selectionManager;
+
+    public void setSelectionManager( SelectionManager selectionManager )
+    {
+        this.selectionManager = selectionManager;
+    }
+
+    // -------------------------------------------
+    // Input & Output
+    // -------------------------------------------
+
+    private String fileName;
+
+    public void setUploadFileName( String fileName )
+    {
+        this.fileName = fileName;
+    }
+
+    private File upload;
+
+    public void setUpload( File upload )
+    {
+        this.upload = upload;
+    }
+
+    public String execute()
+    {
+        String templateDirectoryConfig = (String) systemSettingManager
+            .getSystemSetting( SystemSettingManager.KEY_REPORT_TEMPLATE_DIRECTORY );
+
+        File output = new File( templateDirectoryConfig + File.separator + fileName );
+
+        selectionManager.setUploadFilePath( output.getAbsolutePath() );
+
+        if ( output.exists() )
+        {
+            message = "override_successful";
+        }
+        else
+        {
+            message = "upload_successful";
+        }
+
+        try
+        {
+            StreamUtils.write( upload, output );
+        }
+        catch ( Exception e )
+        {
+            message = "cannot_write_file_being_used";
+
+            return SUCCESS;
+        }
+
+        return SUCCESS;
+    }
+
+}
