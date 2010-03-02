218cd439d77e0e01f42af5f63fadad4532a2751a
Lars Helge Oeverland <larshelge@gmail.com>
2010-03-02 10:10:46 +0100

    revno: 1528 message:   Added registrationDate to createPatient supportive method, fixed a test error as that property was set to not-null in the hibernate mapping
    committer: Lars Helge Oeverland <larshelge@gmail.com>
    branch nick: trunk
    timestamp: Tue 2010-03-02 10:10:46 +0100

--- dhis-2/dhis-support/dhis-support-test/src/main/java/org/hisp/dhis/DhisConvenienceTest.java
+++ dhis-2/dhis-support/dhis-support-test/src/main/java/org/hisp/dhis/DhisConvenienceTest.java
@@ -873,6 +873,8 @@ public abstract class DhisConvenienceTest
         patient.setLastName( "LastName" + uniqueChar );
         patient.setGender( Patient.MALE );
         patient.setBirthDate( getDate( 1970, 1, 1 ) );
+        patient.setBloodGroup( "A" );
+        patient.setRegistrationDate( new Date() );
         
         return patient;
     }
