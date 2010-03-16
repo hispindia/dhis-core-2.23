1dee6fcaab1a21dbe713ea069fc810c04c1b25b3
Tran Chau <tran.hispvietnam@gmail.com>
2010-03-16 14:56:31 +0700

    revno: 1653 message:   Usability issues with static report web interface.
    committer: Tran Chau <tran.hispvietnam@gmail.com>
    branch nick: trunk
    timestamp: Tue 2010-03-16 14:56:31 +0700

--- dhis-2/dhis-web/dhis-web-reporting/src/main/webapp/dhis-web-reporting/javascript/document.js
+++ dhis-2/dhis-web/dhis-web-reporting/src/main/webapp/dhis-web-reporting/javascript/document.js
@@ -2,7 +2,17 @@
 function saveDocument()
 {
     var url = "validateDocument.action?name=" + getFieldValue( "name" );
-    
+	
+    var request = new Request();
+    request.setResponseTypeXML( 'message' );
+    request.setCallbackSuccess( saveDocumentReceived );
+    request.send( url );
+}
+
+function updateDocument()
+{
+    var url = "validateDocument.action?name=" + getFieldValue( "name" ) + "&id=" + getFieldValue('id');
+	
     var request = new Request();
     request.setResponseTypeXML( 'message' );
     request.setCallbackSuccess( saveDocumentReceived );
