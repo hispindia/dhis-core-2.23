fffba138927486f4106b9fbc9d5b103216852865
Viet <Viet@Viet-Nguyen>
2010-03-19 17:10:44 +0530

    revno: 1674 message:   Improve codes for checking date in jquery validation.
    committer: Viet <Viet@Viet-Nguyen>
    branch nick: trunk
    timestamp: Fri 2010-03-19 17:10:44 +0530

--- dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/util/validate/jquery.validate.js
+++ dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/util/validate/jquery.validate.js
@@ -1002,8 +1002,21 @@ $.extend($.validator, {
 	
 		// http://docs.jquery.com/Plugins/Validation/Methods/dateISO
 		dateISO: function(value, element) {
-			return this.optional(element) || /^\d{4}[\/-]\d{1,2}[\/-]\d{1,2}$/.test(value);
-			//return this.optional(element) || isDate(value, 'yyyy-MM-dd');
+			var check = false;
+			var re = /^\d{4}[\/-]\d{1,2}[\/-]\d{1,2}$/
+			if( re.test(value)){
+				var adata = value.split('-');
+				var aaaa = parseInt(adata[0],10);
+				var mm = parseInt(adata[1],10);
+				var gg = parseInt(adata[2],10);
+				var xdata = new Date(aaaa,mm-1,gg);
+				if ( ( xdata.getFullYear() == aaaa ) && ( xdata.getMonth () == mm - 1 ) && ( xdata.getDate() == gg ) )
+					check = true;
+				else
+					check = false;
+			} else
+				check = false;
+			return this.optional(element) || check;
 		},
 	
 
