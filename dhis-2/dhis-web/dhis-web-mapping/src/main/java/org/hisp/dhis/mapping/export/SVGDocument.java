90e7f5bab7d4a42231db386f017a00ac3269ac1e
Jan Henrik Overland <janhenrik.overland@gmail.com>
2010-04-10 14:08:02 +0200

    revno: 1764 message:   (GIS) Changed the output order to title - indicator - period in exported image.
    committer: Jan Henrik Overland <janhenrik.overland@gmail.com>
    branch nick: trunk
    timestamp: Sat 2010-04-10 14:08:02 +0200

--- dhis-2/dhis-web/dhis-web-mapping/src/main/java/org/hisp/dhis/mapping/export/SVGDocument.java
+++ dhis-2/dhis-web/dhis-web-mapping/src/main/java/org/hisp/dhis/mapping/export/SVGDocument.java
@@ -66,10 +66,10 @@ public class SVGDocument
         String title_ = "<g id=\"title\" style=\"display: block; visibility: visible;\"><text id=\"title\" x=\"30\" y=\"15\" font-size=\"14\" font-weight=\"bold\"><tspan>"
             + this.title + "</tspan></text></g>";
 
-        String indicator_ = "<g id=\"indicator\" style=\"display: block; visibility: visible;\"><text id=\"indicator\" x=\"30\" y=\"45\" font-size=\"12\"><tspan>"
+        String indicator_ = "<g id=\"indicator\" style=\"display: block; visibility: visible;\"><text id=\"indicator\" x=\"30\" y=\"30\" font-size=\"12\"><tspan>"
             + this.indicator.getName() + "</tspan></text></g>";
 
-        String period_ = "<g id=\"period\" style=\"display: block; visibility: visible;\"><text id=\"period\" x=\"30\" y=\"30\" font-size=\"12\"><tspan>"
+        String period_ = "<g id=\"period\" style=\"display: block; visibility: visible;\"><text id=\"period\" x=\"30\" y=\"45\" font-size=\"12\"><tspan>"
             + this.period.getName() + "</tspan></text></g>";
 
         String svg_ = doctype + this.svg;
