de61a150eb6a2d769d6775e601a49b19c0deafd1
Tran Ng Minh Luan <Luan@MinhLuan-PC>
2010-08-20 15:46:36 +0700

    revno: 1898 message:   Fix Login error with basic authentication   Add function download Activities, OrgUnit
    committer: Tran Ng Minh Luan <Luan@MinhLuan-PC>
    branch nick: cbhis-mobile
    timestamp: Fri 2010-08-20 15:46:36 +0700

---
+++ CBHISMobileClient/.processed/src/org/hisp/dhis/cbhis/model/Task.java
@@ -0,0 +1,40 @@
+package org.hisp.dhis.cbhis.model;
+
+public class Task {
+	
+	private int progStageInstId,progStageId;
+	private String progStageName;
+	private boolean complete;
+	
+	
+	public Task() {
+	}
+	
+	//Getter and Setter
+	public int getProgStageInstId() {
+		return progStageInstId;
+	}
+	public void setProgStageInstId(int progStageInstId) {
+		this.progStageInstId = progStageInstId;
+	}
+	public int getProgStageId() {
+		return progStageId;
+	}
+	public void setProgStageId(int progStageId) {
+		this.progStageId = progStageId;
+	}
+	public String getProgStageName() {
+		return progStageName;
+	}
+	public void setProgStageName(String progStageName) {
+		this.progStageName = progStageName;
+	}
+	public boolean isComplete() {
+		return complete;
+	}
+	public void setComplete(boolean complete) {
+		this.complete = complete;
+	}
+	
+	
+}
