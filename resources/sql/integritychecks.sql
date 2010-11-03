fc8f8d030429bf9be910b4963957675351138525
Lars Helge Overland <larshelge@gmail.com>
2010-11-03 21:27:57 +0100

    revno: 1996 message:   Added handy integrity check sql queries
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Wed 2010-11-03 21:27:57 +0100

---
+++ resources/sql/integritychecks.sql
@@ -0,0 +1,38 @@
+﻿
+-- Get name of datasets for a dataelement
+
+select ds.name from dataset ds
+join datasetmembers dm on (ds.datasetid=dm.datasetid) 
+join dataelement de on (dm.dataelementid=de.dataelementid)
+where de.name = 'Adverse Events Following Immunization';
+
+-- Get dataelement name and category combo for a section
+
+select de.name as dataelementname, cc.name as categorycomboname from dataelement de
+join categorycombo cc on(de.categorycomboid=cc.categorycomboid)
+join sectiondataelements sd on(de.dataelementid=sd.dataelementid)
+join section sc on(sd.sectionid=sc.sectionid)
+where sc.name = 'OPD Diagnoses';
+
+-- Get dataset memberships for data elements with more than one membership
+
+select de.name, ds.name from dataelement de
+join datasetmembers dm on(de.dataelementid=dm.dataelementid)
+join dataset ds on(dm.datasetid=ds.datasetid)
+where de.dataelementid in (
+  select de.dataelementid from dataelement de
+  full join datasetmembers ds on (de.dataelementid=ds.dataelementid)
+  group by de.dataelementid
+  having(count(de.dataelementid) > 1) );
+
+-- Get dataelements which are members of a section but not the section's dataset
+
+select de.name as dataelementname, sc.name as sectionname from sectiondataelements sd
+join dataelement de on(sd.dataelementid=de.dataelementid)
+join section sc on (sd.sectionid=sc.sectionid)
+where sd.dataelementid not in (
+  select dm.dataelementid from datasetmembers dm
+  join dataset ds on(dm.datasetid=ds.datasetid)
+  where sc.datasetid=ds.datasetid);
+
+
