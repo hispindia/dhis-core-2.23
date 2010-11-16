b1c7780c8db5e097f36ba1ceabc4a9abca16f4a0
Tran Chau <tran.hispvietnam@gmail.com>
2010-11-16 14:42:09 +0700

    revno: 2071 message:   Add Filter Excel file when uploading a file - Finished.
    committer: Tran Chau <tran.hispvietnam@gmail.com>
    branch nick: dhis2
    timestamp: Tue 2010-11-16 14:42:09 +0700

--- dhis-2/dhis-web/dhis-web-maintenance/dhis-web-maintenance-patient/src/main/webapp/dhis-web-maintenance-patient/javascript/import.js
+++ dhis-2/dhis-web/dhis-web-maintenance/dhis-web-maintenance-patient/src/main/webapp/dhis-web-maintenance-patient/javascript/import.js
@@ -19,3 +19,30 @@ function saveConfigurationCompleted( messageElement )
 {
     showSuccessMessage( i18n_save_successfull );
 }
+
+function validateUploadExcelFile()
+{
+	$.ajaxFileUpload
+	(
+		{
+			url:'validateUploadExcelFile.action',
+			secureuri:false,
+			fileElementId:'upload',
+			dataType: 'xml',
+			success: function (data, status)
+			{
+				data = data.getElementsByTagName('message')[0]; 
+				var type = data.getAttribute("type");
+				if(type=='error'){                    
+					showErrorMessage(data.firstChild.nodeValue);
+				}else{
+					byId('importingParam').submit();
+				} 
+			},
+			error: function (data, status, e)
+			{
+			
+			}
+		}
+	);
+}
