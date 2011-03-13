e302f97a3e1e841b0c5808412b8782564bff123b
Lars Helge Overland <larshelge@gmail.com>
2011-03-13 23:45:22 +0100

    revno: 3016 message:   Added util sql script
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Sun 2011-03-13 23:45:22 +0100

---
+++ resources/sql/movedata.sql
@@ -0,0 +1,17 @@
+
+-- Move population data from last year to this year
+
+-- Replace first periodid with current year, replace second periodid with last year, replace dataset.name with population dataset name
+
+delete from datavalue where periodid=43668 and dataelementid in (
+select dataelementid from datasetmembers
+join dataset using(datasetid)
+where dataset.name='Population estimates' );
+
+insert into datavalue(dataelementid,periodid,sourceid,categoryoptioncomboid,value,storedby,lastupdated,comment,followup)
+select dataelementid,43668 as periodid,sourceid,categoryoptioncomboid,value,storedby,lastupdated,comment,followup
+from datavalue
+where periodid=21011 and dataelementid in (
+select dataelementid from datasetmembers
+join dataset using(datasetid)
+where dataset.name='Population estimates' );
