04f22d93e4558e30713352843bbfd5fa4cfc06fa
Bjørnar Valbø <heia@norge.no>
2011-03-18 14:19:45 +0100

    revno: 3076 [merge] message:   The generic installer now supports multiple database restore.
    committer: Bjørnar Valbø <heia@norge.no>
    branch nick: dhis2
    timestamp: Fri 2011-03-18 14:19:45 +0100

--- installers/resources/postgres/restore_db_psql.bat
+++ installers/resources/postgres/restore_db_psql.bat
@@ -1,4 +1,13 @@
-SET PGPASSWORD=PG_PASSWORD
+SET PGPASSWORD=postgres
+SET DB_NAME=dhis2db
+SET n=2
+setlocal enabledelayedexpansion
+
 "POSTGRES_INSTALL_LOCATION\bin\psql.exe" -c "CREATE USER dhis CREATEDB LOGIN PASSWORD 'dhis';" -U postgres -w postgres
-"POSTGRES_INSTALL_LOCATION\bin\createdb.exe" -U postgres -w -O dhis dhis2db
-"POSTGRES_INSTALL_LOCATION\bin\psql.exe" -U postgres -w -d dhis2db -f "DATABASE_FILE"
+
+for %%X in (dhis2db\*.*) do (
+	"POSTGRES_INSTALL_LOCATION\bin\createdb.exe" -U postgres -w -O dhis !DB_NAME!
+	"POSTGRES_INSTALL_LOCATION\bin\psql.exe" -U postgres -w -d !DB_NAME! -f "%%X"
+	SET DB_NAME=%DB_NAME%!n!
+	SET /a n+=1
+)
