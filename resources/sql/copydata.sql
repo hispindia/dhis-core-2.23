74d3cc9c101dd12eaa465def0aaf3fda948d0e2f
Lars Helge Overland <larshelge@gmail.com>
2011-03-19 23:37:57 +0100

    revno: 3091 message:   Minor fix
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Sat 2011-03-19 23:37:57 +0100

--- resources/sql/copydata.sql
+++ resources/sql/copydata.sql
@@ -9,7 +9,7 @@ join dataset using(datasetid)
 where dataset.name='Population estimates' );
 
 insert into datavalue(dataelementid,periodid,sourceid,categoryoptioncomboid,value,storedby,lastupdated,comment,followup)
-select dataelementid,43668 as periodid,sourceid,categoryoptioncomboid,value,storedby,lastupdated,comment,followup
+select dataelementid,43668 as periodid,sourceid,categoryoptioncomboid,ceil(cast(value as double precision)*1.029) as value,storedby,lastupdated,comment,followup
 from datavalue
 where periodid=21011 and dataelementid in (
 select dataelementid from datasetmembers
