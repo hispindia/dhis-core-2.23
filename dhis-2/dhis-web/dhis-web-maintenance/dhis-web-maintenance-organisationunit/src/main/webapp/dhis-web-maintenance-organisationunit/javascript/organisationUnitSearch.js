a3827a87939915696dc89aac4d57703ac6814e8b
Lars Helge Overland <larshelge@gmail.com>
2011-03-20 23:46:36 +0100

    revno: 3106 message:   Disallowed unselect from tree in orgunit search
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Sun 2011-03-20 23:46:36 +0100

--- dhis-2/dhis-web/dhis-web-maintenance/dhis-web-maintenance-organisationunit/src/main/webapp/dhis-web-maintenance-organisationunit/javascript/organisationUnitSearch.js
+++ dhis-2/dhis-web/dhis-web-maintenance/dhis-web-maintenance-organisationunit/src/main/webapp/dhis-web-maintenance-organisationunit/javascript/organisationUnitSearch.js
@@ -12,7 +12,6 @@ function organisationUnitSelected( orgUnitIds )
 }
 
 selection.setListenerFunction( organisationUnitSelected );
-selection.setUnselectAllowed( true );
 
 function initOrgUnitDetails()
 {
