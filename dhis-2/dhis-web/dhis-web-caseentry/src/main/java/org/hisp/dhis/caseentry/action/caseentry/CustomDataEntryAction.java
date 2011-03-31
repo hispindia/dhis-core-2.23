23c90ee63a701f583081f493d1405d4988aff998
Tran Chau <tran.hispvietnam@gmail.com>
2011-03-31 12:48:54 +0700

    revno: 3226 message:   Delete system.out into files ( dhis-web-caseentry module  ).
    committer: Tran Chau <tran.hispvietnam@gmail.com>
    branch nick: dhis2
    timestamp: Thu 2011-03-31 12:48:54 +0700

--- dhis-2/dhis-web/dhis-web-caseentry/src/main/java/org/hisp/dhis/caseentry/action/caseentry/CustomDataEntryAction.java
+++ dhis-2/dhis-web/dhis-web-caseentry/src/main/java/org/hisp/dhis/caseentry/action/caseentry/CustomDataEntryAction.java
@@ -53,7 +53,6 @@ import org.hisp.dhis.patientdatavalue.PatientDataValueService;
 import org.hisp.dhis.program.Program;
 import org.hisp.dhis.program.ProgramInstance;
 import org.hisp.dhis.program.ProgramInstanceService;
-import org.hisp.dhis.program.ProgramService;
 import org.hisp.dhis.program.ProgramStage;
 import org.hisp.dhis.program.ProgramStageDataElement;
 import org.hisp.dhis.program.ProgramStageInstance;
--- dhis-2/dhis-web/dhis-web-caseentry/src/main/java/org/hisp/dhis/caseentry/action/caseentry/DataRecordingSelectAction.java
+++ dhis-2/dhis-web/dhis-web-caseentry/src/main/java/org/hisp/dhis/caseentry/action/caseentry/DataRecordingSelectAction.java
@@ -355,8 +355,7 @@ public class DataRecordingSelectAction
         {
             customDataEntryFormExists = true;
         }
-System.out.println("\n\n ++++++++++ \n customDataEntryFormExists : " + customDataEntryFormExists );
-System.out.println("\n\n useDefaultForm : " + useDefaultForm );
+
         if ( customDataEntryFormExists && useDefaultForm == null )
         {
             return CUSTOM_DATAENTRY_FORM;
