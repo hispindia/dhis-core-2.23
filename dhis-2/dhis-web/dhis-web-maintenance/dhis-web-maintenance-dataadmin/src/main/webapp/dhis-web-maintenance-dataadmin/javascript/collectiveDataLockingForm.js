3574d7d4a20da728a8f53815d360716fbac82170
Morten Olav Hansen <mortenoh@gmail.com>
2011-04-15 14:27:41 +0200

    revno: 3404 message:   fixed selection list in data locking
    committer: Morten Olav Hansen <mortenoh@gmail.com>
    branch nick: dhis2
    timestamp: Fri 2011-04-15 14:27:41 +0200

--- dhis-2/dhis-web/dhis-web-maintenance/dhis-web-maintenance-dataadmin/src/main/webapp/dhis-web-maintenance-dataadmin/javascript/collectiveDataLockingForm.js
+++ dhis-2/dhis-web/dhis-web-maintenance/dhis-web-maintenance-dataadmin/src/main/webapp/dhis-web-maintenance-dataadmin/javascript/collectiveDataLockingForm.js
@@ -1,7 +1,14 @@
-jQuery(document).ready(function() {
-	validation2('lockingForm', function(form) {
-		validateCollectiveDataLockingForm(form);
-	}, {
-		'rules' : getValidationRules("dataLocking")
-	});
+jQuery(document).ready(function()
+{
+    validation2('lockingForm', function( form )
+    {
+        validateCollectiveDataLockingForm(form);
+    },
+    {
+        'beforeValidateHandler' : function() {
+            $("#selectedPeriods option").each(function() { $(this).attr("selected", "true"); });
+            $("#selectedDataSets option").each(function() { $(this).attr("selected", "true"); });
+        },
+        'rules' : getValidationRules("dataLocking")
+    });
 });
