23ef10c754aad48431caea66bb550eb15879b365
Hieu <hieu.hispvietnam@gmail.com>
2011-05-10 16:05:05 +0700

    revno: 3555 message:   Fixed bug when updating the blood group of a beneficiary
    committer: Hieu <hieu.hispvietnam@gmail.com>
    branch nick: dhis2
    timestamp: Tue 2011-05-10 16:05:05 +0700

--- dhis-2/dhis-web/dhis-web-maintenance/dhis-web-maintenance-patient/src/main/java/org/hisp/dhis/patient/action/patient/UpdatePatientAction.java
+++ dhis-2/dhis-web/dhis-web-maintenance/dhis-web-maintenance-patient/src/main/java/org/hisp/dhis/patient/action/patient/UpdatePatientAction.java
@@ -102,6 +102,8 @@ public class UpdatePatientAction
 
     private String gender;
 
+    private String bloodGroup;
+	
     private boolean underAge;
 
     private Integer representativeId;
@@ -160,13 +162,15 @@ public class UpdatePatientAction
         // ---------------------------------------------------------------------
 
         patient.setGender( gender );
-
         patient.setIsDead( isDead );
+        patient.setBloodGroup( bloodGroup );
+		
         if ( deathDate != null )
         {
             deathDate = deathDate.trim();
             patient.setDeathDate( format.parseDate( deathDate ) );
         }
+		
         patient.setUnderAge( underAge );
         patient.setOrganisationUnit( organisationUnit );
 
