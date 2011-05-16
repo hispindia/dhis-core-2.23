1a029559f6231df2773a4d2ed825b29e6753cb29
Jan Henrik Overland <janhenrik.overland@gmail.com>
2011-05-16 17:26:21 +0200

    revno: 3632 [merge] message:   CSS fixes.
    committer: Jan Henrik Overland <janhenrik.overland@gmail.com>
    branch nick: dhis2
    timestamp: Mon 2011-05-16 17:26:21 +0200

--- dhis-2/dhis-web/dhis-web-mapping/src/main/webapp/dhis-web-mapping/resources/ext-ux/msg/msg.js
+++ dhis-2/dhis-web/dhis-web-mapping/src/main/webapp/dhis-web-mapping/resources/ext-ux/msg/msg.js
@@ -5,7 +5,7 @@ Ext.message = function(){
         var icon = bool ? '../../images/check.png' : '../../images/error2.png';
         return ['<div class="msg">',
                 '<div class="x-box-tl"><div class="x-box-tr"><div class="x-box-tc"></div></div></div>',
-                '<div class="x-box-ml"><div class="x-box-mr"><div class="x-box-mc"><img src="' + icon + '" style="vertical-align:middle; padding:0px 8px 2px 0px;"/>', s, '</div></div></div>',
+                '<div class="x-box-ml"><div class="x-box-mr"><div class="x-box-mc"><img src="' + icon + '" class="icon"/>', s, '</div></div></div>',
                 '<div class="x-box-bl"><div class="x-box-br"><div class="x-box-bc"></div></div></div>',
                 '</div>'].join('');
     }
