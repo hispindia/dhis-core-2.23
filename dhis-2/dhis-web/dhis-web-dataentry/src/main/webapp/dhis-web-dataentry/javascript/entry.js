9cd93d3d4a7a46e6a70d4c17781f3521daf2b4d9
Lars Helge Overland <larshelge@gmail.com>
2011-06-03 15:21:47 +0200

    revno: 3836 message:   Removed unused function
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Fri 2011-06-03 15:21:47 +0200

--- dhis-2/dhis-web/dhis-web-dataentry/src/main/webapp/dhis-web-dataentry/javascript/entry.js
+++ dhis-2/dhis-web/dhis-web-dataentry/src/main/webapp/dhis-web-dataentry/javascript/entry.js
@@ -286,24 +286,3 @@ function ValueSaver( dataElementId_, optionComboId_, organisationUnitId_, value_
         element.style.backgroundColor = color;
     }
 }
-
-// -----------------------------------------------------------------------------
-// Section
-// -----------------------------------------------------------------------------
-
-function openCloseSection( sectionId )
-{
-	var divSection = document.getElementById( sectionId );
-	var sectionLabel = document.getElementById( sectionId + ":name" );	
-	
-	if( divSection.style.display == 'none' )
-	{			
-		divSection.style.display = ('block');
-		sectionLabel.style.textAlign = 'center';
-	}
-	else
-	{			
-		divSection.style.display = ('none');
-		sectionLabel.style.textAlign = 'left';
-	}
-}
