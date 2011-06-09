f6b48af2a273a7a218fbfbfa5de1d67df1a779f8
Morten Olav Hansen <mortenoh@gmail.com>
2011-06-09 21:35:49 +0200

    revno: 3860 message:   minor fixes
    committer: Morten Olav Hansen <mortenoh@gmail.com>
    branch nick: dhis2
    timestamp: Thu 2011-06-09 21:35:49 +0200

--- dhis-2/dhis-web/dhis-web-dashboard-integration/src/main/webapp/dhis-web-dashboard-integration/javascript/dropdown.js
+++ dhis-2/dhis-web/dhis-web-dashboard-integration/src/main/webapp/dhis-web-dashboard-integration/javascript/dropdown.js
@@ -1,32 +1,23 @@
-
 function setAreaItem( area, item )
 {
-    var request = new Request();
-    request.setCallbackSuccess(setAreaItemReceived);
-    request.send("setAreaItem.action?area=" + area + "&item=" + item);
-}
-
-function setAreaItemReceived( messageElement )
-{
-    window.location.href = "index.action";
+    $.get("setAreaItem.action?area=" + area + "&item=" + item, function()
+    {
+        window.location.href = "index.action";
+    });
 }
 
 function clearArea( area )
 {
-    var request = new Request();
-    request.setCallbackSuccess(clearAreaReceived);
-    request.send("clearArea.action?area=" + area);
-}
-
-function clearAreaReceived( messageElement )
-{
-    window.location.href = "index.action";
+    $.get("clearArea.action?area=" + area, function()
+    {
+        window.location.href = "index.action";
+    });
 }
 
 function viewChart( url )
 {
-    var width = size === 'wide' ? 1000 : 700;
-    var height = size === 'tall' ? 800 : 500;
+    var width = 700;
+    var height = 500;
 
     $('#chartImage').attr('src', url);
     $('#chartView').dialog({
