d8f78f5e03b8963d0805c7ec9a0b9a1dc1c603e3
Lars Helge Overland <larshelge@gmail.com>
2011-06-13 23:19:39 +0200

    revno: 3891 message:   Cron readme
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Mon 2011-06-13 23:19:39 +0200

--- resources/cron/pg_backup.sh
+++ resources/cron/pg_backup.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# The backupdir must be created manually (mkdir pg_backups)
-# The backupdir owner must be changed to postgres (chown postgres pg_backups)
-# Script must be made executable (chmod 755 pg_backup.sh)
-# Script must be invoked by postgres user
-
 backup_dir="/var/backups/pg_backups"
 datetime=`date +%F`
 backupfile="$backup_dir/pg-backup-$datetime.gz"
