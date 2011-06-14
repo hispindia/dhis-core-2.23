803b6eb79e164938f628a2cd0a7d44d457f9a257
Morten Olav Hansen <mortenoh@gmail.com>
2011-06-14 10:04:40 +0200

    revno: 3895 message:   fixed bug when removing/adding charts on the dashboard
    committer: Morten Olav Hansen <mortenoh@gmail.com>
    branch nick: dhis2
    timestamp: Tue 2011-06-14 10:04:40 +0200

--- dhis-2/dhis-web/dhis-web-dashboard-integration/src/main/webapp/dhis-web-dashboard-integration/javascript/dropdown.js
+++ dhis-2/dhis-web/dhis-web-dashboard-integration/src/main/webapp/dhis-web-dashboard-integration/javascript/dropdown.js
@@ -1,17 +1,22 @@
 function setAreaItem( area, item )
 {
-    $.get("setAreaItem.action?area=" + area + "&item=" + item, function()
+    $.get( "setAreaItem.action", {
+        'area' : area,
+        'item' : item
+    }, function()
     {
         window.location.href = "index.action";
-    });
+    } );
 }
 
 function clearArea( area )
 {
-    $.get("clearArea.action?area=" + area, function()
+    $.get( "clearArea.action", {
+        'area' : area
+    }, function()
     {
         window.location.href = "index.action";
-    });
+    } );
 }
 
 function viewChart( url )
@@ -19,13 +24,13 @@ function viewChart( url )
     var width = 700 + 20;
     var height = 500 + 20;
 
-    $('#chartImage').attr('src', url);
-    $('#chartView').dialog({
+    $( '#chartImage' ).attr( 'src', url );
+    $( '#chartView' ).dialog( {
         autoOpen : true,
         modal : true,
         height : height + 35,
         width : width,
         resizable : false,
         title : 'Viewing Chart'
-    });
+    } );
 }
