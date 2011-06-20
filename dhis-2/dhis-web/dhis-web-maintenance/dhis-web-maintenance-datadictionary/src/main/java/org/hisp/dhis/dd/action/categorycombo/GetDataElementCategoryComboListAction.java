92146f680cc8ed83c980a7eb8c04ec586ff0776d
Morten Olav Hansen <mortenoh@gmail.com>
2011-06-20 09:56:23 +0300

    revno: 3959 message:   reverted..
    committer: Morten Olav Hansen <mortenoh@gmail.com>
    branch nick: dhis2
    timestamp: Mon 2011-06-20 09:56:23 +0300

--- dhis-2/dhis-web/dhis-web-maintenance/dhis-web-maintenance-datadictionary/src/main/java/org/hisp/dhis/dd/action/categorycombo/GetDataElementCategoryComboListAction.java
+++ dhis-2/dhis-web/dhis-web-maintenance/dhis-web-maintenance-datadictionary/src/main/java/org/hisp/dhis/dd/action/categorycombo/GetDataElementCategoryComboListAction.java
@@ -100,8 +100,6 @@ public class GetDataElementCategoryComboListAction
         defaultCombo = dataElementCategoryService
             .getDataElementCategoryComboByName( DataElementCategoryCombo.DEFAULT_CATEGORY_COMBO_NAME );
 
-        Collections.sort( dataElementCategoryCombos, new DataElementCategoryComboNameComparator() );
-
         if ( isNotBlank( key ) ) // Filter on key only if set
         {
             this.paging = createPaging( dataElementCategoryService.getDataElementCategoryComboCountByName( key ) );
@@ -114,6 +112,8 @@ public class GetDataElementCategoryComboListAction
             
             dataElementCategoryCombos = new ArrayList<DataElementCategoryCombo>( dataElementCategoryService.getDataElementCategoryCombosBetween( paging.getStartPos(), paging.getPageSize() ) );
         }
+
+        Collections.sort( dataElementCategoryCombos, new DataElementCategoryComboNameComparator() );
         
         return SUCCESS;
     }
