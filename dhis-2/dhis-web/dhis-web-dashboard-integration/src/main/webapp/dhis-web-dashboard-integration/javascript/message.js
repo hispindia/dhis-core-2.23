0b60945a0c7e57fa1d4a20c68dcf60d061827f6d
Lars Helge Overland <larshelge@gmail.com>
2011-08-03 14:14:53 +0200

    revno: 4253 [merge] message:   Added validation for messages
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Wed 2011-08-03 14:14:53 +0200

--- dhis-2/dhis-web/dhis-web-dashboard-integration/src/main/webapp/dhis-web-dashboard-integration/javascript/message.js
+++ dhis-2/dhis-web/dhis-web-dashboard-integration/src/main/webapp/dhis-web-dashboard-integration/javascript/message.js
@@ -1,3 +1,11 @@
+
+var selectedOrganisationUnits = [];
+
+function selectOrganisationUnit__( units )
+{
+	selectedOrganisationUnits = units;
+}
+
 function removeMessage( id )
 {
     removeItem( id, "", i18n_confirm_delete_message, "removeMessage.action" );
@@ -6,4 +14,24 @@ function removeMessage( id )
 function read( id )
 {
     window.location.href = "readMessage.action?id=" + id;
+}
+
+function validateMessage()
+{
+	var subject = $( '#subject' ).val();
+	var text = $( '#text' ).val();
+	
+	if ( subject == null || subject.trim() == '' )
+	{
+		setHeaderMessage( i18n_enter_subject );
+		return false;
+	}
+	
+	if ( text == null || text.trim() == '' )
+	{
+		setHeaderMessage( i18n_enter_text );
+		return false;
+	}
+	
+	return true;
 }
