3ac9e59d1f6c9c3978f87b6516c2b2ebb6e3add3
Morten Olav Hansen <mortenoh@gmail.com>
2011-08-26 11:54:28 +0200

    revno: 4454 message:   minor fix
    committer: Morten Olav Hansen <mortenoh@gmail.com>
    branch nick: dhis2
    timestamp: Fri 2011-08-26 11:54:28 +0200

--- dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/javascripts/dhis2/dhis2.availability.js
+++ dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/javascripts/dhis2/dhis2.availability.js
@@ -49,6 +49,7 @@ dhis2.availability.startAvailabilityCheck = function( onlineInterval, offlineInt
     {
         $.ajax( {
             url : "../dhis-web-commons-stream/ping.action",
+            cache : false,
             success : function( data, textStatus, jqXHR )
             {
                 dhis2.availability._isAvailable = true;
