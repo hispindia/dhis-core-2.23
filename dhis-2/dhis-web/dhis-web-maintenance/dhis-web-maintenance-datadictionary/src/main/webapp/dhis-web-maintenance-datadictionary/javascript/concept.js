6ffa9756a80f38bda3174d973ab64bc7b7ef4fae
Lars Helge Overland <larshelge@gmail.com>
2011-09-27 14:08:17 +0200

    revno: 4716 message:   Cleaned up concept javascript
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Tue 2011-09-27 14:08:17 +0200

--- dhis-2/dhis-web/dhis-web-maintenance/dhis-web-maintenance-datadictionary/src/main/webapp/dhis-web-maintenance-datadictionary/javascript/concept.js
+++ dhis-2/dhis-web/dhis-web-maintenance/dhis-web-maintenance-datadictionary/src/main/webapp/dhis-web-maintenance-datadictionary/javascript/concept.js
@@ -4,17 +4,10 @@
 
 function showConceptDetails( conceptId )
 {
-    var request = new Request();
-    request.setResponseTypeXML( 'concept' );
-    request.setCallbackSuccess( conceptReceived );
-    request.send( 'getConcept.action?id=' + conceptId );
-}
-
-function conceptReceived( conceptElement )
-{
-    setFieldValue( 'nameField', getElementValue( conceptElement, 'name' ) );
-
-    showDetails();
+    jQuery.post( 'getConcept.action', { id : conceptId }, function( json ) {
+		setInnerHTML( 'nameField', json.concept.name );
+		showDetails();
+	});
 }
 
 // -----------------------------------------------------------------------------
@@ -24,32 +17,4 @@ function conceptReceived( conceptElement )
 function removeConcept( conceptId, conceptName )
 {
     removeItem( conceptId, conceptName, i18n_confirm_delete, 'removeConcept.action' );
-}
-
-// -----------------------------------------------------------------------------
-// Add/Update category concept
-// -----------------------------------------------------------------------------
-
-function validateAddUpdateConcept( mode )
-{
-    var name = $( "#name" ).val();
-
-    $.getJSON( "validateAddUpdateConcept.action", {
-        "name" : name,
-        "mode" : mode
-    }, function( json )
-    {
-        if ( json.response == "success" )
-        {
-            if ( mode == "add" )
-            {
-                byId( "addConceptForm" ).submit();
-                return;
-            }
-            byId( "updateConceptForm" ).submit();
-        } else if ( json.response == "input" )
-        {
-            setHeaderDelayMessage( json.message );
-        }
-    } );
 }
