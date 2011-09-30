07ea9459ef1155efd18d4d5e9088ab3c40a3679c
Lars Helge Overland <larshelge@gmail.com>
2011-09-30 15:50:03 +0200

    revno: 4785 message:   Added util script
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Fri 2011-09-30 15:50:03 +0200

---
+++ resources/sql/div.sql
@@ -0,0 +1,8 @@
+
+-- Delete all data values for category combo
+
+delete from datavalue where categoryoptioncomboid in (
+select cc.categoryoptioncomboid from categoryoptioncombo cc
+join categorycombos_optioncombos co
+on (cc.categoryoptioncomboid=co.categoryoptioncomboid)
+where categorycomboid=12414 );
