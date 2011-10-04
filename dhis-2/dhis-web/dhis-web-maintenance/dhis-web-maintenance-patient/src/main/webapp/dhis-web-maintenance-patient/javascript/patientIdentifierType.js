ea20556155d222544f6b154b98c0bc0f1ac765a5
Tran Chau <tran.hispvietnam@gmail.com>
2011-10-04 14:35:17 +0700

    revno: 4820 message:   Error when showing details for Identifier Type.
    committer: Tran Chau <tran.hispvietnam@gmail.com>
    branch nick: dhis2
    timestamp: Tue 2011-10-04 14:35:17 +0700

--- dhis-2/dhis-web/dhis-web-maintenance/dhis-web-maintenance-patient/src/main/webapp/dhis-web-maintenance-patient/javascript/patientIdentifierType.js
+++ dhis-2/dhis-web/dhis-web-maintenance/dhis-web-maintenance-patient/src/main/webapp/dhis-web-maintenance-patient/javascript/patientIdentifierType.js
@@ -6,20 +6,20 @@ function showPatientIdentifierTypeDetails( patientIdentifierTypeId )
 {
 	jQuery.post( 'getPatientIdentifierType.action', { id: patientIdentifierTypeId },
 		function ( json ) {
-			setInnerHTML( 'idField', json.patientAttributeType.id );
-			setInnerHTML( 'nameField', json.patientAttributeType.name );	
-			setInnerHTML( 'descriptionField', json.patientAttributeType.description );
+			setInnerHTML( 'idField', json.patientIdentifierType.id );
+			setInnerHTML( 'nameField', json.patientIdentifierType.name );	
+			setInnerHTML( 'descriptionField', json.patientIdentifierType.description );
 			
 			var boolValueMap = { 'true':i18n_yes, 'false':i18n_no };
-			var boolType = json.patientAttributeType.mandatory;
+			var boolType = json.patientIdentifierType.mandatory;
 			setInnerHTML( 'mandatoryField', boolValueMap[boolType] );
 			
-			boolType = json.patientAttributeType.related;
+			boolType = json.patientIdentifierType.related;
 			setInnerHTML( 'relatedField', boolValueMap[boolType] );
-			setInnerHTML( 'noCharsField', json.patientAttributeType.noChars );
+			setInnerHTML( 'noCharsField', json.patientIdentifierType.noChars );
 			
 			var valueTypeMap = { 'text':i18n_string, 'number':i18n_number, 'letter':i18n_alphabet };
-			var valueType = json.patientAttributeType.type;
+			var valueType = json.patientIdentifierType.type;
 			setInnerHTML( 'typeField', valueTypeMap[valueType] );
 			
 			showDetails();
