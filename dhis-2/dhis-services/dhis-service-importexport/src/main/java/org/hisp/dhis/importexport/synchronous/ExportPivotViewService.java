1c7eaed11d9494398776d8f2edd18ba6807e3f4d
Bob Jolliffe <bobjolliffe@gmail.com>
2011-10-12 10:47:13 +0100

    revno: 4905 message:   Finer control over buffering the output of mydatamart export
    committer: Bob Jolliffe <bobjolliffe@gmail.com>
    branch nick: dhis2
    timestamp: Wed 2011-10-12 10:47:13 +0100

--- dhis-2/dhis-services/dhis-service-importexport/src/main/java/org/hisp/dhis/importexport/synchronous/ExportPivotViewService.java
+++ dhis-2/dhis-services/dhis-service-importexport/src/main/java/org/hisp/dhis/importexport/synchronous/ExportPivotViewService.java
@@ -61,6 +61,11 @@ public class ExportPivotViewService
 {
     private static final Log log = LogFactory.getLog( ExportPivotViewService.class );
 
+    // The number of values to buffer before forcing attempt to flush
+    // Ideally this value should be set to a number of bytes say twice the size of the underlying buffer
+    // 500 is just a conservative thumbsuck
+    private static final int CHUNK = 500;
+
     // service can export either aggregated datavalues or aggregated indicator values
     public enum RequestType
     {
@@ -185,6 +190,9 @@ public class ExportPivotViewService
         AggregatedDataValue adv = Iterator.next();
 
         writer.write( "# period, orgunit, dataelement, catoptcombo, value\n" );
+        
+        int values = 0;
+
         while ( adv != null )
         {
             int periodId = adv.getPeriodId();
@@ -195,6 +203,14 @@ public class ExportPivotViewService
             writer.write( adv.getDataElementId() + "," );
             writer.write( adv.getCategoryOptionComboId() + "," );
             writer.write( adv.getValue() + "\n" );
+
+            // defend against expanding the writer buffer uncontrollably
+            values = ++values % CHUNK;
+
+            if (values == 0)
+            {
+                writer.flush();
+            }
 
             adv = Iterator.next();
         }
@@ -211,6 +227,8 @@ public class ExportPivotViewService
 
         writer.write( "# period, orgunit, indicator, factor, numerator, denominator\n" );
         
+        int values = 0;
+
         while ( aiv != null )
         {
             int periodId = aiv.getPeriodId();
@@ -222,6 +240,14 @@ public class ExportPivotViewService
             writer.write( MathUtils.roundToString( aiv.getFactor(), PRECISION ) + "," );
             writer.write( MathUtils.roundToString( aiv.getNumeratorValue(), PRECISION ) + "," );
             writer.write( MathUtils.roundToString( aiv.getDenominatorValue(), PRECISION ) + "\n" );
+
+            // defend against expanding the writer buffer uncontrollably
+            values = ++values % CHUNK;
+
+            if (values == 0)
+            {
+                writer.flush();
+            }
 
             aiv = Iterator.next();
         }
