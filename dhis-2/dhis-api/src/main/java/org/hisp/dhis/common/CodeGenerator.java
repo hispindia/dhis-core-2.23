6bb49f1e7c2dbda83f3429c269da0a80e8c7f0e1
Bob Jolliffe <bobjolliffe@gmail.com>
2011-10-22 21:37:04 +0100

    revno: 5003 message:   Basic 11 char pseudo random code generator using default algorithm
    committer: Bob Jolliffe <bobjolliffe@gmail.com>
    branch nick: dhis2
    timestamp: Sat 2011-10-22 21:37:04 +0100

---
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/common/CodeGenerator.java
@@ -0,0 +1,61 @@
+package org.hisp.dhis.common;
+
+
+import java.security.SecureRandom;
+
+/*
+ * Copyright (c) 2004-2005, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * * Neither the name of the <ORGANIZATION> nor the names of its contributors may
+ *   be used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ *
+ * @author bobj
+ * @version created 22-Oct-2011
+ */
+public class CodeGenerator {
+
+    public static final String allowedChars = "0123456789" +
+          "abcdefghijklmnopqrstuvwxyz" + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public static final int codeSize = 10;
+
+    /**
+     * Generates a pseudo random string using the allowed characters
+     *
+     * @return the Code
+     */
+    public static String generateCode()
+    {
+        // using the system default algorithm and seed
+        SecureRandom sr = new SecureRandom();
+
+        char[] randomChars = new char[11];
+        for (int i = 0; i< 11; ++i) {
+            randomChars[i] = allowedChars.charAt( sr.nextInt( 61) );
+        }
+        return new String(randomChars);
+    }
+}
