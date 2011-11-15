4af562762aa63cd1f5261e2f59fe8e74e3bf4621
Morten Olav Hansen <mortenoh@gmail.com>
2011-11-15 12:05:52 +0000

    revno: 5165 message:   minor changes
    committer: Morten Olav Hansen <mortenoh@gmail.com>
    branch nick: dhis2
    timestamp: Tue 2011-11-15 12:05:52 +0000

---
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/codelist/CodeList.java
@@ -0,0 +1,41 @@
+package org.hisp.dhis.codelist;
+
+import org.hisp.dhis.common.AbstractIdentifiableObject;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Morten Olav Hansen <mortenoh@gmail.com>
+ */
+@XmlRootElement( name = "codeList" )
+@XmlAccessorType( value = XmlAccessType.FIELD )
+public class CodeList<T extends AbstractIdentifiableObject>
+{
+    @XmlElement( name = "identifiableObject" )
+    private List<T> list = new ArrayList<T>();
+
+    public CodeList()
+    {
+
+    }
+
+    public CodeList( List<T> list )
+    {
+        this.list = list;
+    }
+
+    public List<T> getList()
+    {
+        return list;
+    }
+
+    public void setList( List<T> list )
+    {
+        this.list = list;
+    }
+}
