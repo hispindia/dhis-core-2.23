55e4e948d623e3ce811a500718ce5fe1fad2a4b1
Lars Helge Overland <larshelge@gmail.com>
2011-11-24 10:18:23 +0100

    revno: 5210 message:   scripts
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Thu 2011-11-24 10:18:23 +0100

--- resources/sql/div.sql
+++ resources/sql/div.sql
@@ -6,3 +6,18 @@ select cc.categoryoptioncomboid from categoryoptioncombo cc
 join categorycombos_optioncombos co
 on (cc.categoryoptioncomboid=co.categoryoptioncomboid)
 where categorycomboid=12414 );
+
+-- Data elements and frequency with average agg operator (lower than yearly negative for data mart performance)
+
+select d.dataelementid, d.name, pt.name from dataelement d 
+join datasetmembers dsm on d.dataelementid=dsm.dataelementid 
+join dataset ds on dsm.datasetid=ds.datasetid 
+join periodtype pt on ds.periodtypeid = pt.periodtypeid 
+where d.aggregationtype = 'average';
+
+-- Data elements with aggregation levels
+
+select d.dataelementid, d.name, dal.aggregationlevel from dataelementaggregationlevels dal 
+join dataelement d on dal.dataelementid=d.dataelementid 
+order by name, aggregationlevel;
+
