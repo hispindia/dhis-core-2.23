45ccbc8b8130470493e39dd3262c51c1ed597124
Morten Olav Hansen <mortenoh@gmail.com>
2011-12-14 11:28:01 +0100

    revno: 5403 message:   added reporttable to resources
    committer: Morten Olav Hansen <mortenoh@gmail.com>
    branch nick: dhis2
    timestamp: Wed 2011-12-14 11:28:01 +0100

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/api/webdomain/Resources.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/api/webdomain/Resources.java
@@ -46,6 +46,7 @@ import org.hisp.dhis.organisationunit.OrganisationUnitGroupSets;
 import org.hisp.dhis.organisationunit.OrganisationUnitGroups;
 import org.hisp.dhis.organisationunit.OrganisationUnits;
 import org.hisp.dhis.report.Reports;
+import org.hisp.dhis.reporttable.ReportTables;
 import org.hisp.dhis.sqlview.SqlViews;
 import org.hisp.dhis.user.Users;
 import org.hisp.dhis.validation.ValidationRuleGroups;
@@ -134,5 +135,6 @@ public class Resources extends BaseLinkableObject
         resources.add( new Resource( "DataSets", DataSets.class, requestMethods, mediaTypes ) );
         resources.add( new Resource( "Users", Users.class, requestMethods, mediaTypes ) );
         resources.add( new Resource( "Reports", Reports.class, requestMethods, mediaTypes ) );
+        resources.add( new Resource( "ReportTables", ReportTables.class, requestMethods, mediaTypes ) );
     }
 }
