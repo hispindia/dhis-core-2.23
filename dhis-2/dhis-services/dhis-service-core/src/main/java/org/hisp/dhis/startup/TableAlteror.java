8e8c5b7421c621eacdc094c21e9dc99a16a59f5c
Lars Helge Overland <larshelge@gmail.com>
2011-12-15 11:02:30 +0100

    revno: 5427 message:   Minor fix to table alteror
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Thu 2011-12-15 11:02:30 +0100

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/startup/TableAlteror.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/startup/TableAlteror.java
@@ -345,30 +345,37 @@ public class TableAlteror
         executeSql( "update dataelement set zeroissignificant = false where zeroissignificant is null" );
         executeSql( "update organisationunit set haspatients = false where haspatients is null" );
 
+        executeSql( "update reporttable set reportingmonth = false where reportingmonth is null" );
+        executeSql( "update reporttable set reportingbimonth = false where reportingbimonth is null" );
+        executeSql( "update reporttable set reportingquarter = false where reportingquarter is null" );
         executeSql( "update reporttable set monthsthisyear = false where monthsthisyear is null" );
-        executeSql( "update reporttable set quartersthisyear = false where quartersthisyear is null" );
-        executeSql( "update reporttable set thisyear = false where thisyear is null" );
-        executeSql( "update reporttable set monthslastyear = false where monthslastyear is null" );
         executeSql( "update reporttable set quartersthisyear = false where quartersthisyear is null" );
         executeSql( "update reporttable set thisyear = false where thisyear is null" );
         executeSql( "update reporttable set monthslastyear = false where monthslastyear is null" );
         executeSql( "update reporttable set quarterslastyear = false where quarterslastyear is null" );
-        executeSql( "update reporttable set monthsthisyear = false where monthsthisyear is null" );
         executeSql( "update reporttable set lastyear = false where lastyear is null" );
         executeSql( "update reporttable set last5years = false where last5years is null" );
         executeSql( "update reporttable set last4quarters = false where last4quarters is null" );
         executeSql( "update reporttable set last12months = false where last12months is null" );
+        executeSql( "update reporttable set last6bimonths = false where last6bimonths is null" );
+        executeSql( "update reporttable set last4quarters = false where last4quarters is null" );
+        executeSql( "update reporttable set last2sixmonths = false where last2sixmonths is null" );
 
+        executeSql( "update chart set reportingmonth = false where reportingmonth is null" );
+        executeSql( "update chart set reportingbimonth = false where reportingbimonth is null" );
+        executeSql( "update chart set reportingquarter = false where reportingquarter is null" );
         executeSql( "update chart set monthsthisyear = false where monthsthisyear is null" );
-        executeSql( "update chart set quartersthisyear = false where quartersthisyear is null" );
-        executeSql( "update chart set thisyear = false where thisyear is null" );
-        executeSql( "update chart set monthslastyear = false where monthslastyear is null" );
         executeSql( "update chart set quartersthisyear = false where quartersthisyear is null" );
         executeSql( "update chart set thisyear = false where thisyear is null" );
         executeSql( "update chart set monthslastyear = false where monthslastyear is null" );
         executeSql( "update chart set quarterslastyear = false where quarterslastyear is null" );
-        executeSql( "update chart set monthsthisyear = false where monthsthisyear is null" );
         executeSql( "update chart set lastyear = false where lastyear is null" );
+        executeSql( "update chart set last5years = false where last5years is null" );
+        executeSql( "update chart set last4quarters = false where last4quarters is null" );
+        executeSql( "update chart set last12months = false where last12months is null" );
+        executeSql( "update chart set last6bimonths = false where last6bimonths is null" );
+        executeSql( "update chart set last4quarters = false where last4quarters is null" );
+        executeSql( "update chart set last2sixmonths = false where last2sixmonths is null" );
 
         log.info( "Tables updated" );
     }
