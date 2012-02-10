f28719709cc66395a23578a7f32e793c1dee21d9
Tran Chau <tran.hispvietnam@gmail.com>
2012-02-10 12:52:52 +0700

    revno: 5905 message:   Fix bug: (patient) Error when running program-stage formula.
    committer: Tran Chau <tran.hispvietnam@gmail.com>
    branch nick: dhis2
    timestamp: Fri 2012-02-10 12:52:52 +0700

--- dhis-2/dhis-services/dhis-service-patient/src/main/java/org/hisp/dhis/caseaggregation/DefaultCaseAggregationConditionService.java
+++ dhis-2/dhis-services/dhis-service-patient/src/main/java/org/hisp/dhis/caseaggregation/DefaultCaseAggregationConditionService.java
@@ -763,7 +763,7 @@ public class DefaultCaseAggregationConditionService
         
         if( operator.equals( AGGRERATION_SUM ) )
         {
-            sql = "SELECT pi.programstageinstanceid ";
+            sql = "SELECT psi.programstageinstanceid ";
         }
         
         return sql + "FROM programstageinstance as psi "
@@ -879,6 +879,8 @@ public class DefaultCaseAggregationConditionService
         }
 
         return "SELECT distinct(p.patientid) FROM programinstance as pi "
+                   + "INNER JOIN programstageinstance psi "
+                   + "ON pi.programinstanceid = psi.programinstanceid"
                    + "INNER JOIN patient as p ON pi.patientid = p.patientid  " 
                    + "WHERE psi.programstageid=" + programStageId + " "
                    + "AND psi.executiondate >= '" + startDate
