55d9c37ce3127709e019097b9c525f4cabb6e59e
Lars Helge Overland <larshelge@gmail.com>
2012-02-20 15:25:48 +0100

    revno: 6053 message:   Green style extreme make-over
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Mon 2012-02-20 15:25:48 +0100

--- dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/css/green/dom.js
+++ dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/css/green/dom.js
@@ -3,31 +3,27 @@ $( document ).ready( function()
 {        
   $( "li.menuDropDownItem" ).mouseover( function() // Over dropdown item
   {
-    $( this ).css( "background-color", "#5a845d" );
-    $( this ).css( "border", "1px solid #D0D0D0" );
+    $( this ).css( "background-color", "#44b06a" );
   });
   
   $( "li.menuDropDownItem" ).mouseout( function() // Out dropdown item
   {
-    $( this ).css( "background-color", "#405f43" );
-    $( this ).css( "border", "1px solid #405f43" );
+    $( this ).css( "background-color", "#278f43" );
   });
   
   $( "li.introItem" ).mouseover( function() // Over intro item
   {
     $( this ).css( "background-color", "#a4d2a3" );
-    $( this ).css( "border", "1px solid #ffffff" );
   });
   
   $( "li.introItem" ).mouseout( function() // Out intro item
   {
     $( this ).css( "background-color", "#d5efd5" );
-    $( this ).css( "border", "1px solid #d5efd5" );
   });
 });
 
 // Called from main/Leftbar
 function setMainPageNormal()
 {
-	document.getElementById( 'mainPage' ).style.marginLeft = '300px';
+	document.getElementById( 'mainPage' ).style.marginLeft = '270px';
 }
