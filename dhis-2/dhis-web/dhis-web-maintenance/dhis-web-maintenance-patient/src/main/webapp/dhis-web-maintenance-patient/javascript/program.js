982c78bb6d7c914652330a6d219c8f39f9503772
Tran Chau <tran.hispvietnam@gmail.com>
2012-02-24 09:10:12 +0700

    revno: 6096 message:   (patient) Use jQuery methods insteads of Request object in dhis-web-maintenance-patient.
    committer: Tran Chau <tran.hispvietnam@gmail.com>
    branch nick: dhis2
    timestamp: Fri 2012-02-24 09:10:12 +0700

--- dhis-2/dhis-web/dhis-web-maintenance/dhis-web-maintenance-patient/src/main/webapp/dhis-web-maintenance-patient/javascript/program.js
+++ dhis-2/dhis-web/dhis-web-maintenance/dhis-web-maintenance-patient/src/main/webapp/dhis-web-maintenance-patient/javascript/program.js
@@ -37,46 +37,6 @@ function removeProgram( programId, name )
 	removeItem( programId, name, i18n_confirm_delete, 'removeProgram.action' );
 }
 
-// -----------------------------------------------------------------------------
-// Create validation for dataelements into program-stages
-// -----------------------------------------------------------------------------
-
-var dataelementId;
-function getDataElementsFromStage(stageId, dataelementId){
-
-	this.dataelementId = dataelementId;
-	clearListById(dataelementId);
-	
-	if(stageId != ''){
-		var request = new Request();
-		request.setResponseTypeXML( 'xmlObject' );
-		request.setCallbackSuccess( getDataElementsFromStageReceived);
-		request.send( "getProgramStage.action?id=" + stageId );
-	}	
-}
-
-function getDataElementsFromStageReceived(xmlObject){
-	
-	var dataelementField = byId(dataelementId);
-	clearListById(dataelementId);
-	
-	xmlObject = xmlObject.getElementsByTagName('dataElements')[0];
-	var dataelementList = xmlObject.getElementsByTagName( 'dataElement' );
-  
-	for ( var i = 0; i < dataelementList.length; i++ )
-    {
-        var id = dataelementList[ i ].getElementsByTagName("id")[0].firstChild.nodeValue;
-        var name = dataelementList[ i ].getElementsByTagName("name")[0].firstChild.nodeValue;
-
-        var option = document.createElement("option");
-        option.value = id;
-        option.text = name;
-        option.title = name;
-        
-        dataelementField.add(option, null);       	
-    }
-}
-
 function singleEventOnChange()
 {
 	var checked = byId('singleEvent').checked;
