38416f577bb10bd0f2d8236677d6aab0c2bf0765
Lars Helge Overland <larshelge@gmail.com>
2012-02-26 19:28:52 +0100

    revno: 6112 message:   Script update
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Sun 2012-02-26 19:28:52 +0100

--- resources/sql/div.sql
+++ resources/sql/div.sql
@@ -46,3 +46,14 @@ select cc.categorycomboid, cc.name as categorycomboname, cn.* from _categoryopti
 join categorycombos_optioncombos co using(categoryoptioncomboid)
 join categorycombo cc using(categorycomboid)
 order by categorycomboname, categoryoptioncomboname;
+
+-- Groups orgunits into groups based on the text match in the where clause for the orgunit group with the given id
+
+insert into orgunitgroupmembers(orgunitgroupid,organisationunitid)
+select 22755 as orgunitgroupid,ou.organisationunitid as organisationunitid from organisationunit ou 
+where lower(name) like '%dispensary%'
+and not exists (
+select orgunitgroupid from orgunitgroupmembers om 
+where ou.organisationunitid=om.organisationunitid
+and om.orgunitgroupid=22755);
+
