3f5932cadae1e1b086e44f1813932239ffe6fccd
Lars Helge Overland <larshelge@gmail.com>
2012-03-20 16:25:02 +0100

    revno: 6319 message:   Improved identifiableObjectManager
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Tue 2012-03-20 16:25:02 +0100

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/common/DefaultIdentifiableObjectManager.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/common/DefaultIdentifiableObjectManager.java
@@ -1,10 +1,45 @@
 package org.hisp.dhis.common;
 
+/*
+ * Copyright (c) 2004-2005, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * * Neither the name of the <ORGANIZATION> nor the names of its contributors may
+ *   be used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * @author Lars Helge Overland
+ */
+@Transactional
 public class DefaultIdentifiableObjectManager
     implements IdentifiableObjectManager
 {
@@ -16,7 +51,39 @@ public class DefaultIdentifiableObjectManager
         this.objectStores = objectStores;
     }
 
-    @Transactional
+    private Map<Class<IdentifiableObject>, GenericIdentifiableObjectStore<IdentifiableObject>> objectStoreMap;
+    
+    @PostConstruct
+    public void init()
+    {
+        objectStoreMap = new HashMap<Class<IdentifiableObject>, GenericIdentifiableObjectStore<IdentifiableObject>>();
+        
+        for ( GenericIdentifiableObjectStore<IdentifiableObject> store : objectStores )
+        {
+            objectStoreMap.put( store.getClazz(), store );
+        }
+    }
+
+    public void save( IdentifiableObject object )
+    {
+        objectStoreMap.get( object.getClass() ).save( object );
+    }
+
+    public void update( IdentifiableObject object )
+    {
+        objectStoreMap.get( object.getClass() ).update( object );
+    }
+
+    public void get( Class<IdentifiableObject> clazz, String uid )
+    {
+        objectStoreMap.get( clazz ).getByUid( uid );
+    }
+
+    public void delete( IdentifiableObject object )
+    {
+        objectStoreMap.get( object.getClass() ).delete( object );
+    }
+    
     public IdentifiableObject getObject( String uid, String simpleClassName )
     {
         for ( GenericIdentifiableObjectStore<IdentifiableObject> objectStore : objectStores )
@@ -30,7 +97,6 @@ public class DefaultIdentifiableObjectManager
         return null;
     }
     
-    @Transactional
     public IdentifiableObject getObject( int id, String simpleClassName )
     {
         for ( GenericIdentifiableObjectStore<IdentifiableObject> objectStore : objectStores )
