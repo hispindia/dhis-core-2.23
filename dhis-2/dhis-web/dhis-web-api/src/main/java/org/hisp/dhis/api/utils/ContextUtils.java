39941884375bcdfa4020a6b35e4ea51b9ac0710c
Lars Helge Overland <larshelge@gmail.com>
2012-04-08 20:10:08 +0200

    revno: 6525 message:   Added public to cache-control header, necessary when authorization header is present in request
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Sun 2012-04-08 20:10:08 +0200

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/api/utils/ContextUtils.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/api/utils/ContextUtils.java
@@ -103,7 +103,7 @@ public class ContextUtils
         }
         else if ( cacheStrategy.equals( CacheStrategy.CACHE_6AM_TOMORROW ) )
         {
-            response.setHeader( HEADER_CACHE_CONTROL, "max-age=" + DateUtils.getSecondsUntilTomorrow( 6 ) );
+            response.setHeader( HEADER_CACHE_CONTROL, "public, max-age=" + DateUtils.getSecondsUntilTomorrow( 6 ) );
             response.setHeader( HEADER_EXPIRES, DateUtils.getHttpDateString( DateUtils.getDateForTomorrow( 6 ) ) );
         }
         else if ( cacheStrategy.equals( CacheStrategy.CACHE_TWO_WEEKS ) )
@@ -111,7 +111,7 @@ public class ContextUtils
             Calendar cal = Calendar.getInstance();
             cal.add( Calendar.DAY_OF_YEAR, 14 );
             
-            response.setHeader( HEADER_CACHE_CONTROL, "max-age=1209600" );
+            response.setHeader( HEADER_CACHE_CONTROL, "public, max-age=1209600" );
             response.setHeader( HEADER_EXPIRES, DateUtils.getHttpDateString( cal.getTime() ) );
         }
 
