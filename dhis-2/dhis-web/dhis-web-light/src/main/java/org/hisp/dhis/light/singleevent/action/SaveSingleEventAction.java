ed7f56c8f0a0cff1f1603c6574c3736c489ceb0b
Long <thanhlongngo1988@yahoo.com>
2012-05-03 16:53:40 +0700

    revno: 6842 message:   [mobile] add Single Event for mobile browser + minor fix
    committer: Long <thanhlongngo1988@yahoo.com>
    branch nick: dhis2
    timestamp: Thu 2012-05-03 16:53:40 +0700

---
+++ dhis-2/dhis-web/dhis-web-light/src/main/java/org/hisp/dhis/light/singleevent/action/SaveSingleEventAction.java
@@ -0,0 +1,317 @@
+/*
+ * Copyright (c) 2004-2012, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * * Neither the name of the HISP project nor the names of its contributors may
+ *   be used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.hisp.dhis.light.singleevent.action;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.hisp.dhis.dataelement.DataElement;
+import org.hisp.dhis.light.utils.NamebasedUtils;
+import org.hisp.dhis.organisationunit.OrganisationUnit;
+import org.hisp.dhis.organisationunit.OrganisationUnitService;
+import org.hisp.dhis.patient.Patient;
+import org.hisp.dhis.patient.PatientService;
+import org.hisp.dhis.patientdatavalue.PatientDataValue;
+import org.hisp.dhis.patientdatavalue.PatientDataValueService;
+import org.hisp.dhis.program.Program;
+import org.hisp.dhis.program.ProgramInstance;
+import org.hisp.dhis.program.ProgramInstanceService;
+import org.hisp.dhis.program.ProgramService;
+import org.hisp.dhis.program.ProgramStage;
+import org.hisp.dhis.program.ProgramStageDataElement;
+import org.hisp.dhis.program.ProgramStageInstance;
+import org.hisp.dhis.program.ProgramStageInstanceService;
+import com.opensymphony.xwork2.Action;
+
+public class SaveSingleEventAction
+    implements Action
+{
+    // -------------------------------------------------------------------------
+    // Dependencies
+    // -------------------------------------------------------------------------
+
+    private ProgramInstanceService programInstanceService;
+
+    public void setProgramInstanceService( ProgramInstanceService programInstanceService )
+    {
+        this.programInstanceService = programInstanceService;
+    }
+
+    private ProgramStageInstanceService programStageInstanceService;
+
+    public void setProgramStageInstanceService( ProgramStageInstanceService programStageInstanceService )
+    {
+        this.programStageInstanceService = programStageInstanceService;
+    }
+
+    private ProgramService programService;
+
+    public void setProgramService( ProgramService programService )
+    {
+        this.programService = programService;
+    }
+
+    private PatientService patientService;
+
+    public void setPatientService( PatientService patientService )
+    {
+        this.patientService = patientService;
+    }
+
+    private PatientDataValueService patientDataValueService;
+
+    public void setPatientDataValueService( PatientDataValueService patientDataValueService )
+    {
+        this.patientDataValueService = patientDataValueService;
+    }
+
+    private OrganisationUnitService organisationUnitService;
+
+    public void setOrganisationUnitService( OrganisationUnitService organisationUnitService )
+    {
+        this.organisationUnitService = organisationUnitService;
+    }
+
+    private NamebasedUtils util;
+
+    public NamebasedUtils getUtil()
+    {
+        return util;
+    }
+
+    public void setUtil( NamebasedUtils util )
+    {
+        this.util = util;
+    }
+
+    // -------------------------------------------------------------------------
+    // Input Output
+    // -------------------------------------------------------------------------
+
+    private Map<String, String> typeViolations = new HashMap<String, String>();
+
+    public Map<String, String> getTypeViolations()
+    {
+        return typeViolations;
+    }
+
+    private Integer programId;
+
+    public Integer getProgramId()
+    {
+        return programId;
+    }
+
+    public void setProgramId( Integer programId )
+    {
+        this.programId = programId;
+    }
+
+    private Integer beneficiaryId;
+
+    public Integer getBeneficiaryId()
+    {
+        return beneficiaryId;
+    }
+
+    public void setBeneficiaryId( Integer beneficiaryId )
+    {
+        this.beneficiaryId = beneficiaryId;
+    }
+
+    private Patient patient;
+
+    public Patient getPatient()
+    {
+        return patient;
+    }
+
+    private String eventName;
+
+    public String getEventName()
+    {
+        return this.eventName;
+    }
+
+    private Integer organisationUnitId;
+
+    public void setOrganisationUnitId( Integer organisationUnitId )
+    {
+        this.organisationUnitId = organisationUnitId;
+    }
+
+    public Integer getOrganisationUnitId()
+    {
+        return this.organisationUnitId;
+    }
+
+    private boolean update;
+
+    public void setUpdate( boolean update )
+    {
+        this.update = update;
+    }
+
+    public boolean getUpdate()
+    {
+        return this.update;
+    }
+
+    private Integer instId;
+
+    public void setInstId( Integer instId )
+    {
+        this.instId = instId;
+    }
+
+    public Integer getInstId()
+    {
+        return this.instId;
+    }
+
+    private List<String> dynForm = new ArrayList<String>();
+
+    public void setDynForm( List<String> dynForm )
+    {
+        this.dynForm = dynForm;
+    }
+
+    public List<String> getDynForm()
+    {
+        return dynForm;
+    }
+
+    private String resultString;
+
+    public void setResultString( String resultString )
+    {
+        this.resultString = resultString;
+    }
+
+    public String getResultString()
+    {
+        return this.resultString;
+    }
+
+    private ArrayList<ProgramStageDataElement> programStageDataElements = new ArrayList<ProgramStageDataElement>();
+
+    public ArrayList<ProgramStageDataElement> getProgramStageDataElements()
+    {
+        return this.programStageDataElements;
+    }
+
+    static final Comparator<ProgramStageDataElement> OrderBySortOrder = new Comparator<ProgramStageDataElement>()
+    {
+        public int compare( ProgramStageDataElement i1, ProgramStageDataElement i2 )
+        {
+            return i1.getSortOrder().compareTo( i2.getSortOrder() );
+        }
+    };
+
+    @Override
+    public String execute()
+        throws Exception
+    {
+        Program program = programService.getProgram( programId );
+        eventName = program.getName();
+
+        Patient patient = patientService.getPatient( beneficiaryId );
+        ProgramStage programStage = program.getProgramStages().iterator().next();
+        OrganisationUnit organisationUnit = organisationUnitService.getOrganisationUnit( organisationUnitId );
+
+        programStageDataElements = new ArrayList<ProgramStageDataElement>( programStage.getProgramStageDataElements() );
+        Collections.sort( programStageDataElements, OrderBySortOrder );
+
+        // -------------------------------------------------------------------------
+        // Validation
+        // -------------------------------------------------------------------------
+
+        int i = 0;
+        for ( ProgramStageDataElement programStageDataElement : programStageDataElements )
+        {
+            DataElement dataElement = programStageDataElement.getDataElement();
+            String value = dynForm.get( i ).trim();
+            Boolean valueIsEmpty = (value == null || value.length() == 0);
+
+            if ( !valueIsEmpty )
+            {
+                String typeViolation = util.getTypeViolation( dataElement, value );
+
+                if ( typeViolation != null )
+                {
+                    typeViolations.put( String.valueOf( dataElement.getId() ), typeViolation );
+                }
+            }
+
+            i++;
+        }
+        
+        if ( !typeViolations.isEmpty() )
+        {
+            return ERROR;
+        }
+
+        ProgramInstance programInstance = new ProgramInstance();
+        programInstance.setEnrollmentDate( new Date() );
+        programInstance.setDateOfIncident( new Date() );
+        programInstance.setProgram( program );
+        programInstance.setPatient( patient );
+        programInstance.setCompleted( false );
+        programInstanceService.addProgramInstance( programInstance );
+
+        ProgramStageInstance programStageInstance = new ProgramStageInstance();
+        programStageInstance.setOrganisationUnit( organisationUnit );
+        programStageInstance.setProgramInstance( programInstance );
+        programStageInstance.setProgramStage( programStage );
+        programStageInstance.setDueDate( new Date() );
+        programStageInstance.setExecutionDate( new Date() );
+        programStageInstance.setCompleted( false );
+        programStageInstanceService.addProgramStageInstance( programStageInstance );
+
+        i = 0;
+        for ( ProgramStageDataElement programStageDataElement : programStageDataElements )
+        {
+            DataElement dataElement = programStageDataElement.getDataElement();
+
+            PatientDataValue patientDataValue = new PatientDataValue();
+            patientDataValue.setDataElement( dataElement );
+            patientDataValue.setProgramStageInstance( programStageInstance );
+            patientDataValue.setValue( dynForm.get( i ).trim() );
+            patientDataValue.setProgramStageInstance( programStageInstance );
+            patientDataValueService.savePatientDataValue( patientDataValue );
+            i++;
+        }
+
+        return SUCCESS;
+    }
+
+}
