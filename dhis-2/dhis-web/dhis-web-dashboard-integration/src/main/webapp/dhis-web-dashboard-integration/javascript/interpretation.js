5a3dc3b55a26e47330c74e58b78899e783daed89
Lars Helge Overland <larshelge@gmail.com>
2012-05-29 21:30:37 +0200

    revno: 7084 message:   Impl auto grow for message reply area
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Tue 2012-05-29 21:30:37 +0200

--- dhis-2/dhis-web/dhis-web-dashboard-integration/src/main/webapp/dhis-web-dashboard-integration/javascript/interpretation.js
+++ dhis-2/dhis-web/dhis-web-dashboard-integration/src/main/webapp/dhis-web-dashboard-integration/javascript/interpretation.js
@@ -5,12 +5,13 @@ $( document ).ready( function() {
 
 function showUserInfo( id )
 {
-	$( "#userInfo" ).load( "../dhis-web-commons-ajax-html/getUser.action?id=" + id );
-	$( "#userInfo" ).dialog( {
-		modal : true,
-		width : 350,
-		height : 350,
-		title : "User"
+	$( "#userInfo" ).load( "../dhis-web-commons-ajax-html/getUser.action?id=" + id, function() {
+		$( "#userInfo" ).dialog( {
+			modal : true,
+			width : 350,
+			height : 350,
+			title : "User"
+		} );
 	} );
 }
 
