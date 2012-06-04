35869a9aa64fb11fd2cd61dda2caf1cc94d4c8a3
Lars Helge Overland <larshelge@gmail.com>
2012-06-04 14:06:05 +0200

    revno: 7174 message:   Impl sharing of charts straight from dashboard
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Mon 2012-06-04 14:06:05 +0200

--- dhis-2/dhis-web/dhis-web-dashboard-integration/src/main/webapp/dhis-web-dashboard-integration/javascript/dropdown.js
+++ dhis-2/dhis-web/dhis-web-dashboard-integration/src/main/webapp/dhis-web-dashboard-integration/javascript/dropdown.js
@@ -1,6 +1,8 @@
 
 $( document ).ready( function()
 {
+	$( "#interpretationArea" ).autogrow();
+	
 	var viewportWidth = parseInt( $( window ).width() );
 	var linkWidth = parseInt( 338 );
 	var chartWidth = parseInt( 325 );
@@ -55,4 +57,42 @@ function viewChart( url )
 function explore( uid )
 {
 	window.location.href = "../dhis-web-visualizer/app/index.html?id=" + uid;
+}
+
+function viewShareForm( uid )
+{	
+	$( "#interpretationChartId" ).val( uid );
+	
+	$( "#shareForm" ).dialog( {
+		modal : true,
+		width : 500,
+		resizable: false,
+		title : i18n_share_your_interpretation
+	} );
+}
+
+function shareInterpretation()
+{
+    var chartId = $( "#interpretationChartId" ).val();
+    var text = $( "#interpretationArea" ).val();
+    
+    if ( text.length && $.trim( text ).length )
+    {
+    	text = $.trim( text );
+    	
+	    var url = "../api/interpretations/chart/" + chartId;
+	    
+	    // TODO url += ( ou && ou.length ) ? "?ou=" + ou : "";
+	    
+	    $.ajax( url, {
+	    	type: "POST",
+	    	contentType: "text/html",
+	    	data: text,
+	    	success: function() {
+	    		$( "#shareForm" ).dialog( "close" );
+	    		$( "#interpretationArea" ).val( "" );
+	    		setHeaderDelayMessage( i18n_interpretation_was_shared );
+	    	}    	
+	    } );
+    }
 }
