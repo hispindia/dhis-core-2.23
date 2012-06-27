528cac0f3d07e4fc19cd9a02f0e9859f1aee4806
Morten Olav Hansen <mortenoh@gmail.com>
2012-06-27 16:28:16 +0300

    revno: 7432 message:   changed from lastApproved to lastDays in mfl routes
    committer: Morten Olav Hansen <mortenoh@gmail.com>
    branch nick: dhis2
    timestamp: Wed 2012-06-27 16:28:16 +0300

--- local/ke/mfl2dxf2/src/main/java/org/hisp/dhis/integration/routes/MFLTransformRoute.java
+++ local/ke/mfl2dxf2/src/main/java/org/hisp/dhis/integration/routes/MFLTransformRoute.java
@@ -42,7 +42,7 @@ public class MFLTransformRoute
     {
         // from( "file:/Users/mortenoh/GDrive/HISP/ke/mfl/data/inbox?consumer.initialDelay=10000&consumer.delay=5000" ).
         from( "quartz://every1minute?cron=0+0/1+*+*+*+%3F" ).
-            to( "http://api.ehealth.or.ke/api/facilities?lastApproved=1/1/2011&paging=off&scheme=on" ).
+            to( "http://api.ehealth.or.ke/api/facilities?lastDays=720&paging=off&scheme=code" ).
             to( "xslt:transform/mfl2dxf2.xsl" ).
             convertBodyTo( InputStream.class ).inOut( "dhis2:metadata" ).
             to( "log:org.hisp.dhis.camel?level=INFO" );
