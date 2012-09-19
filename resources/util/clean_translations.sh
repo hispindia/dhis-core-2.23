47890f2e93b461fc60b0c2bfc58b558091974a31
Jason P. Pickering <jason.p.pickering@gmail.com>
2012-09-19 14:53:42 +0000

    revno: 8140 message:   (Translations) Minor adjustment to cleaning script.
    committer: Jason P. Pickering <jason.p.pickering@gmail.com>
    branch nick: dhis2
    timestamp: Wed 2012-09-19 14:53:42 +0000

--- resources/util/clean_translations.sh
+++ resources/util/clean_translations.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-
 isNotSet() {
     if [[ ! ${!1} && ${!1-_} ]]
     then
         return 1
     fi
 }
-
 
 
 if [[ $# -lt 1 ]]; then
@@ -18,24 +16,37 @@ declare -A array1
 declare -A array2
 PROP_FILE=$2
 
+grep -Ev '^#' $1 | sed '/^\r/d' | grep -Ev '^$'| sort > tmpfile1
+grep -Ev '^#' $2 | sed '/^\r/d' | grep -Ev '^$'| sort > tmpfile2
+
 while IFS='=' read -r key val; do
         [[ $key = '#'* ]] && continue
-        array1["$key"]="$val"       
-done < $1
+        array1["$key"]="$val"
+done < tmpfile1
 
 
 while IFS='=' read -r key val; do
         [[ $key = '#'* ]] && continue
         array2["$key"]="$val"
-done < $2
+done < tmpfile2
 
-echo "" > $2
+echo -n "" > $2
 
 for key in "${!array1[@]}"; do
 
 isNotSet array2[${key}]
-if [ $? -ne 1 ]
-then
-        echo "$key=${array2[$key]}" >> ${PROP_FILE};
+
+if [ $? -ne 1  ];then
+
+value2=${array2[$key]}
+value1=${array1[$key]}
+
+if [[ *"$value1"* != *"$value2"* ]];then
+echo "$key=${array2[$key]}" >> ${PROP_FILE};
+fi
 fi
 done
+
+rm tmpfile1
+rm tmpfile2
+
