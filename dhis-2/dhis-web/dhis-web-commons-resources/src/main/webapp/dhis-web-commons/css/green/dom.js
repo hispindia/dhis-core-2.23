a363e17236f56bb5552627fbf9355f5506cc8237
Lars Helge Øverland <larshelge@gmail.com>
2012-10-12 18:21:05 +0200

    revno: 8513 message:   Replaced js for intro item hover effect with css
    committer: Lars Helge Øverland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Fri 2012-10-12 18:21:05 +0200

--- dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/css/green/dom.js
+++ dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/css/green/dom.js
@@ -1,17 +1,4 @@
 
-$( document ).ready( function()
-{
-  $( "li.introItem" ).mouseover( function() // Over intro item
-  {
-    $( this ).css( "background-color", "#9dc69c" );
-  });
-  
-  $( "li.introItem" ).mouseout( function() // Out intro item
-  {
-    $( this ).css( "background-color", "#d3e5d3" );
-  });
-});
-
 // Called from main/Leftbar
 function setMainPageNormal()
 {
--- dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/css/vietnam/dom.js
+++ dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/css/vietnam/dom.js
@@ -1,17 +1,4 @@
 
-$( document ).ready( function()
-{
-  $( "li.introItem" ).mouseover( function() // Over intro item
-  {
-    $( this ).css( "background-color", "#a4d2a3" );
-  });
-  
-  $( "li.introItem" ).mouseout( function() // Out intro item
-  {
-    $( this ).css( "background-color", "#d5efd5" );
-  });
-});
-
 // Called from main/Leftbar
 function setMainPageNormal()
 {
