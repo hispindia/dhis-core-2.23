651dfc0071e12744af52b76eaa9cb043d572e64f
Tran Chau <tran.hispvietnam@gmail.com>
2012-10-18 14:47:25 +0700

    revno: 8572 message:   Display provided-elsewhere checkboxes in custom entry form for completed event.
    committer: Tran Chau <tran.hispvietnam@gmail.com>
    branch nick: dhis2
    timestamp: Thu 2012-10-18 14:47:25 +0700

--- dhis-2/dhis-services/dhis-service-patient/src/main/java/org/hisp/dhis/program/DefaultProgramDataEntryService.java
+++ dhis-2/dhis-services/dhis-service-patient/src/main/java/org/hisp/dhis/program/DefaultProgramDataEntryService.java
@@ -277,7 +277,7 @@ public class DefaultProgramDataEntryService
                         inputHTML += jQueryCalendar;
                     }
 
-                    if ( !programStageInstance.isCompleted() && allowProvidedElsewhere )
+                    if ( allowProvidedElsewhere )
                     {
                         // Add ProvidedByOtherFacility checkbox
                         inputHTML = addProvidedElsewherCheckbox( inputHTML, patientDataValue, programStage );
