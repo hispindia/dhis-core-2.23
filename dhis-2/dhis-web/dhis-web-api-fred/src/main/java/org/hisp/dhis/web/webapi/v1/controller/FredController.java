38609dac86ba61fa1556ff57c892e33e848858d2
Morten Olav Hansen <mortenoh@gmail.com>
2012-12-28 12:28:02 +0100

    revno: 9411 message:   moved bootstrap to dhis-web-commons-resources
    committer: Morten Olav Hansen <mortenoh@gmail.com>
    branch nick: dhis2
    timestamp: Fri 2012-12-28 12:28:02 +0100

--- dhis-2/dhis-web/dhis-web-api-fred/src/main/java/org/hisp/dhis/web/webapi/v1/controller/FredController.java
+++ dhis-2/dhis-web/dhis-web-api-fred/src/main/java/org/hisp/dhis/web/webapi/v1/controller/FredController.java
@@ -27,6 +27,7 @@ package org.hisp.dhis.web.webapi.v1.controller;
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import org.hisp.dhis.web.webapi.v1.utils.ContextUtils;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -34,21 +35,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 /**
  * @author Morten Olav Hansen <mortenoh@gmail.com>
  */
-@Controller( value = "fred-controller-" + FredController.PREFIX )
-@RequestMapping( value = FredController.PREFIX )
-@PreAuthorize( "hasRole('M_dhis-web-api-fred') or hasRole('ALL')" )
+@Controller(value = "fred-controller-" + FredController.PREFIX)
+@RequestMapping(value = FredController.PREFIX)
+@PreAuthorize("hasRole('M_dhis-web-api-fred') or hasRole('ALL')")
 public class FredController
 {
     public static final String PREFIX = "v1";
 
-    @RequestMapping( value = "", method = RequestMethod.GET, produces = { MediaType.TEXT_HTML_VALUE, MediaType.TEXT_XML_VALUE } )
-    public String home( Model model )
+    @RequestMapping(value = "", method = RequestMethod.GET, produces = { MediaType.TEXT_HTML_VALUE, MediaType.TEXT_XML_VALUE })
+    public String home( Model model, HttpServletRequest request )
     {
+        ContextUtils.populateContextPath( model, request );
+
         model.addAttribute( "baseUrl", linkTo( FredController.class ).toString() );
         model.addAttribute( "pageName", "home" );
         model.addAttribute( "page", FredController.PREFIX + "/index.vm" );
