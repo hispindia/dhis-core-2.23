96bb968ccd39fdd25b265042eb07cd8170f275c6
Long <Long@Long-Laptop>
2013-03-13 13:43:01 +0700

    revno: 10162 message:   [mobile] improve J2MEDataEntryParser
    committer: Long <Long@Long-Laptop>
    branch nick: dhis2
    timestamp: Wed 2013-03-13 13:43:01 +0700

--- dhis-2/dhis-services/dhis-service-sms/src/main/java/org/hisp/dhis/sms/parse/J2MEDataEntryParser.java
+++ dhis-2/dhis-services/dhis-service-sms/src/main/java/org/hisp/dhis/sms/parse/J2MEDataEntryParser.java
@@ -1,24 +1,53 @@
 package org.hisp.dhis.sms.parse;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
+
+import org.hisp.dhis.smscommand.SMSCommand;
 
 public class J2MEDataEntryParser
     implements IParser
 {
+    private SMSCommand smsCommand;
 
     @Override
     public Map<String, String> parse( String sms )
     {
-        String keyValueSection = sms.split( "" )[1];
-        
-        return null;
+        String[] keyValuePairs = null;
+        if ( sms.indexOf( "#" ) > -1 )
+        {
+            keyValuePairs = sms.split( "#" );
+        }
+        else
+        {
+            keyValuePairs = new String[1];
+            keyValuePairs[0] = sms;
+        }
+
+        Map<String, String> keyValueMap = new HashMap<String, String>();
+        for ( String keyValuePair : keyValuePairs )
+        {
+            String[] token = keyValuePair.split( Pattern.quote( smsCommand.getSeparator() ) );
+            keyValueMap.put( token[0], token[1] );
+        }
+
+        return keyValueMap;
     }
 
     @Override
     public void setSeparator( String separator )
     {
         // TODO Auto-generated method stub
-
     }
 
+    public SMSCommand getSmsCommand()
+    {
+        return smsCommand;
+    }
+
+    public void setSmsCommand( SMSCommand smsCommand )
+    {
+        this.smsCommand = smsCommand;
+    }
 }
