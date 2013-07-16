3b0293b88a4a9a8c54c06fb79309c3f84887ecf9
Lars Helge Øverland <larshelge@gmail.com>
2013-07-16 13:57:30 +0200

    revno: 11409 message:   Mobile, removed outdated bean definition
    committer: Lars Helge Øverland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Tue 2013-07-16 13:57:30 +0200

---
+++ dhis-2/dhis-services/dhis-service-mobile/src/test/java/org/hisp/dhis/mobile/service/ActivityReportingServiceTest.java
@@ -0,0 +1,19 @@
+package org.hisp.dhis.mobile.service;
+
+import org.hisp.dhis.DhisSpringTest;
+import org.hisp.dhis.api.mobile.ActivityReportingService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ActivityReportingServiceTest
+    extends DhisSpringTest
+{
+    @Autowired
+    private ActivityReportingService activityReportingService;
+
+    @Test
+    public void test()
+    {
+        //TODO
+    }
+}
