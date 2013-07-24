60cdb3cdde4cae2398ed38942aaa03169a5d439f
Lars Helge Øverland <larshelge@gmail.com>
2013-07-24 17:58:43 +0200

    revno: 11504 message:   Impl new dashboard. Supports multiple dashboards per user. Lets you insert dashboard items directly from search field. Supports maps in dashboard.
    committer: Lars Helge Øverland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Wed 2013-07-24 17:58:43 +0200

--- dhis-2/dhis-web/dhis-web-dashboard-integration/src/main/webapp/dhis-web-dashboard-integration/javascript/dashboard.js
+++ dhis-2/dhis-web/dhis-web-dashboard-integration/src/main/webapp/dhis-web-dashboard-integration/javascript/dashboard.js
@@ -1,9 +1,15 @@
 
+dhis2.util.namespace( 'dhis2.db' );
+
+dhis2.db.current;
+dhis2.db.currentShareType;
+dhis2.db.currentShareId;
+
 $( document ).ready( function()
 {
 	$( "#interpretationArea" ).autogrow();
 	
-	$( document ).click( hideSearch );
+	$( document ).click( dhis2.db.hideSearch );
 
 	$( "#searchField" ).focus( function() {
 		$( "#searchDiv" ).css( "border-color", "#999" );
@@ -12,68 +18,389 @@ $( document ).ready( function()
 	} );
 
 	$( "#searchField" ).focus();
-	$( "#searchField" ).keyup( search );
+	$( "#searchField" ).keyup( dhis2.db.search );
 	
-	var viewportWidth = parseInt( $( window ).width() );
-	var linkWidth = parseInt( 334 );
-	var chartWidth = parseInt( 325 );
-	
-	if ( viewportWidth == undefined )
-	{
-		viewportWidth = parseInt( 1366 );
-	}
-	
-	var noCharts = 2 * Math.floor( ( viewportWidth - linkWidth + 4 ) / chartWidth ); 
-		
-	$( "#contentDiv" ).load( "provideContent.action?noCharts=" + noCharts + "&_dc=" + getRandomNumber() );
+	dhis2.db.renderDashboardListLoadFirst();
 } );
 
-function setAreaItem( area, item )
+//------------------------------------------------------------------------------
+// Dashboard
+//------------------------------------------------------------------------------
+
+dhis2.db.tmpl = {
+	openAddLink: "<li><a href='javascript:dhis2.db.openAddDashboardForm()'>Add new</a></li><li>" +
+	             "<a href='javascript:dhis2.db.openManageDashboardForm()'>Manage</a></li>",
+	
+	dashboardLink: "<li id='dashboard-${id}'><a href='javascript:dhis2.db.renderDashboard( \"${id}\" )'>${name}</a></li>",
+	
+	moduleIntro: "<li><div class='dasboardIntro'>Click Add new to get started</div></li>",
+	
+	dashboardIntro: "<li><div class='dasboardIntro'>Add stuff by searching from the search field above</div></li>",
+	
+	hitHeader: "<li class='hitHeader'>${title}</li>",
+	
+	hitItem: "<li><a class='viewLink' href='${link}'><img src='../images/${img}.png'>${name}</a>" +
+	         "<a class='addLink' href='javascript:dhis2.db.addItemContent( \"${type}\", \"${id}\" )'>Add</a></li>",
+		         
+	chartItem: "<li><div class='itemHeader'><a href='javascript:dhis2.db.removeItem( \"${itemId}\" )'>Remove</a>" +
+	           "<a href='javascript:dhis2.db.viewImage( \"../api/charts/${id}/data?width=820&height=550\", \"${name}\" )'>View full size</a>" +
+	           "<a href='javascript:dhis2.db.viewShareForm( \"${id}\", \"chart\", \"${name}\" )'>Share</a></div>" +
+	           "<div class='item'><img src='../api/charts/${id}/data?width=405&height=295' onclick='dhis2.db.exploreChart( \"${id}\" )' title='Click to explore'></div></li>",
+	           
+	mapItem: "<li><div class='itemHeader'><a href='javascript:dhis2.db.removeItem( \"${itemId}\" )'>Remove</a>" +
+	         "<a href='javascript:dhis2.db.viewImage( \"../api/maps/${id}/data?width=690\", \"${name}\" )'>View full size</a>" +
+	         "<a href='javascript:dhis2.db.viewShareForm( \"${id}\", \"map\", \"${name}\" )'>Share</a></div>" +
+		     "<div class='item'><img src='../api/maps/${id}/data?width=405' onclick='dhis2.db.exploreMap( \"${id}\" )' title='Click to explore'></div></li>"
+};
+
+dhis2.db.openAddDashboardForm = function()
 {
-    $.get( "setAreaItem.action", {
-        'area' : area,
-        'item' : item
-    }, function()
-    {
-        window.location.href = "index.action";
+	$( "#addDashboardForm" ).dialog( {
+		autoOpen: true,
+		modal: true,
+		width: 405,
+		height: 100,
+		resizable: false,
+		title: "Add new dashboard"
+	} );
+}
+
+dhis2.db.openManageDashboardForm = function()
+{
+	if ( undefined !== dhis2.db.current )
+	{
+		$.getJSON( "../api/dashboards/" + dhis2.db.current, function( data )
+		{
+			var name = data.name;
+			$( "#dashboardRename" ).val( name );
+
+			$( "#manageDashboardForm" ).dialog( {
+				autoOpen: true,
+				modal: true,
+				width: 405,
+				height: 275,
+				resizable: false,
+				title: name
+			} );
+		} );
+	}
+}
+
+dhis2.db.addDashboard = function()
+{
+	var item = '{"name": "' + $( "#dashboardName" ).val() + '"}';
+	
+	$.ajax( {
+		type: "post",
+		url: "../api/dashboards",
+		data: item,
+		contentType: "application/json",
+		success: function( data, text, xhr ) {
+			$( "#addDashboardForm" ).dialog( "destroy" );
+			$( "#dashboardName" ).val( "" );
+			dhis2.db.renderDashboardListLoadFirst();
+		}
+	} );
+}
+
+dhis2.db.renameDashboard = function()
+{
+	var name = $( "#dashboardRename" ).val();
+	
+	if ( undefined !== dhis2.db.current && undefined !== name && name.trim().length > 0 )
+	{
+		var data = "{ \"name\": \"" + name + "\"}";
+		
+		$.ajax( {
+	    	type: "put",
+	    	url: "../api/dashboards/" + dhis2.db.current,
+	    	contentType: "application/json",
+	    	data: data,
+	    	success: function() {
+	    		$( "#manageDashboardForm" ).dialog( "destroy" );
+	    		$( "#dashboardRename" ).val( "" );
+	    		dhis2.db.renderDashboardListLoadFirst();
+	    	}
+	    } );
+	}
+}
+
+dhis2.db.removeDashboard = function()
+{
+	if ( undefined !== dhis2.db.current )
+	{
+		$.ajax( {
+			type: "delete",
+			url: "../api/dashboards/" + dhis2.db.current,
+	    	success: function() {
+	    		$( "#manageDashboardForm" ).dialog( "destroy" );
+	    		dhis2.db.renderDashboardListLoadFirst();
+	    	}
+		} );
+	}
+}
+
+dhis2.db.renderDashboardListLoadFirst = function()
+{
+	var $l = $( "#dashboardList" );
+	
+	$l.empty().append( dhis2.db.tmpl.openAddLink );
+	
+	$.getJSON( "../api/dashboards.json?paging=false&links=false", function( data )
+	{
+		if ( undefined !== data.dashboards )
+		{
+			var first;
+			
+			$.each( data.dashboards, function( index, dashboard )
+			{
+				$l.append( $.tmpl( dhis2.db.tmpl.dashboardLink, { "id": dashboard.id, "name": dashboard.name } ) );
+	
+				if ( index == 0 )
+				{
+					first = dashboard.id;
+				}
+			} );
+
+			if ( undefined == dhis2.db.current )
+			{
+				dhis2.db.current = first;
+			}
+			
+			dhis2.db.renderDashboard( dhis2.db.current );
+		}
+		else
+		{
+			dhis2.db.clearDashboard();
+			$( "#contentList" ).append( $.tmpl( dhis2.db.tmpl.moduleIntro ) );			
+		}
+	} );
+}
+
+dhis2.db.clearDashboard = function()
+{
+	$d = $( "#contentList" ).empty();
+}
+
+dhis2.db.renderDashboard = function( id )
+{
+	$( "#dashboard-" + dhis2.db.current ).removeClass( "currentDashboard" );
+	
+	dhis2.db.current = id;
+	
+	$( "#dashboard-" + dhis2.db.current ).addClass( "currentDashboard" );
+	
+	$d = $( "#contentList" ).empty();
+	
+	$.getJSON( "../api/dashboards/" + id, function( data )
+	{
+		if ( undefined !== data.items )
+		{
+			$.each( data.items, function( index, item )
+			{
+				if ( "chart" == item.type )
+				{
+					$d.append( $.tmpl( dhis2.db.tmpl.chartItem, { "itemId": item.id, "id": item.chart.id, "name": item.chart.name } ) )
+				}
+				else if ( "map" == item.type )
+				{
+					$d.append( $.tmpl( dhis2.db.tmpl.mapItem, { "itemId": item.id, "id": item.map.id, "name": item.map.name } ) )
+				}
+				else if ( "users" == item.type )
+				{
+					dhis2.db.renderLinkItem( $d, item.id, item.users, "Users" );
+				}
+				else if ( "reportTables" == item.type )
+				{
+					dhis2.db.renderLinkItem( $d, item.id, item.reportTables, "Pivot tables" );
+				}
+				else if ( "reports" == item.type )
+				{
+					dhis2.db.renderLinkItem( $d, item.id, item.reports, "Reports" );
+				}
+				else if ( "resources" == item.type )
+				{
+					dhis2.db.renderLinkItem( $d, item.id, item.resources, "Resources" );
+				}
+			} );
+		}
+		else
+		{
+			$d.append( $.tmpl( dhis2.db.tmpl.dashboardIntro ) );
+		}
+	} );
+}
+
+dhis2.db.renderLinkItem = function( $d, itemId, contents, title )
+{
+	var html = 
+		"<li><div class='itemHeader'><a href='javascript:dhis2.db.removeItem( \"" + itemId + "\" )'>Remove</a></div>" +
+		"<div class='item'><ul class='itemList'><li class='itemTitle'>" + title + "</li>";
+	
+	$.each( contents, function( index, content )
+	{
+		html += 
+			"<li><a href=''>" + content.name + "</a><a class='removeItemLink' href='javascript:dhis2.db.removeItemContent( \"" + itemId + "\", \"" + content.id + "\" )' title='Remove'>" + 
+			"<img src='../images/hide.png'></a></li>";
+	} );
+	
+	html += "</ul></div></li>";
+	
+	$d.append( html );
+}
+
+dhis2.db.addItemContent = function( type, id )
+{
+	if ( undefined !== dhis2.db.current )
+	{
+		$.ajax( {
+	    	type: "post",
+	    	url: "../api/dashboards/" + dhis2.db.current + "/items/content",
+	    	data: {
+	    		type: type,
+	    		id: id
+	    	},
+	    	success: function() {
+	    		dhis2.db.renderDashboard( dhis2.db.current );
+	    	}
+	    } );
+	}
+}
+
+dhis2.db.removeItem = function( id )
+{
+	$.ajax( {
+    	type: "delete",
+    	url: "../api/dashboards/" + dhis2.db.current + "/items/" + id,
+    	success: function() {
+    		dhis2.db.renderDashboard( dhis2.db.current );
+    	}
     } );
 }
 
-function clearArea( area )
+dhis2.db.removeItemContent = function( itemId, contentId )
 {
-    $.get( "clearArea.action", {
-        'area' : area
-    }, function()
-    {
-        window.location.href = "index.action";
+	$.ajax( {
+    	type: "delete",
+    	url: "../api/dashboards/" + dhis2.db.current + "/items/" + itemId + "/content/" + contentId,
+    	success: function() {
+    		dhis2.db.renderDashboard( dhis2.db.current );
+    	}
     } );
 }
 
-function viewChart( url, name )
-{
-    var width = 820;
-    var height = 520;
-    var title = i18n_viewing + " " + name;
+//------------------------------------------------------------------------------
+// Search
+//------------------------------------------------------------------------------
 
-    $( "#chartImage" ).attr( "src", url );
-    $( "#chartView" ).dialog( {
-        autoOpen : true,
-        modal : true,
-        height : height + 65,
-        width : width + 25,
-        resizable : false,
-        title : title
-    } );
+dhis2.db.search = function()
+{
+	var query = $.trim( $( "#searchField" ).val() );
+	
+	if ( query.length == 0 )
+	{
+		dhis2.db.hideSearch();
+		return false;
+	}
+	
+	var hits = $.get( "../api/dashboards/q/" + query, function( data ) {
+		var $h = $( "#hitDiv" );
+		dhis2.db.renderSearch( data, $h );
+		$h.show();
+	} );		
 }
 
-function explore( uid )
+dhis2.db.renderSearch = function( data, $h )
 {
-	window.location.href = "../dhis-web-visualizer/app/index.html?id=" + uid;
+	$h.empty().append( "<ul>" );
+	
+	if ( data.searchCount > 0 )
+	{
+		if ( data.userCount > 0 )
+		{
+			$h.append( $.tmpl( dhis2.db.tmpl.hitHeader, { "title": "Users" } ) );
+			
+			for ( var i in data.users )
+			{
+				var o = data.users[i];
+				$h.append( $.tmpl( dhis2.db.tmpl.hitItem, { "link": "profile.action?id=" + o.id, "img": "user_small", "name": o.name, "type": "users", "id": o.id } ) );
+			}
+		}
+		
+		if ( data.chartCount > 0 )
+		{
+			$h.append( $.tmpl( dhis2.db.tmpl.hitHeader, { "title": "Charts" } ) );
+			
+			for ( var i in data.charts )
+			{
+				var o = data.charts[i];
+				$h.append( $.tmpl( dhis2.db.tmpl.hitItem, { "link": "../dhis-web-visualizer/app/index.html?id=" + o.id, "img": "chart_small", "name": o.name, "type": "chart", "id": o.id } ) );
+			}
+		}
+		
+		if ( data.mapCount > 0 )
+		{
+			$h.append( $.tmpl( dhis2.db.tmpl.hitHeader, { "title": "Maps" } ) );
+			
+			for ( var i in data.maps )
+			{
+				var o = data.maps[i];
+				$h.append( $.tmpl( dhis2.db.tmpl.hitItem, { "link": "../dhis-web-mapping/app/index.html?id=" + o.id, "img": "map_small", "name": o.name, "type": "map", "id": o.id } ) );
+			}
+		}
+		
+		if ( data.reportTableCount > 0 )
+		{
+			$h.append( $.tmpl( dhis2.db.tmpl.hitHeader, { "title": "Pivot tables" } ) );
+			
+			for ( var i in data.reportTables )
+			{
+				var o = data.reportTables[i];
+				$h.append( $.tmpl( dhis2.db.tmpl.hitItem, { "link": "../dhis-web-pivot/app/index.html?id=" + o.id, "img": "table_small", "name": o.name, "type": "reportTables", "id": o.id } ) );
+			}
+		}
+		
+		if ( data.reportCount > 0 )
+		{
+			$h.append( $.tmpl( dhis2.db.tmpl.hitHeader, { "title": "Standard reports" } ) );
+			
+			for ( var i in data.reports )
+			{
+				var o = data.reports[i];
+				$h.append( $.tmpl( dhis2.db.tmpl.hitItem, { "link": "../dhis-web-reporting/getReportParams.action?uid=" + o.id, "img": "standard_report_small", "name": o.name, "type": "reports", "id": o.id } ) );
+			}
+		}
+		
+		if ( data.resourceCount > 0 )
+		{
+			$h.append( $.tmpl( dhis2.db.tmpl.hitHeader, { "title": "Resources" } ) );
+			
+			for ( var i in data.resources )
+			{
+				var o = data.resources[i];
+				$h.append( $.tmpl( dhis2.db.tmpl.hitItem, { "link": "../api/documents/" + o.id, "img": "document_small", "name": o.name, "type": "resources", "id": o.id } ) );
+			}
+		}
+	}
+	else
+	{
+		$h.append( $.tmpl( dhis2.db.tmpl.hitHeader, { "title": "No results found" } ) );
+	}
 }
 
-function viewShareForm( uid, name )
+dhis2.db.hideSearch = function()
+{
+	$( "#hitDiv" ).hide();
+}
+
+//------------------------------------------------------------------------------
+// Share
+//------------------------------------------------------------------------------
+
+dhis2.db.viewShareForm = function( id, type, name )
 {	
-	$( "#interpretationChartId" ).val( uid );
+	dhis2.db.currentShareId = id;
+	dhis2.db.currentShareType = type;
 	
 	var title = i18n_share_your_interpretation_of + " " + name;
 	
@@ -85,21 +412,21 @@ function viewShareForm( uid, name )
 	} );
 }
 
-function shareInterpretation()
+dhis2.db.shareInterpretation = function()
 {
-    var chartId = $( "#interpretationChartId" ).val();
-    var text = $( "#interpretationArea" ).val();
-    
-    if ( text.length && $.trim( text ).length )
-    {
-    	text = $.trim( text );
-    	
-	    var url = "../api/interpretations/chart/" + chartId;
+  var text = $( "#interpretationArea" ).val();
+  
+  if ( text.length && $.trim( text ).length )
+  {
+  	text = $.trim( text );
+  	
+	    var url = "../api/interpretations/" + dhis2.db.currentShareType + "/" + dhis2.db.currentShareId;
 	    
 	    // TODO url += ( ou && ou.length ) ? "?ou=" + ou : "";
 	    
-	    $.ajax( url, {
-	    	type: "POST",
+	    $.ajax( {
+	    	type: "post",
+	    	url: url,
 	    	contentType: "text/html",
 	    	data: text,
 	    	success: function() {
@@ -108,10 +435,10 @@ function shareInterpretation()
 	    		setHeaderDelayMessage( i18n_interpretation_was_shared );
 	    	}    	
 	    } );
-    }
+  }
 }
 
-function showShareHelp()
+dhis2.db.showShareHelp = function()
 {
 	$( "#shareHelpForm" ).dialog( {
 		modal: true,
@@ -121,98 +448,34 @@ function showShareHelp()
 	} );
 }
 
-function search()
+//------------------------------------------------------------------------------
+// Chart
+//------------------------------------------------------------------------------
+
+dhis2.db.viewImage = function( url, name )
 {
-	var query = $.trim( $( "#searchField" ).val() );
-	
-	if ( query.length == 0 )
-	{
-		hideSearch();
-		return false;
-	}
-	
-	var hits = $.get( "../api/dashboards/q/" + query, function( data ) {
-		$( "#hitDiv" ).show().html( getSearchResultList( data ) );		
-	} );		
+  var width = 820;
+  var height = 550;
+  var title = i18n_viewing + " " + name;
+
+  $( "#chartImage" ).attr( "src", url );
+  $( "#chartView" ).dialog( {
+      autoOpen : true,
+      modal : true,
+      height : height + 65,
+      width : width + 25,
+      resizable : false,
+      title : title
+  } );
 }
 
-function getSearchResultList( data )
+dhis2.db.exploreChart = function( uid )
 {
-	var html = "<ul>";
-	
-	if ( data.searchCount > 0 )
-	{
-		if ( data.userCount > 0 )
-		{
-			html += "<li class='hitHeader'>Users</li>";			
-			for ( var i in data.users )
-			{
-				var o = data.users[i];
-				html += "<li><a href='profile.action?id=" + o.id + "'><img src='../images/user_small.png'>" + o.name + "</a></li>";
-			}
-		}
-		
-		if ( data.chartCount > 0 )
-		{
-			html += "<li class='hitHeader'>Charts</li>";
-			for ( var i in data.charts )
-			{
-				var o = data.charts[i];
-				html += "<li><a href='../dhis-web-visualizer/app/index.html?id=" + o.id + "'><img src='../images/chart_small.png'>" + o.name + "</a></li>";
-			}
-		}
-		
-		if ( data.mapCount > 0 )
-		{
-			html += "<li class='hitHeader'>Maps</li>";
-			for ( var i in data.maps )
-			{
-				var o = data.maps[i];
-				html += "<li><a href='../dhis-web-mapping/app/index.html?id=" + o.id + "'><img src='../images/map_small.png'>" + o.name + "</a></li>";
-			}
-		}
-		
-		if ( data.reportTableCount > 0 )
-		{
-			html += "<li class='hitHeader'>Pivot tables</li>";
-			for ( var i in data.reportTables )
-			{
-				var o = data.reportTables[i];
-				html += "<li><a href='../dhis-web-pivot/app/index.html?id=" + o.id + "'><img src='../images/table_small.png'>" + o.name + "</a></li>";
-			}
-		}
-		
-		if ( data.reportCount > 0 )
-		{
-			html += "<li class='hitHeader'>Standard reports</li>";
-			for ( var i in data.reports )
-			{
-				var o = data.reports[i];
-				html += "<li><a href='../dhis-web-reporting/getReportParams.action?uid=" + o.id + "&mode=report'><img src='../images/standard_report_small.png'>" + o.name + "</a></li>";
-			}
-		}
-		
-		if ( data.resourceCount > 0 )
-		{
-			html += "<li class='hitHeader'>Resources</li>";
-			for ( var i in data.resources )
-			{
-				var o = data.resources[i];
-				html += "<li><a href='../api/documents/" + o.id + "/data'><img src='../images/document_small.png'>" + o.name + "</a></li>";
-			}
-		}
-	}
-	else
-	{
-		html += "<li class='hitHeader'>No results found</li>";
-	}
-	
-	html += "</ul>";
-	
-	return html;
+	window.location.href = "../dhis-web-visualizer/app/index.html?id=" + uid;
 }
 
-function hideSearch()
+dhis2.db.exploreMap = function( uid )
 {
-	$( "#hitDiv" ).hide();
+	window.location.href = "../dhis-web-mapping/app/index.html?id=" + uid;
 }
+
