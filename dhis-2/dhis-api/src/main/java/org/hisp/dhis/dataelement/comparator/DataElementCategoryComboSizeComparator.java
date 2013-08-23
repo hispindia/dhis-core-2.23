dbfa1be71d9e9d0da9d62ae0a45ac1856365a214
Morten Olav Hansen <mortenoh@gmail.com>
2013-08-23 17:56:19 +0200

    revno: 11779 message:   update copyright to 2004-2013
    committer: Morten Olav Hansen <mortenoh@gmail.com>
    branch nick: dhis2
    timestamp: Fri 2013-08-23 17:56:19 +0200

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/dataelement/comparator/DataElementCategoryComboSizeComparator.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/dataelement/comparator/DataElementCategoryComboSizeComparator.java
@@ -1,15 +1,18 @@
+package org.hisp.dhis.dataelement.comparator;
+
 /*
- * Copyright (c) 2004-2009, University of Oslo
+ * Copyright (c) 2004-2013, University of Oslo
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * * Redistributions of source code must retain the above copyright notice, this
+ * Redistributions of source code must retain the above copyright notice, this
  * list of conditions and the following disclaimer.
- * * Redistributions in binary form must reproduce the above copyright notice,
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
  * this list of conditions and the following disclaimer in the documentation
  * and/or other materials provided with the distribution.
- * * Neither the name of the HISP project nor the names of its contributors may
+ * Neither the name of the HISP project nor the names of its contributors may
  * be used to endorse or promote products derived from this software without
  * specific prior written permission.
  *
@@ -24,7 +27,6 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.hisp.dhis.dataelement.comparator;
 
 /**
  * @author Abyot Asalefew Gizaw
