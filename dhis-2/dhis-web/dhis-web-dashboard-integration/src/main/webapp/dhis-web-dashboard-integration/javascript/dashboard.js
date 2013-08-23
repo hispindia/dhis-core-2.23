8c5fe048c6d13086eedaffbcdc453eadd0faa393
Lars Helge Øverland <larshelge@gmail.com>
2013-08-23 18:37:06 +0200

    revno: 11784 message:   Dashboard, light grey color for disabled links
    committer: Lars Helge Øverland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Fri 2013-08-23 18:37:06 +0200

--- dhis-2/dhis-web/dhis-web-dashboard-integration/src/main/webapp/dhis-web-dashboard-integration/javascript/dashboard.js
+++ dhis-2/dhis-web/dhis-web-dashboard-integration/src/main/webapp/dhis-web-dashboard-integration/javascript/dashboard.js
@@ -263,7 +263,7 @@ dhis2.db.removeDashboard = function()
 function updateSharing( dashboard ) {
     $( '#dashboardMenu' ).data( 'canManage', dashboard.access.manage );
 
-    if( dashboard.access.manage ) {
+    if ( dashboard.access.manage ) {
         var manageLink = $( '<a/>' )
             .addClass( 'bold' )
             .text( i18n_manage )
@@ -275,24 +275,26 @@ function updateSharing( dashboard ) {
             .text( i18n_share )
             .attr( 'href', 'javascript:showSharingDialog("dashboard", "' + dashboard.id + '")' );
         $( '#manageSharing' ).html( sharingLink );
-    } else {
+    } 
+    else 
+    {
         var manageLink = $( '<a/>' )
             .addClass( 'bold' )
-            .css({
+            .css( {
                 'cursor': 'default',
                 'text-decoration': 'none',
-                'color': 'black'
-            })
+                'color': '#888'
+            } )
             .text( i18n_manage );
         $( '#manageDashboard' ).html( manageLink );
 
         var sharingLink = $( '<a/>' )
             .addClass( 'bold' )
-            .css({
+            .css( {
                 'cursor': 'default',
                 'text-decoration': 'none',
-                'color': 'black'
-            })
+                'color': '#888'
+            } )
             .text( i18n_share );
         $( '#manageSharing' ).html( sharingLink );
     }
