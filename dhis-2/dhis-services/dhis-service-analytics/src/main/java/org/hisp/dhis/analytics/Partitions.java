800d1f21de1fe747310bcf660e51df77ae017ff3
Lars Helge Øverland <larshelge@gmail.com>
2013-09-01 20:00:34 +0200

    revno: 11850 message:   Missing license
    committer: Lars Helge Øverland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Sun 2013-09-01 20:00:34 +0200

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/Partitions.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/Partitions.java
@@ -1,12 +1,47 @@
 package org.hisp.dhis.analytics;
 
+/*
+ * Copyright (c) 2004-2013, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author Lars Helge Overland
+ */
 public class Partitions
 {
     private List<String> partitions = new ArrayList<String>();
-    
+
+    // -------------------------------------------------------------------------
+    // Constructors
+    // -------------------------------------------------------------------------
+
     public Partitions()
     {
     }
@@ -15,7 +50,11 @@ public class Partitions
     {
         this.partitions = partitions;
     }
-    
+
+    // -------------------------------------------------------------------------
+    // Logic
+    // -------------------------------------------------------------------------
+
     public Partitions add( String partition )
     {
         partitions.add( partition );
@@ -30,16 +69,6 @@ public class Partitions
     public String getSinglePartition()
     {
         return partitions.get( 0 );
-    }
-
-    public List<String> getPartitions()
-    {
-        return partitions;
-    }
-
-    public void setPartitions( List<String> partitions )
-    {
-        this.partitions = partitions;
     }
 
     @Override
@@ -75,5 +104,19 @@ public class Partitions
         Partitions other = (Partitions) object;
         
         return partitions.equals( other.partitions );
+    }
+    
+    // -------------------------------------------------------------------------
+    // Getters and setters
+    // -------------------------------------------------------------------------
+
+    public List<String> getPartitions()
+    {
+        return partitions;
+    }
+
+    public void setPartitions( List<String> partitions )
+    {
+        this.partitions = partitions;
     }
 }
