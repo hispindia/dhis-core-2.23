a89f1601bc338b79c488e10bb07bbe7002cf70fb
Tran Chau <tran.hispvietnam@gmail.com>
2013-09-06 02:12:06 +0700

    revno: 11939 message:   Only Display programs by current user in Advanced search div
    committer: Tran Chau <tran.hispvietnam@gmail.com>
    branch nick: dhis2
    timestamp: Fri 2013-09-06 02:12:06 +0700

--- dhis-2/dhis-web/dhis-web-caseentry/src/main/java/org/hisp/dhis/caseentry/action/patient/SelectAction.java
+++ dhis-2/dhis-web/dhis-web-caseentry/src/main/java/org/hisp/dhis/caseentry/action/patient/SelectAction.java
@@ -118,9 +118,7 @@ public class SelectAction
 
         patientAttributes = patientAttributeService.getAllPatientAttributes();
 
-        programs = new ArrayList<Program>( programService.getProgramsByDisplayOnAllOrgunit( true, null ) );
-        programs.addAll( programService.getProgramsByDisplayOnAllOrgunit( false, organisationUnit ) );
-        programs.retainAll( programService.getProgramsByCurrentUser() );
+        programs = new ArrayList<Program>( programService.getProgramsByCurrentUser() );
         programs.removeAll( programService.getPrograms( Program.SINGLE_EVENT_WITHOUT_REGISTRATION ) );
 
         Collections.sort( programs, IdentifiableObjectNameComparator.INSTANCE );
