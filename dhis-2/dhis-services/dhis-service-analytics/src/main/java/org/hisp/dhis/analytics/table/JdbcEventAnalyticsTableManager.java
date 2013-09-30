ac515f8de3198ef682e405c9a867ba6604bd5dda
Lars Helge Øverland <larshelge@gmail.com>
2013-09-30 16:44:52 +0200

    revno: 12337 message:   Event analytics tables, removing not null on ou code
    committer: Lars Helge Øverland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Mon 2013-09-30 16:44:52 +0200

--- dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/table/JdbcEventAnalyticsTableManager.java
+++ dhis-2/dhis-services/dhis-service-analytics/src/main/java/org/hisp/dhis/analytics/table/JdbcEventAnalyticsTableManager.java
@@ -233,7 +233,7 @@ public class JdbcEventAnalyticsTableManager
         String[] ed = { "executiondate", "date", "psi.executiondate" };
         String[] ou = { "ou", "character(11) not null", "ou.uid" };
         String[] oun = { "ouname", "character varying(230) not null", "ou.name" };
-        String[] ouc = { "oucode", "character varying(50) not null", "ou.code" };
+        String[] ouc = { "oucode", "character varying(50)", "ou.code" };
         
         columns.addAll( Arrays.asList( gender, isdead, psi, ps, ed, ou, oun, ouc ) );
         
