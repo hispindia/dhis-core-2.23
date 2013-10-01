09505526715f9d44d02da0e5b1707bfa1b8e8181
Bob Jolliffe <bobjolliffe@gmail.com>
2013-10-01 16:59:23 +0100

    revno: 12367 message:   Removed integration service module   Added dependency to servlet-api on web modules which refused to build
    committer: Bob Jolliffe <bobjolliffe@gmail.com>
    branch nick: dhis2
    timestamp: Tue 2013-10-01 16:59:23 +0100

--- local/ke/mfl2dxf2/src/main/java/org/hisp/dhis/integration/routes/MFLTransformRoute.java
+++ local/ke/mfl2dxf2/src/main/java/org/hisp/dhis/integration/routes/MFLTransformRoute.java
@@ -30,6 +30,7 @@ package org.hisp.dhis.integration.routes;
 import org.apache.camel.builder.RouteBuilder;
 
 import java.io.InputStream;
+import org.apache.camel.LoggingLevel;
 
 /**
  * @author Morten Olav Hansen <mortenoh@gmail.com>
@@ -41,10 +42,11 @@ public class MFLTransformRoute
     public void configure() throws Exception
     {
         // from( "file:/Users/mortenoh/GDrive/HISP/ke/mfl/data/inbox?consumer.initialDelay=10000&consumer.delay=5000" ).
-        from( "quartz://every1minute?cron=0+0/1+*+*+*+%3F" ).
-            to( "http://api.ehealth.or.ke/api/facilities?lastDays=720&paging=off&scheme=code" ).
+        // from( "quartz://every1minute?cron=0+0/1+*+*+*+%3F" ).
+        from( "quartz://every1minute?trigger.repeatInterval=60000&fireNow=true" ).
+            to( "http://api.ehealth.or.ke/api/facilities?lastDays=720&paging=off&scheme=code&httpClient.soTimeout=120000" ).
             to( "xslt:transform/mfl2dxf2.xsl" ).
-            convertBodyTo( InputStream.class ).inOut( "dhis2:metadata" ).
+            // convertBodyTo( InputStream.class ).inOut( "dhis2:metadata" ).
             to( "log:org.hisp.dhis.camel?level=INFO" );
     }
 }
