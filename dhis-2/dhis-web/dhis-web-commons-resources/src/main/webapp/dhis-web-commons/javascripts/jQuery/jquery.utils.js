39fa4d5935ccc5f83901e37f2fbf1cb67efe323e
Morten Olav Hansen <mortenoh@gmail.com>
2013-10-07 17:30:32 +0200

    revno: 12481 message:   minor fix in jquery.utils.js, return promise object from ajax calls, makes it possible to add additional callback handlers
    committer: Morten Olav Hansen <mortenoh@gmail.com>
    branch nick: dhis2
    timestamp: Mon 2013-10-07 17:30:32 +0200

--- dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/javascripts/jQuery/jquery.utils.js
+++ dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/javascripts/jQuery/jquery.utils.js
@@ -17,15 +17,15 @@
 jQuery.extend( {
 	
 	postJSON: function( url, data, success ) {
-		$.ajax( { url:url, data:data, success:success, type:'post', dataType:'json', contentType:'application/x-www-form-urlencoded;charset=utf-8' } );
+		return $.ajax( { url:url, data:data, success:success, type:'post', dataType:'json', contentType:'application/x-www-form-urlencoded;charset=utf-8' } );
 	},
 
 	postUTF8: function( url, data, success ) {
-		$.ajax( { url:url, data:data, success:success, type:'post', contentType:'application/x-www-form-urlencoded;charset=utf-8' } );
+		return $.ajax( { url:url, data:data, success:success, type:'post', contentType:'application/x-www-form-urlencoded;charset=utf-8' } );
 	},
 	
 	loadNoCache: function( elementId, url, data ) {
-		$.ajax( { url:url, data:data, type:'get', dataType:'html', success:function( data ) {
+		return $.ajax( { url:url, data:data, type:'get', dataType:'html', success:function( data ) {
 			$( '#' + elementId ).html( data );
 		} } );
 	},
