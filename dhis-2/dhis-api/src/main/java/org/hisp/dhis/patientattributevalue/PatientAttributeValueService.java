cb8c0e21ecc6b9693140e1acf5d7df1e876eb224
Tran Chau <tran.hispvietnam@gmail.com>
2013-10-29 18:02:46 +0700

    revno: 12827 message:   Fixed bug build error.
    committer: Tran Chau <tran.hispvietnam@gmail.com>
    branch nick: dhis2
    timestamp: Tue 2013-10-29 18:02:46 +0700

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/patientattributevalue/PatientAttributeValueService.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/patientattributevalue/PatientAttributeValueService.java
@@ -125,4 +125,6 @@ public interface PatientAttributeValueService
      */
     void updatePatientAttributeValues( PatientAttributeOption patientAttributeOption );
 
+    int countByPatientAttributeoption( PatientAttributeOption patientAttributeOption );
+
 }
