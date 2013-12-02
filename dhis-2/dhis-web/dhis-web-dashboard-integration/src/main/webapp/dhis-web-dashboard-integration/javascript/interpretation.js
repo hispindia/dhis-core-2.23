6be00c375bf0250cdb3792d13c21fcfe92a7439d
Morten Olav Hansen <mortenoh@gmail.com>
2013-12-02 13:21:20 +0100

    revno: 13097 message:   add a bit more space between icon and text in dropdowns
    committer: Morten Olav Hansen <mortenoh@gmail.com>
    branch nick: dhis2
    timestamp: Mon 2013-12-02 13:21:20 +0100

--- dhis-2/dhis-web/dhis-web-dashboard-integration/src/main/webapp/dhis-web-dashboard-integration/javascript/interpretation.js
+++ dhis-2/dhis-web/dhis-web-dashboard-integration/src/main/webapp/dhis-web-dashboard-integration/javascript/interpretation.js
@@ -1,96 +1,87 @@
-
 var currentPage = 0;
 var pageLock = false;
 
-$( document ).ready( function() {
-	$( document ).scroll( function() {
-		isNextPage();
-	} );
-	
-	$( "#interpretationFeed" ).load( "getInterpretations.action", function() {
-		$( ".commentArea" ).autogrow();
-	} );
-} );
+$(function() {
+  $(document).scroll(function() {
+    isNextPage();
+  });
 
-function expandComments( id )
-{
-	$( "#comments" + id ).children().show();
-	$( "#commentHeader" + id ).hide();
+  $("#interpretationFeed").load("getInterpretations.action", function() {
+    $(".commentArea").autogrow();
+  });
+});
+
+function expandComments( id ) {
+  $("#comments" + id).children().show();
+  $("#commentHeader" + id).hide();
 }
 
-function isNextPage()
-{
-	var fromTop = $( document ).scrollTop();
-	var docHeight = $( document ).height();
-	var windowHeight = $( window ).height();
-	var threshold = parseInt( 350 );
-	var remaining = parseInt( docHeight - ( fromTop + windowHeight ) );
-	
-	if ( remaining < threshold )
-	{
-		loadNextPage();
-	}
+function isNextPage() {
+  var fromTop = $(document).scrollTop();
+  var docHeight = $(document).height();
+  var windowHeight = $(window).height();
+  var threshold = parseInt(350);
+  var remaining = parseInt(docHeight - ( fromTop + windowHeight ));
+
+  if( remaining < threshold ) {
+    loadNextPage();
+  }
 }
 
-function loadNextPage()
-{
-	if ( pageLock == true )
-	{
-		return false;
-	}
-	
-	pageLock = true;
-	currentPage++;
-	
-	$.get( "getInterpretations.action", { page: currentPage }, function( data ) {
-		$( "#interpretationFeed" ).append( data );
-		
-		if ( !isDefined ( data ) || $.trim( data ).length == 0 )
-		{
-			$( document ).off( "scroll" );
-		}
-		
-		pageLock = false;
-	} );
+function loadNextPage() {
+  if( pageLock == true ) {
+    return false;
+  }
+
+  pageLock = true;
+  currentPage++;
+
+  $.get("getInterpretations.action", { page: currentPage }, function( data ) {
+    $("#interpretationFeed").append(data);
+
+    if( !isDefined(data) || $.trim(data).length == 0 ) {
+      $(document).off("scroll");
+    }
+
+    pageLock = false;
+  });
 }
 
-function postComment( uid )
-{	
-	var text = $( "#commentArea" + uid ).val();
-	
-	$( "#commentArea" + uid ).val( "" );
-	
-	var url = "../api/interpretations/" + uid + "/comment";
-	
-	var created = getCurrentDate();
+function postComment( uid ) {
+  var text = $("#commentArea" + uid).val();
+
+  $("#commentArea" + uid).val("");
+
+  var url = "../api/interpretations/" + uid + "/comment";
+
+  var created = getCurrentDate();
 
   var gearBox = "<div class=\"gearDropDown\">\n  <span><i class=\"fa fa-gear\"></i> <i class=\"fa fa-caret-down\"></i></span>\n</div>\n";
 
-	if ( text.length && $.trim( text ).length )
-	{
-		$.ajax( url, {
-			type: "POST",
-			contentType: "text/html",
-			data: $.trim( text ),
-			success: function(data, textStatus, request) {
+  if( text.length && $.trim(text).length ) {
+    $.ajax(url, {
+      type: "POST",
+      contentType: "text/html",
+      data: $.trim(text),
+      success: function( data, textStatus, request ) {
         var locationArray = request.getResponseHeader('Location').split('/');
-        var commentUid = locationArray[locationArray.length-1];
+        var commentUid = locationArray[locationArray.length - 1];
 
-				var template =
+        var template =
           "<div class='interpretationComment' data-ip-comment-uid='" + commentUid + "'>" +
-					"<div><div class=\"interpretationName\">" +
-					"<a class=\"bold userLink\" href=\"profile.action?id=${userUid}\">${userName}</a>&nbsp;" +
-					"<span class=\"grey\">${created}<\/span>" + gearBox + "<\/div><\/div>" +
-					"<div class=\"interpretationText\">${text}<\/div>" +
-          "</div>";
+            "<div><div class=\"interpretationName\">" +
+            "<a class=\"bold userLink\" href=\"profile.action?id=${userUid}\">${userName}</a>&nbsp;" +
+            "<span class=\"grey\">${created}<\/span>" + gearBox + "<\/div><\/div>" +
+            "<div class=\"interpretationText\">${text}<\/div>" +
+            "</div>";
 
-				$.tmpl( template, {
-					"userId": currentUser.id,
-					"userUid": currentUser.uid,
-					"userName": currentUser.name, 
-					created: created, 
-					text: text } ).appendTo( "#comments" + uid );
-			}		
-		} );
-	}
+        $.tmpl(template, {
+          "userId": currentUser.id,
+          "userUid": currentUser.uid,
+          "userName": currentUser.name,
+          created: created,
+          text: text }).appendTo("#comments" + uid);
+      }
+    });
+  }
 }
