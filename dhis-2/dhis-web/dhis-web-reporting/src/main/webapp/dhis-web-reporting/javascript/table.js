c091139456fcdcb42fadb8979ce24e7a338e8367
Morten Olav Hansen <mortenoh@gmail.com>
2013-12-06 14:03:52 +0100

    revno: 13162 message:   context-menus for report module
    committer: Morten Olav Hansen <mortenoh@gmail.com>
    branch nick: dhis2
    timestamp: Fri 2013-12-06 14:03:52 +0100

--- dhis-2/dhis-web/dhis-web-reporting/src/main/webapp/dhis-web-reporting/javascript/table.js
+++ dhis-2/dhis-web/dhis-web-reporting/src/main/webapp/dhis-web-reporting/javascript/table.js
@@ -1,5 +1,7 @@
+function removeTable( context ) {
+  removeItem(context.id, context.name, i18n_confirm_delete, "removeTable.action");
+}
 
-function removeTable( tableId, tableName )
-{
-    removeItem( tableId, tableName, i18n_confirm_delete, "removeTable.action" );
+function runReportTable( context ) {
+  location.href = 'getReportParams.action?uid=' + context.uid + '&mode=table';
 }
