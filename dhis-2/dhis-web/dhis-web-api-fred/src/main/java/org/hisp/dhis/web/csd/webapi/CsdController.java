8297b69738b0cb62d1aaa2cb32c89173c3d91152
Morten Olav Hansen <mortenoh@gmail.com>
2014-01-14 11:30:06 +0700

    revno: 13708 message:   csd: start of csd domain model, controller now returns very a very primitive csd model (primaryName, record). Oid mapped as Uid for now.
    committer: Morten Olav Hansen <mortenoh@gmail.com>
    branch nick: dhis2
    timestamp: Tue 2014-01-14 11:30:06 +0700

--- dhis-2/dhis-web/dhis-web-api-fred/src/main/java/org/hisp/dhis/web/csd/webapi/CsdController.java
+++ dhis-2/dhis-web/dhis-web-api-fred/src/main/java/org/hisp/dhis/web/csd/webapi/CsdController.java
@@ -28,20 +28,21 @@ package org.hisp.dhis.web.csd.webapi;
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import org.hisp.dhis.dxf2.metadata.MetaData;
-import org.hisp.dhis.dxf2.utils.JacksonUtils;
 import org.hisp.dhis.organisationunit.OrganisationUnit;
 import org.hisp.dhis.organisationunit.OrganisationUnitService;
 import org.hisp.dhis.web.csd.domain.Envelope;
+import org.hisp.dhis.web.csd.domain.csd.Csd;
+import org.hisp.dhis.web.csd.domain.csd.Facility;
+import org.hisp.dhis.web.csd.domain.csd.Record;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.HttpClientErrorException;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -59,7 +60,7 @@ public class CsdController
     private OrganisationUnitService organisationUnitService;
 
     @RequestMapping( value = "", method = RequestMethod.POST )
-    public void csdRequest( @RequestBody Envelope envelope, HttpServletResponse response ) throws IOException
+    public @ResponseBody Csd csdRequest( @RequestBody Envelope envelope, HttpServletResponse response ) throws IOException
     {
         Date lastModified = null;
 
@@ -75,9 +76,36 @@ public class CsdController
         List<OrganisationUnit> byLastUpdated = new ArrayList<OrganisationUnit>(
             organisationUnitService.getAllOrganisationUnitsByLastUpdated( lastModified ) );
 
-        MetaData metaData = new MetaData();
-        metaData.setOrganisationUnits( byLastUpdated );
+        Csd csd = convertToCsd( byLastUpdated );
 
-        JacksonUtils.toXml( response.getOutputStream(), metaData );
+        return csd;
+    }
+
+    private Csd convertToCsd( Iterable<OrganisationUnit> organisationUnits )
+    {
+        Csd csd = new Csd();
+        csd.getFacilityDirectory().setFacilities( new ArrayList<Facility>() );
+
+        for ( OrganisationUnit organisationUnit : organisationUnits )
+        {
+            Facility facility = new Facility();
+            facility.setOid( organisationUnit.getUid() );
+            facility.setPrimaryName( organisationUnit.getDisplayName() );
+
+            Record record = new Record();
+            record.setCreated( organisationUnit.getCreated() );
+            record.setUpdated( organisationUnit.getLastUpdated() );
+
+            if ( organisationUnit.isActive() )
+            {
+                record.setStatus( "Active" );
+            }
+
+            facility.setRecord( record );
+
+            csd.getFacilityDirectory().getFacilities().add( facility );
+        }
+
+        return csd;
     }
 }
