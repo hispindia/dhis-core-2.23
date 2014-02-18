550f7304bc9120e1f93b490fdbfe60e7d3b325d8
Lars Helge Øverland <larshelge@gmail.com>
2014-02-18 12:05:27 +0100

    revno: 14067 message:   Increased shortname lenght in validation
    committer: Lars Helge Øverland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Tue 2014-02-18 12:05:27 +0100

--- dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/javascripts/validationRules.js
+++ dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/javascripts/validationRules.js
@@ -124,7 +124,7 @@ var validationRules = {
         "shortName" : {
             "required" : true,
             "alphanumericwithbasicpuncspaces" : true,
-            "rangelength" : [ 2, 40 ]
+            "rangelength" : [ 2, 50 ]
         },
         "code" : {
             "alphanumericwithbasicpuncspaces" : true,
@@ -166,7 +166,7 @@ var validationRules = {
         "shortName" : {
             "required" : true,
             "alphanumericwithbasicpuncspaces" : true,
-            "rangelength" : [ 2, 40 ]
+            "rangelength" : [ 2, 50 ]
         },
         "code" : {
             "alphanumericwithbasicpuncspaces" : true,
@@ -225,7 +225,7 @@ var validationRules = {
         },
         "shortName" : {
             "required" : true,
-            "rangelength" : [ 2, 40 ],
+            "rangelength" : [ 2, 50 ],
             "alphanumericwithbasicpuncspaces" : true,
             "notOnlyDigits" : true
         },
@@ -279,7 +279,7 @@ var validationRules = {
         },
         "shortName" : {
             "required" : true,
-            "rangelength" : [ 2, 40 ],
+            "rangelength" : [ 2, 50 ],
             "alphanumericwithbasicpuncspaces" : true,
             "notOnlyDigits" : true
         },
@@ -320,7 +320,7 @@ var validationRules = {
         },
         "shortName" : {
             "required" : true,
-            "rangelength" : [ 2, 40 ],
+            "rangelength" : [ 2, 50 ],
             "alphanumericwithbasicpuncspaces" : true
         },
         "alternativeName" : {
