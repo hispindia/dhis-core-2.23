6b9f75c2b94d746275e5916935fb85dc4a98128e
Abyot Asalefew Gizaw <abyota@gmail.com>
2014-03-17 16:59:31 +0100

    revno: 14264 message:   new app tracker-capture, WIP
    committer: Abyot Asalefew Gizaw <abyota@gmail.com>
    branch nick: dhis2
    timestamp: Mon 2014-03-17 16:59:31 +0100

---
+++ dhis-2/dhis-web/dhis-web-tracker-capture/src/main/webapp/dhis-web-tracker-capture/scripts/controllers.js
@@ -0,0 +1,309 @@
+'use strict';
+
+/* Controllers */
+var trackerCaptureControllers = angular.module('trackerCaptureControllers', [])
+
+//Controller for settings page
+.controller('SelectionController',
+        function($scope,
+                $location,
+                Paginator,
+                TranslationService, 
+                SelectedEntity,
+                storage,
+                AttributesFactory,
+                TrackedEntityService) {   
+   
+    //Selection
+    $scope.selectedOrgUnit = '';
+    $scope.selectedProgram = '';
+    
+    //Filtering
+    $scope.reverse = false;
+    $scope.filterText = {}; 
+    $scope.currentFilter;
+    
+    //Paging
+    $scope.rowsPerPage = 50;
+    $scope.currentPage = Paginator.getPage() + 1;   
+    
+    //Searching
+    $scope.showSearchDiv = false;
+    $scope.searchField = {title: 'search', isOpen: false};     
+    
+    //watch for selection of org unit from tree
+    $scope.$watch('selectedOrgUnit', function() {
+        
+        if( angular.isObject($scope.selectedOrgUnit)){                  
+            
+            $scope.trackedEntityList = [];
+            
+            //apply translation - by now user's profile is fetched from server.
+            TranslationService.translate();
+            $scope.attributes = storage.get('ATTRIBUTES');
+            
+            var programs = storage.get('PROGRAMS');            
+            if( programs ){                
+                $scope.loadPrograms($scope.selectedOrgUnit);     
+            }
+        }
+    });
+    
+    //load programs associated with the selected org unit.
+    $scope.loadPrograms = function(orgUnit) {      
+                                
+        $scope.selectedOrgUnit = orgUnit;
+        $scope.selectedProgram = null;
+        $scope.trackedEntityList = [];
+        
+        if (angular.isObject($scope.selectedOrgUnit)) {   
+
+            $scope.programs = [];
+            
+            var programs = storage.get('PROGRAMS');
+            
+            if( programs ){
+                for(var i=0; i<programs.length; i++){
+                    var program = storage.get(programs[i].id);   
+                    if(program.organisationUnits.hasOwnProperty(orgUnit.id)){
+                        $scope.programs.push(program);
+                    }
+                }               
+            }
+        }        
+    };   
+    
+    //get events for the selected program (and org unit)
+    $scope.loadTrackedEntities = function(){
+        
+        $scope.searchField.isOpen = false;
+        
+        $scope.trackedEntityList = [];
+        
+        $scope.gridColumns = AttributesFactory.getForListing();
+
+        //generate grid column for the selected program
+        angular.forEach($scope.gridColumns, function(gridColumn){
+            gridColumn.showFilter =  false;
+            gridColumn.hide = false;                   
+            if(gridColumn.type === 'date'){
+                 $scope.filterText[gridColumn.id]= {start: '', end: ''};
+            }
+        });
+            
+        if( angular.isObject($scope.selectedProgram)){  
+            
+            //Load entities for the selected program and orgunit
+            TrackedEntityService.getByOrgUnitAndProgram($scope.selectedOrgUnit.id, $scope.selectedProgram.id).then(function(data){
+                $scope.trackedEntityList = data;      
+                console.log('the entities are:  ', $scope.trackedEntityList);
+            });            
+        }
+        else{
+            
+            //Load entities for the selected orgunit
+            TrackedEntityService.getByOrgUnit($scope.selectedOrgUnit.id).then(function(data){
+                $scope.trackedEntityList = data;  
+                console.log('the entities are:  ', $scope.trackedEntityList);
+            });
+        }
+    };
+    
+    $scope.clearEntities = function(){
+        $scope.trackedEntityList = [];
+    };
+    
+    $scope.showRegistration = function(){
+        
+    };  
+    
+    $scope.showSearch = function(){
+        $scope.showSearchDiv = !$scope.showSearchDiv;
+        $scope.searchField.isOpen = true;
+    };
+    
+    $scope.closeSearch = function(){
+        $scope.showSearchDiv = !$scope.showSearchDiv;
+    };   
+    
+    $scope.sortGrid = function(gridHeader){
+        
+        if ($scope.sortHeader === gridHeader.id){
+            $scope.reverse = !$scope.reverse;
+            return;
+        }
+        
+        $scope.sortHeader = gridHeader.id;
+        $scope.reverse = false;    
+    };    
+    
+    $scope.filterInGrid = function(gridColumn){
+        
+        $scope.currentFilter = gridColumn;
+        for(var i=0; i<$scope.gridColumns.length; i++){
+            
+            //toggle the selected grid column's filter
+            if($scope.gridColumns[i].id === gridColumn.id){
+                $scope.gridColumns[i].showFilter = !$scope.gridColumns[i].showFilter;
+            }            
+            else{
+                $scope.gridColumns[i].showFilter = false;
+            }
+        }
+    };    
+    
+    $scope.showDashboard = function(currentEntity){       
+        SelectedEntity.setSelectedEntity(currentEntity);
+        storage.set('SELECTED_OU', $scope.selectedOrgUnit);        
+        $location.path('/dashboard').search({selectedEntityId: currentEntity.person, selectedProgramId: $scope.selectedProgram ? $scope.selectedProgram.id : null});                                    
+    };   
+    
+    $scope.search = function(){       
+        console.log('the search is:  ', $scope.attributes);
+    };
+       
+    $scope.getHelpContent = function(){
+        console.log('I will get help content');
+    };    
+})
+
+//Controller for dashboard
+.controller('DashboardController',
+        function($scope,  
+                $location,
+                storage,
+                EnrollmentService,
+                TrackedEntityService,                
+                TranslationService) {
+
+    //do translation of the dashboard page
+    TranslationService.translate();    
+    
+    //dashboard contents
+    $scope.profile = {title: 'profile', isOpen: true};
+    $scope.enrollment = {title: 'enrollment', isOpen: true};
+    $scope.notes = {title: 'notes', isOpen: true};
+    $scope.dataEntry = {title: 'dataentry', isOpen: true};  
+    $scope.selected = {title: 'current_selections', isOpen: true, selections: []};        
+    
+    //attributes for profile    
+    $scope.attributes = {};    
+    angular.forEach(storage.get('ATTRIBUTES'), function(attribute){
+        $scope.attributes[attribute.id] = attribute;
+    });   
+    
+    //selections
+    $scope.selectedEntityId = ($location.search()).selectedEntityId;
+    $scope.selectedProgramId = ($location.search()).selectedProgramId;
+    
+    $scope.selectedOrgUnit = storage.get('SELECTED_OU');
+    
+    $scope.selectedProgram = null;
+    
+    if($scope.selectedProgramId){
+        $scope.selectedProgram = storage.get($scope.selectedProgramId);        
+    }
+    
+    //programs for enrollment
+    $scope.programs = [];
+    var programs = storage.get('PROGRAMS');            
+    if( programs ){
+        for(var i=0; i<programs.length; i++){
+            var program = storage.get(programs[i].id);   
+            if($scope.selectedProgramId == programs[i].id){
+                $scope.selectedProgram = program;
+            }
+            if(program.organisationUnits.hasOwnProperty($scope.selectedOrgUnit.id)){
+                $scope.programs.push(program);
+            }
+        }               
+    }   
+    
+    if( $scope.selectedEntityId ){
+        
+        //Fetch the selected entity
+        TrackedEntityService.get($scope.selectedEntityId).then(function(data){
+            $scope.selectedEntity = data;            
+            console.log('the entity is:  ', $scope.selectedEntity);
+        });
+        
+        EnrollmentService.get($scope.selectedEntityId).then(function(data){
+            console.log('enrollment:  ', data);
+        });
+    }
+    
+    $scope.selected.selections.push({title: 'registering_unit', value: $scope.selectedOrgUnit ? $scope.selectedOrgUnit.name : 'not_selected'});
+    $scope.selected.selections.push({title: 'program', value: $scope.selectedProgram ? $scope.selectedProgram.name : 'not_selected'});    
+    
+    $scope.back = function(){
+        $location.path('/');
+    };
+    
+    $scope.displayEnrollment = false;
+    $scope.showEnrollment = function(){
+        $scope.displayEnrollment = true;
+    };
+
+})
+
+//Controller for the enrollment section
+.controller('EnrollmentController',
+        function($scope,                
+                storage,
+                TranslationService) {
+
+    TranslationService.translate();
+    
+    $scope.attributes = storage.get('ATTRIBUTES');
+    
+})
+
+//Controller for the notes section
+.controller('NotesController',
+        function($scope,                
+                storage,
+                TranslationService) {
+
+    TranslationService.translate();
+    
+    $scope.attributes = storage.get('ATTRIBUTES');
+    
+})
+
+//Controller for the notes section
+.controller('NotesController',
+        function($scope,                
+                storage,
+                TranslationService) {
+
+    TranslationService.translate();
+    
+    $scope.attributes = storage.get('ATTRIBUTES');
+    
+})
+
+//Controller for the data entry section
+.controller('DataEntryController',
+        function($scope,                
+                storage,
+                TranslationService) {
+
+    TranslationService.translate();
+    
+    $scope.attributes = storage.get('ATTRIBUTES');
+    
+})
+
+//Controller for the header section
+.controller('HeaderController',
+        function($scope,                
+                DHIS2URL,
+                TranslationService) {
+
+    TranslationService.translate();
+    
+    $scope.home = function(){        
+        window.location = DHIS2URL;
+    };
+    
+});
