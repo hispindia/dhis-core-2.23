752984e3249e3b3ad89fb0905ac79481e3dc7b3c
Lars Helge Øverland <larshelge@gmail.com>
2014-03-31 10:14:03 +0200

    revno: 14555 message:   Javadoc
    committer: Lars Helge Øverland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Mon 2014-03-31 10:14:03 +0200

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/common/OrganisationUnitSelectionMode.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/common/OrganisationUnitSelectionMode.java
@@ -29,6 +29,15 @@ package org.hisp.dhis.common;
  */
 
 /**
+ * Defines the selection of organisation units.
+ * 
+ * <ul>
+ * <li>SELECTED: specified units only.</li>
+ * <li>CHILDREN: immediate children of specified units, including specified units.</li>
+ * <li>DESCENDANTS: all units in sub-hierarchy of specified units, including specified units.</li>
+ * <li>ALL: all units in system.</li>
+ * </ul>
+ * 
  * @author Lars Helge Overland
  */
 public enum OrganisationUnitSelectionMode
