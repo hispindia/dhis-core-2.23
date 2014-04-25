7fe975a803b4ac8f19014c6aa19d0de1a611fffa
Lars Helge Øverland <larshelge@gmail.com>
2014-04-25 16:00:28 +0200

    revno: 14992 message:   Adhoc, added sample random data value command
    committer: Lars Helge Øverland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Fri 2014-04-25 16:00:28 +0200

--- dhis-2/dhis-api/src/main/java/org/hisp/dhis/dataelement/DataElement.java
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/dataelement/DataElement.java
@@ -83,18 +83,18 @@ public class DataElement
      */
     private static final long serialVersionUID = -7131541880444446669L;
 
-    public static final String VALUE_TYPE_STRING = "string";
     public static final String VALUE_TYPE_INT = "int";
-    public static final String VALUE_TYPE_NUMBER = "number";
+    public static final String VALUE_TYPE_STRING = "string";
     public static final String VALUE_TYPE_USER_NAME = "username";
     public static final String VALUE_TYPE_BOOL = "bool";
     public static final String VALUE_TYPE_TRUE_ONLY = "trueOnly";
     public static final String VALUE_TYPE_DATE = "date";
     public static final String VALUE_TYPE_UNIT_INTERVAL = "unitInterval";
 
-    public static final String VALUE_TYPE_ZERO_OR_POSITIVE_INT = "zeroPositiveInt";
+    public static final String VALUE_TYPE_NUMBER = "number";
     public static final String VALUE_TYPE_POSITIVE_INT = "posInt";
     public static final String VALUE_TYPE_NEGATIVE_INT = "negInt";
+    public static final String VALUE_TYPE_ZERO_OR_POSITIVE_INT = "zeroPositiveInt";
     public static final String VALUE_TYPE_TEXT = "text";
     public static final String VALUE_TYPE_LONG_TEXT = "longText";
 
