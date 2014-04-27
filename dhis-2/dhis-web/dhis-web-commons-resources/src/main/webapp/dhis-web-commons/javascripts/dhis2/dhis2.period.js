2d5c29cd85d5db8f206f5f04a8f7010763f1e5f3
Morten Olav Hansen <mortenoh@gmail.com>
2014-04-27 20:18:19 +0545

    revno: 15022 message:   add quarterly js generator
    committer: Morten Olav Hansen <mortenoh@gmail.com>
    branch nick: dhis2
    timestamp: Sun 2014-04-27 20:18:19 +0545

--- dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/javascripts/dhis2/dhis2.period.js
+++ dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/javascripts/dhis2/dhis2.period.js
@@ -82,3 +82,31 @@ dhis2.period.generateBiMonthlyPeriods = function( cal, offset ) {
 
   return periods;
 };
+
+dhis2.period.generateQuarterlyPeriods = function( cal, offset ) {
+  var year = cal.today().year() - offset;
+
+  var periods = [];
+
+  for( var month = 1, idx = 1; month <= cal.monthsInYear(year); month += 3, idx++ ) {
+    log('month: ' + month);
+    var startDate = cal.newDate(year, month, 1);
+    var endDate = cal.newDate(startDate).set(month + 2, 'm');
+    endDate.set(endDate.daysInMonth(month + 2), 'd');
+
+    if( startDate.year() != endDate.year() ) {
+      break;
+    }
+
+    var period = {};
+    period['startDate'] = startDate.formatDate(dhis2.period.DATE_FORMAT);
+    period['endDate'] = endDate.formatDate(dhis2.period.DATE_FORMAT);
+    period['name'] = startDate.formatDate("MM") + '-' + endDate.formatDate('MM') + ' ' + year;
+    period['id'] = 'Monthly_' + period['startDate'];
+    period['iso'] = startDate.formatDate("yyyy") + 'Q' + idx;
+
+    periods.push(period);
+  }
+
+  return periods;
+};
