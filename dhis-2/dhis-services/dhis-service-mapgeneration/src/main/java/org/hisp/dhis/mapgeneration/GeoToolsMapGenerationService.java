ab854f506c1065cc66baafa6a505c3460a25a6e3
Lars Helge Overland <larshelge@gmail.com>
2014-06-17 15:14:04 +0200

    revno: 15726 message:   PNG maps, making sure that map image height is never less than legend
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Tue 2014-06-17 15:14:04 +0200

--- dhis-2/dhis-services/dhis-service-mapgeneration/src/main/java/org/hisp/dhis/mapgeneration/GeoToolsMapGenerationService.java
+++ dhis-2/dhis-services/dhis-service-mapgeneration/src/main/java/org/hisp/dhis/mapgeneration/GeoToolsMapGenerationService.java
@@ -366,9 +366,12 @@ public class GeoToolsMapGenerationService
         Assert.isTrue( mapImage != null );
         Assert.isTrue( legendImage.getType() == mapImage.getType() );
 
-        // Create a new image with dimension (legend.width + map.width,
-        // max(legend.height, map.height))
-        BufferedImage finalImage = new BufferedImage( legendImage.getWidth() + mapImage.getWidth(), titleImage.getHeight() + mapImage.getHeight(), mapImage.getType() );
+        // Create image, note that image height cannot be less than legend
+        
+        int width = legendImage.getWidth() + mapImage.getWidth();
+        int height = Math.max( titleImage.getHeight() + mapImage.getHeight(), ( legendImage.getHeight() + 1 ) );
+        
+        BufferedImage finalImage = new BufferedImage( width, height, mapImage.getType() );
 
         // Draw the two images onto the final image with the legend to the left
         // and the map to the right
