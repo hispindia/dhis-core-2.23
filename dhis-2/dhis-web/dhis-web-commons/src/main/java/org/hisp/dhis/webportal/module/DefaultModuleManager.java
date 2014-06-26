cdc936594de92bef3102311c6f7a70216172095b
Lars Helge Overland <larshelge@gmail.com>
2014-06-26 17:19:09 +0200

    revno: 15864 message:   ModuleManager, made list of modules immutable
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Thu 2014-06-26 17:19:09 +0200

--- dhis-2/dhis-web/dhis-web-commons/src/main/java/org/hisp/dhis/webportal/module/DefaultModuleManager.java
+++ dhis-2/dhis-web/dhis-web-commons/src/main/java/org/hisp/dhis/webportal/module/DefaultModuleManager.java
@@ -121,7 +121,7 @@ public class DefaultModuleManager
     {
         detectModules();
 
-        return menuModules;
+        return new ArrayList<>( menuModules );
     }
 
     public List<Module> getAccessibleMenuModules()
@@ -149,7 +149,7 @@ public class DefaultModuleManager
     {
         detectModules();
 
-        return modulesByName.values();
+        return new ArrayList<>( modulesByName.values() );
     }
     
     public Module getCurrentModule()
