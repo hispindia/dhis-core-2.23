09954770d90987ab7440b48c55dbbfbaa0297f91
Bob Jolliffe <bobjolliffe@gmail.com>
2014-07-04 11:56:19 +0100

    revno: 15965 message:   dhis2-tools:   Created new install.sh which makes use of the new package repository at apt.dhis2.org   Added support for 14.04 LTS   Continued WIP on documentation
    committer: Bob Jolliffe <bobjolliffe@gmail.com>
    branch nick: dhis2
    timestamp: Fri 2014-07-04 11:56:19 +0100

--- tools/dhis2-tools-deb/install_14.04.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# set -e
-
-# install the dhis2-tools deb
-dpkg -i dhis2-tools* 
-apt-get -y install -f
-
-# Uncomment below to install postgres and nginx servers on this machine
-# apt-get -y install nginx postgresql 
