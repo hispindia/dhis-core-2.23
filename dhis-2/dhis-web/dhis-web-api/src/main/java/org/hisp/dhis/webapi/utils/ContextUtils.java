1b82a58d13c714ab4265e75282d12ba98f2b18dd
Lars Helge Overland <larshelge@gmail.com>
2014-07-08 21:02:06 +0200

    revno: 16030 message:   Web API, impl support for including custom javascripts and css styles into web app. Useful for custom forms and HTML reports where you want to centralize scripts/styles and keep them out of the forms/reports themselves.
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Tue 2014-07-08 21:02:06 +0200

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/utils/ContextUtils.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/utils/ContextUtils.java
@@ -68,6 +68,7 @@ public class ContextUtils
     public static final String CONTENT_TYPE_JSON = "application/json; charset=UTF-8";
     public static final String CONTENT_TYPE_HTML = "text/html; charset=UTF-8";
     public static final String CONTENT_TYPE_TEXT = "text/plain; charset=UTF-8";
+    public static final String CONTENT_TYPE_CSS = "text/css; charset=UTF-8";
     public static final String CONTENT_TYPE_XML = "application/xml; charset=UTF-8";
     public static final String CONTENT_TYPE_CSV = "application/csv; charset=UTF-8";
     public static final String CONTENT_TYPE_PNG = "image/png";
