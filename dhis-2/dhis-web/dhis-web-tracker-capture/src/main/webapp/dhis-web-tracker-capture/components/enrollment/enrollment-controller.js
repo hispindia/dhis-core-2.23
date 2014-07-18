c4f6e388867d245553638cc573230249bdd9a0c4
Abyot Asalefew Gizaw <abyota@gmail.com>
2014-07-18 20:24:39 +0200

    revno: 16187 message:   tracker capture - removed unused code
    committer: Abyot Asalefew Gizaw <abyota@gmail.com>
    branch nick: dhis2
    timestamp: Fri 2014-07-18 20:24:39 +0200

--- dhis-2/dhis-web/dhis-web-tracker-capture/src/main/webapp/dhis-web-tracker-capture/components/enrollment/enrollment-controller.js
+++ dhis-2/dhis-web/dhis-web-tracker-capture/src/main/webapp/dhis-web-tracker-capture/components/enrollment/enrollment-controller.js
@@ -194,6 +194,7 @@ trackerCapture.controller('EnrollmentController',
                     $scope.autoGenerateEvents();                    
                     $scope.broadCastSelections('dashboardWidgets'); 
                     
+                    $scope.showEnrollmentDiv = false;
                     $scope.outerForm.submitted = false;      
                 });
             }
@@ -268,29 +269,7 @@ trackerCapture.controller('EnrollmentController',
         //$scope.showEnrollmentHistoryDiv = !$scope.showEnrollmentHistoryDiv;
         console.log('need to figure out how to deal with previous enrollments'); 
     };
-    
-    $scope.getEventsForRescheduling = function(){
         
-        DHIS2EventFactory.getEventsByStatus($scope.selectedTei.trackedEntityInstance, $scope.selectedOrgUnit.id, $scope.selectedProgram.id, 'ACTIVE', 'SCHEDULE').then(function(data){
-            if(angular.isObject(data)){
-                angular.forEach(data, function(ev){                    
-                    if(ev.enrollment === $scope.selectedEnrollment.enrollment){                        
-                        ev.name = $scope.selectedProgramWithStage[ev.programStage].name;                        
-                        ev.dueDate = DateUtils.format(ev.dueDate);                        
-                        ev.statusColor = EventUtils.getEventStatusColor(ev);                          
-                        $scope.eventsForRescheduling.push(ev);
-                        ev = EventUtils.setEventOrgUnitName(ev);
-                    }                                                                                 
-                });
-            }  
-            
-            if($scope.eventsForRescheduling.length > 0 ){
-                $scope.eventsForRescheduling = orderByFilter($scope.eventsForRescheduling, '-eventDate');
-                $scope.eventsForRescheduling.reverse();
-            }
-        });        
-    };
-    
     $scope.autoGenerateEvents = function(){
         if($scope.selectedTei && $scope.selectedProgram && $scope.selectedOrgUnit && $scope.selectedEnrollment){            
             $scope.dhis2Events = {events: []};
