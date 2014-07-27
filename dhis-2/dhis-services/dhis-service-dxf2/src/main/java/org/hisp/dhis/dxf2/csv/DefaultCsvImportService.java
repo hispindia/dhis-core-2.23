53ff6ada32bb6fdf21ecf78fb8d21692db004143
Lars Helge Overland <larshelge@gmail.com>
2014-07-27 14:20:33 +0200

    revno: 16252 message:   CSV import, validation rules, including null if blank for expressions
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Sun 2014-07-27 14:20:33 +0200

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/csv/DefaultCsvImportService.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/csv/DefaultCsvImportService.java
@@ -258,12 +258,14 @@ public class DefaultCsvImportService
                 
                 leftSide.setExpression( getSafe( values, 9, null, 255 ) );
                 leftSide.setDescription( getSafe( values, 10, null, 255 ) );
+                leftSide.setNullIfBlank( Boolean.valueOf( getSafe( values, 11, Boolean.FALSE.toString(), 5 ) ) );
                 leftSide.setDataElementsInExpression( expressionService.getDataElementsInExpression( leftSide.getExpression() ) );
                 leftSide.setOptionCombosInExpression( expressionService.getOptionCombosInExpression( leftSide.getExpression() ) );
                 leftSide.setNullIfBlank( true );
                 
-                rightSide.setExpression( getSafe( values, 11, null, 255 ) );
-                rightSide.setDescription( getSafe( values, 12, null, 255 ) );
+                rightSide.setExpression( getSafe( values, 12, null, 255 ) );
+                rightSide.setDescription( getSafe( values, 13, null, 255 ) );
+                rightSide.setNullIfBlank( Boolean.valueOf( getSafe( values, 14, Boolean.FALSE.toString(), 5 ) ) );
                 rightSide.setDataElementsInExpression( expressionService.getDataElementsInExpression( rightSide.getExpression() ) );
                 rightSide.setOptionCombosInExpression( expressionService.getOptionCombosInExpression( rightSide.getExpression() ) );
                 rightSide.setNullIfBlank( true );
