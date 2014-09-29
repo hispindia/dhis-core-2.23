ad22724936ec495088f94b81e7230aca1f8aff16
Lars Helge Overland <larshelge@gmail.com>
2014-09-29 21:07:43 +0200

    revno: 16865 message:   Tracker table alteror. Removed statement that takes forever to complete on large databases. We must distribute this as script.
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Mon 2014-09-29 21:07:43 +0200

--- dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/trackedentity/startup/TableAlteror.java
+++ dhis-2/dhis-services/dhis-service-tracker/src/main/java/org/hisp/dhis/trackedentity/startup/TableAlteror.java
@@ -257,8 +257,6 @@ public class TableAlteror
             + "where trackedentityattribute.mandatory is not null" );
         executeSql( "ALTER TABLE trackedentityattribute DROP COLUMN mandatory" );
 
-        executeSql( "update datavalue set storedby='aggregated_from_tracker' where storedby='DHIS-System'" );
-
         executeSql( "ALTER TABLE trackedentityattribute DROP COLUMN groupBy" );
 
         executeSql( "update trackedentityattribute set valuetype='string' where valuetype='combo' and optionsetid is null" );
