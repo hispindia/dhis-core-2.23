e358008df25bf358f33e83d6d94a72211553bbc2
Lars Helge Overland <larshelge@gmail.com>
2014-10-04 12:20:19 +0200

    revno: 16955 message:   Data integrity checks, line breaks
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Sat 2014-10-04 12:20:19 +0200

--- dhis-2/dhis-web/dhis-web-maintenance/dhis-web-maintenance-dataadmin/src/main/webapp/dhis-web-maintenance-dataadmin/javascript/dataIntegrity.js
+++ dhis-2/dhis-web/dhis-web-maintenance/dhis-web-maintenance-dataadmin/src/main/webapp/dhis-web-maintenance-dataadmin/javascript/dataIntegrity.js
@@ -10,30 +10,30 @@ function populateIntegrityItems( json )
 {
 	hideLoader();
 	
-    displayViolationList( json.dataElementsWithoutDataSet, "dataElementsWithoutDataSet" );
-    displayViolationList( json.dataElementsWithoutGroups, "dataElementsWithoutGroups" );
-	displayViolationList( json.dataElementsViolatingExclusiveGroupSets, "dataElementsViolatingExclusiveGroupSets" );
-	displayViolationList( json.dataElementsInDataSetNotInForm, "dataElementsInDataSetNotInForm" );
-    displayViolationList( json.dataElementsAssignedToDataSetsWithDifferentPeriodTypes, "dataElementsAssignedToDataSetsWithDifferentPeriodTypes" );
-    displayViolationList( json.dataSetsNotAssignedToOrganisationUnits, "dataSetsNotAssignedToOrganisationUnits" );
-    displayViolationList( json.sectionsWithInvalidCategoryCombinations, "sectionsWithInvalidCategoryCombinations" );
-    displayViolationList( json.indicatorsWithIdenticalFormulas, "indicatorsWithIdenticalFormulas" );
-    displayViolationList( json.indicatorsWithoutGroups, "indicatorsWithoutGroups" );
-    displayViolationList( json.invalidIndicatorNumerators, "invalidIndicatorNumerators" );
-    displayViolationList( json.invalidIndicatorDenominators, "invalidIndicatorDenominators" );
-	displayViolationList( json.indicatorsViolatingExclusiveGroupSets, "indicatorsViolatingExclusiveGroupSets" );
-    displayViolationList( json.organisationUnitsWithCyclicReferences, "organisationUnitsWithCyclicReferences" );
-    displayViolationList( json.orphanedOrganisationUnits, "orphanedOrganisationUnits" );
-    displayViolationList( json.organisationUnitsWithoutGroups, "organisationUnitsWithoutGroups" );
-    displayViolationList( json.organisationUnitsViolatingExclusiveGroupSets, "organisationUnitsViolatingExclusiveGroupSets" );
-    displayViolationList( json.organisationUnitGroupsWithoutGroupSets, "organisationUnitGroupsWithoutGroupSets" );
-    displayViolationList( json.duplicatePeriods, "duplicatePeriods" );
-    displayViolationList( json.validationRulesWithoutGroups, "validationRulesWithoutGroups" );
-    displayViolationList( json.invalidValidationRuleLeftSideExpressions, "invalidValidationRuleLeftSideExpressions" );
-    displayViolationList( json.invalidValidationRuleRightSideExpressions, "invalidValidationRuleRightSideExpressions" );
+    displayViolationList( json.dataElementsWithoutDataSet, "dataElementsWithoutDataSet", false );
+    displayViolationList( json.dataElementsWithoutGroups, "dataElementsWithoutGroups", false );
+	displayViolationList( json.dataElementsViolatingExclusiveGroupSets, "dataElementsViolatingExclusiveGroupSets", true );
+	displayViolationList( json.dataElementsInDataSetNotInForm, "dataElementsInDataSetNotInForm", true );
+    displayViolationList( json.dataElementsAssignedToDataSetsWithDifferentPeriodTypes, "dataElementsAssignedToDataSetsWithDifferentPeriodTypes", true );
+    displayViolationList( json.dataSetsNotAssignedToOrganisationUnits, "dataSetsNotAssignedToOrganisationUnits", false );
+    displayViolationList( json.sectionsWithInvalidCategoryCombinations, "sectionsWithInvalidCategoryCombinations", false );
+    displayViolationList( json.indicatorsWithIdenticalFormulas, "indicatorsWithIdenticalFormulas", false );
+    displayViolationList( json.indicatorsWithoutGroups, "indicatorsWithoutGroups", false );
+    displayViolationList( json.invalidIndicatorNumerators, "invalidIndicatorNumerators", true );
+    displayViolationList( json.invalidIndicatorDenominators, "invalidIndicatorDenominators", true );
+	displayViolationList( json.indicatorsViolatingExclusiveGroupSets, "indicatorsViolatingExclusiveGroupSets", true );
+    displayViolationList( json.organisationUnitsWithCyclicReferences, "organisationUnitsWithCyclicReferences", false );
+    displayViolationList( json.orphanedOrganisationUnits, "orphanedOrganisationUnits", false );
+    displayViolationList( json.organisationUnitsWithoutGroups, "organisationUnitsWithoutGroups", false );
+    displayViolationList( json.organisationUnitsViolatingExclusiveGroupSets, "organisationUnitsViolatingExclusiveGroupSets", true );
+    displayViolationList( json.organisationUnitGroupsWithoutGroupSets, "organisationUnitGroupsWithoutGroupSets", false );
+    displayViolationList( json.duplicatePeriods, "duplicatePeriods", false );
+    displayViolationList( json.validationRulesWithoutGroups, "validationRulesWithoutGroups", false );
+    displayViolationList( json.invalidValidationRuleLeftSideExpressions, "invalidValidationRuleLeftSideExpressions", true );
+    displayViolationList( json.invalidValidationRuleRightSideExpressions, "invalidValidationRuleRightSideExpressions", true );
 }
 
-function displayViolationList( list, id )
+function displayViolationList( list, id, lineBreak )
 {
     if ( list.length > 0 )
     {
@@ -51,6 +51,7 @@ function displayViolationList( list, id )
         for ( var i = 0; i < list.length; i++ )
         {
             violations += list[i] + "<br>";
+            violations += !!lineBreak ? "<br>" : "";
         }
         
         $( "#" + id + "Div" ).html( violations );
