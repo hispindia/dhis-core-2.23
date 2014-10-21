625d9101b9b9bcea246f6a7b6f5e1c94a1db13b5
Morten Olav Hansen <mortenoh@gmail.com>
2014-10-21 19:10:43 +0700

    revno: 17196 message:   minor indent fix
    committer: Morten Olav Hansen <mortenoh@gmail.com>
    branch nick: dhis2
    timestamp: Tue 2014-10-21 19:10:43 +0700

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/UserSettingController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/UserSettingController.java
@@ -28,6 +28,7 @@ package org.hisp.dhis.webapi.controller;
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import org.hisp.dhis.system.util.LocaleUtils;
 import org.hisp.dhis.user.UserSettingService;
 import org.hisp.dhis.webapi.utils.ContextUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,8 +39,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import javax.servlet.http.HttpServletResponse;
-import org.hisp.dhis.system.util.LocaleUtils;
 import java.io.Serializable;
 import java.util.Locale;
 
@@ -74,11 +75,14 @@ public class UserSettingController
 
         value = value != null ? value : valuePayload;
 
-        if (username == null) {
-            userSettingService.saveUserSetting(key, valueToSet(key, value));
+        if ( username == null )
+        {
+            userSettingService.saveUserSetting( key, valueToSet( key, value ) );
 
-        } else {
-            userSettingService.saveUserSetting(key, valueToSet(key, value), username);
+        }
+        else
+        {
+            userSettingService.saveUserSetting( key, valueToSet( key, value ), username );
         }
 
         ContextUtils.okResponse( response, "User setting saved" );
@@ -87,7 +91,7 @@ public class UserSettingController
     @RequestMapping( value = "/{key}", method = RequestMethod.GET, produces = ContextUtils.CONTENT_TYPE_TEXT )
     public @ResponseBody String getSystemSetting( @PathVariable( "key" ) String key, @RequestParam( value = "user", required = false ) String username )
     {
-        return username == null ? getStringValue(key, userSettingService.getUserSetting( key )) : getStringValue( key,userSettingService.getUserSetting( key, username ));
+        return username == null ? getStringValue( key, userSettingService.getUserSetting( key ) ) : getStringValue( key, userSettingService.getUserSetting( key, username ) );
     }
 
     @RequestMapping( value = "/{key}", method = RequestMethod.DELETE )
@@ -96,19 +100,23 @@ public class UserSettingController
         userSettingService.deleteUserSetting( key );
     }
 
-    private Serializable valueToSet(String key, String value){
-        if (key.equals(UserSettingService.KEY_UI_LOCALE) || key.equals(UserSettingService.KEY_DB_LOCALE)) {
-            return LocaleUtils.getLocale(value);
-        } else {
+    private Serializable valueToSet( String key, String value )
+    {
+        if ( key.equals( UserSettingService.KEY_UI_LOCALE ) || key.equals( UserSettingService.KEY_DB_LOCALE ) )
+        {
+            return LocaleUtils.getLocale( value );
+        }
+        else
+        {
             return value;
         }
     }
 
-    private String getStringValue(String key, Serializable value) {
-        if (key.equals(UserSettingService.KEY_UI_LOCALE) || key.equals(UserSettingService.KEY_DB_LOCALE))
+    private String getStringValue( String key, Serializable value )
+    {
+        if ( key.equals( UserSettingService.KEY_UI_LOCALE ) || key.equals( UserSettingService.KEY_DB_LOCALE ) )
             return ((Locale) value).getLanguage();
         else
             return (String) value;
     }
-
 }
