6919d8e17592240ca39026147d703be59822a3df
Halvdan Hoem Grelland <halvdanhg@gmail.com>
2014-10-21 12:53:08 +0200

    revno: 17194 message:   minor: indentation consistency (use spaces)
    committer: Halvdan Hoem Grelland <halvdanhg@gmail.com>
    branch nick: dhis2
    timestamp: Tue 2014-10-21 12:53:08 +0200

--- dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/main.js
+++ dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/main.js
@@ -7,13 +7,13 @@ $( document ).ready( function() { pageInit(); } );
 
 function pageInit()
 {
-	setTableStyles();
-	
+    setTableStyles();
+
     // Hover on rightbar close image
     
     $( "#hideRightBarImg" ).mouseover( function()
     {
-    	$( this ).attr( "src", "../images/hide_active.png" );
+        $( this ).attr( "src", "../images/hide_active.png" );
     } );
     $( "#hideRightBarImg" ).mouseout( function()
     {
@@ -23,16 +23,16 @@ function pageInit()
 
 function setTableStyles()
 {
-	// Zebra stripes in lists
-	
-	$( "table.listTable tbody tr:odd" ).addClass( "listAlternateRow" );
+    // Zebra stripes in lists
+
+    $( "table.listTable tbody tr:odd" ).addClass( "listAlternateRow" );
     $( "table.listTable tbody tr:even" ).addClass( "listRow" );
 
     // Hover rows in lists
-    
+
     $( "table.listTable tbody tr" ).mouseover( function()
     {
-    	$( this ).addClass( "listHoverRow" );
+        $( this ).addClass( "listHoverRow" );
     } );
     $( "table.listTable tbody tr" ).mouseout( function()
     {
@@ -48,22 +48,22 @@ dhis2.util.namespace( 'dhis2.leftBar' );
 
 dhis2.leftBar.setLinks = function( showLeftBarLink, showExtendMenuLink )
 {
-	$( '#showLeftBar' ).css( 'display', ( showLeftBarLink ? 'inline' : 'none' ) );
-	$( '#extendMainMenuLink' ).css( 'display', ( showExtendMenuLink ? 'inline' : 'none' ) );
+    $( '#showLeftBar' ).css( 'display', ( showLeftBarLink ? 'inline' : 'none' ) );
+    $( '#extendMainMenuLink' ).css( 'display', ( showExtendMenuLink ? 'inline' : 'none' ) );
 };
 
 dhis2.leftBar.showAnimated = function()
 {
-	dhis2.leftBar.setMenuVisible();
-	dhis2.leftBar.setLinks( false, true );
-	$( '#leftBar, #orgUnitTree' ).css( 'width', '' ).show( 'slide', { direction: 'left', duration: 200 } );
-	$( '#mainPage' ).css( 'margin-left', '' );
+    dhis2.leftBar.setMenuVisible();
+    dhis2.leftBar.setLinks( false, true );
+    $( '#leftBar, #orgUnitTree' ).css( 'width', '' ).show( 'slide', { direction: 'left', duration: 200 } );
+    $( '#mainPage' ).css( 'margin-left', '' );
 };
 
 dhis2.leftBar.extendAnimated = function()
 {
-	dhis2.leftBar.setMenuExtended();
-	dhis2.leftBar.setLinks( false, false );
+    dhis2.leftBar.setMenuExtended();
+    dhis2.leftBar.setLinks( false, false );
     $( '#leftBar, #orgUnitTree' ).show().animate( { direction: 'left', width: '+=150px', duration: 20 } );
     $( '#mainPage' ).animate( { direction: 'left', marginLeft: '+=150px', duration: 20 } );
     $( '#hideMainMenuLink' ).attr( 'href', 'javascript:dhis2.leftBar.retract()' );
@@ -71,8 +71,8 @@ dhis2.leftBar.extendAnimated = function()
 
 dhis2.leftBar.extend = function()
 {
-	dhis2.leftBar.setMenuExtended();
-	dhis2.leftBar.setLinks( false, false );
+    dhis2.leftBar.setMenuExtended();
+    dhis2.leftBar.setLinks( false, false );
     $( '#leftBar, #orgUnitTree' ).show().css( "width", "+=150px" );
     $( '#mainPage' ).css( "margin-left", "+=150px" );
     $( '#hideMainMenuLink' ).attr( 'href', 'javascript:dhis2.leftBar.retract()' );
@@ -80,8 +80,8 @@ dhis2.leftBar.extend = function()
 
 dhis2.leftBar.retract = function()
 {
-	dhis2.leftBar.setMenuVisible();
-	dhis2.leftBar.setLinks( false, true );
+    dhis2.leftBar.setMenuVisible();
+    dhis2.leftBar.setLinks( false, true );
     $( '#leftBar, #orgUnitTree' ).show().animate( { direction: 'right', width: '-=150px', duration: 20 } );
     $( '#mainPage' ).animate( { direction: 'right', marginLeft: '-=150px', duration: 20 } );
     $( '#hideMainMenuLink' ).attr( 'href', 'javascript:javascript:dhis2.leftBar.hideAnimated()' );
@@ -89,16 +89,16 @@ dhis2.leftBar.retract = function()
 
 dhis2.leftBar.hideAnimated = function()
 {
-	dhis2.leftBar.setMenuHidden();
-	dhis2.leftBar.setLinks( true, false );
+    dhis2.leftBar.setMenuHidden();
+    dhis2.leftBar.setLinks( true, false );
     $( '#leftBar' ).hide( 'slide', { direction: 'left', duration: 200 } );
     $( '#mainPage' ).animate( { direction: 'right', marginLeft: '20px', duration: 200 } );
 };
 
 dhis2.leftBar.hide = function()
 {
-	dhis2.leftBar.setMenuHidden();
-	dhis2.leftBar.setLinks( true, false );
+    dhis2.leftBar.setMenuHidden();
+    dhis2.leftBar.setLinks( true, false );
     $( '#leftBar' ).hide();
     $( '#mainPage' ).css( 'margin-left', '20px' );
 };
@@ -121,7 +121,7 @@ dhis2.leftBar.setMenuVisible = function()
     
 dhis2.leftBar.setMenuExtended = function()
 {
-	$.get( '../dhis-web-commons/menu/setMenuState.action?state=EXTENDED' );
+    $.get( '../dhis-web-commons/menu/setMenuState.action?state=EXTENDED' );
 };
     
 dhis2.leftBar.setMenuHidden = function()
@@ -131,5 +131,5 @@ dhis2.leftBar.setMenuHidden = function()
     
 dhis2.leftBar.openHelpForm = function( id )
 {
-	window.open( "../dhis-web-commons/help/viewDynamicHelp.action?id=" + id, "Help", "width=800,height=600,scrollbars=yes" );
+    window.open( "../dhis-web-commons/help/viewDynamicHelp.action?id=" + id, "Help", "width=800,height=600,scrollbars=yes" );
 };
