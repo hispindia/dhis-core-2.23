f23dfbcf8670f368ef9fa11924c59bf28d06a7e2
Morten Olav Hansen <mortenoh@gmail.com>
2014-11-29 17:12:58 +0700

    revno: 17615 message:   minor paging fix
    committer: Morten Olav Hansen <mortenoh@gmail.com>
    branch nick: dhis2
    timestamp: Sat 2014-11-29 17:12:58 +0700

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/AbstractCrudController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/AbstractCrudController.java
@@ -209,16 +209,12 @@ public abstract class AbstractCrudController<T extends IdentifiableObject>
 
         Pager pager = metaData.getPager();
 
-        // enable object filter
-        if ( !filters.isEmpty() )
-        {
-            entityList = objectFilterService.filter( entityList, filters );
+        entityList = objectFilterService.filter( entityList, filters );
 
-            if ( hasPaging )
-            {
-                pager = new Pager( options.getPage(), entityList.size(), options.getPageSize() );
-                entityList = PagerUtils.pageCollection( entityList, pager );
-            }
+        if ( hasPaging )
+        {
+            pager = new Pager( options.getPage(), entityList.size(), options.getPageSize() );
+            entityList = PagerUtils.pageCollection( entityList, pager );
         }
 
         postProcessEntities( entityList );
