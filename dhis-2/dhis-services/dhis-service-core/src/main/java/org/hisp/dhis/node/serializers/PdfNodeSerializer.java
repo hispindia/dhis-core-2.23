724895529628c8ff68231da0ebc3c39e1baa2720
Morten Olav Hansen <mortenoh@gmail.com>
2014-12-16 19:07:41 +0100

    revno: 17719 message:   support pdf in web-api GET requests
    committer: Morten Olav Hansen <mortenoh@gmail.com>
    branch nick: dhis2
    timestamp: Tue 2014-12-16 19:07:41 +0100

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/node/serializers/PdfNodeSerializer.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/node/serializers/PdfNodeSerializer.java
@@ -29,11 +29,15 @@ package org.hisp.dhis.node.serializers;
  */
 
 import com.google.common.collect.Lists;
+import com.lowagie.text.Document;
+import com.lowagie.text.pdf.PdfPTable;
 import org.hisp.dhis.node.AbstractNodeSerializer;
+import org.hisp.dhis.node.Node;
 import org.hisp.dhis.node.types.CollectionNode;
 import org.hisp.dhis.node.types.ComplexNode;
 import org.hisp.dhis.node.types.RootNode;
 import org.hisp.dhis.node.types.SimpleNode;
+import org.hisp.dhis.system.util.PDFUtils;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
@@ -53,6 +57,8 @@ public class PdfNodeSerializer extends AbstractNodeSerializer
 {
     public static final String[] CONTENT_TYPES = { "application/pdf" };
 
+    private Document document;
+
     @Override
     public List<String> contentTypes()
     {
@@ -62,8 +68,16 @@ public class PdfNodeSerializer extends AbstractNodeSerializer
     @Override
     protected void startSerialize( RootNode rootNode, OutputStream outputStream ) throws Exception
     {
-        outputStream.write( "Hello PDF".getBytes() );
-        outputStream.flush();
+        document = PDFUtils.openDocument( outputStream );
+    }
+
+    @Override
+    protected void endSerialize( RootNode rootNode, OutputStream outputStream ) throws Exception
+    {
+        if ( document.isOpen() )
+        {
+            document.close();
+        }
     }
 
     @Override
@@ -75,7 +89,54 @@ public class PdfNodeSerializer extends AbstractNodeSerializer
     @Override
     protected void startWriteRootNode( RootNode rootNode ) throws Exception
     {
+        for ( Node child : rootNode.getChildren() )
+        {
+            if ( child.isCollection() )
+            {
+                PdfPTable table = PDFUtils.getPdfPTable( true, 0.25f, 0.75f );
+                boolean haveContent = false;
 
+                for ( Node node : child.getChildren() )
+                {
+                    for ( Node property : node.getChildren() )
+                    {
+                        if ( property.isSimple() )
+                        {
+                            table.addCell( PDFUtils.getItalicCell( property.getName() ) );
+                            table.addCell( PDFUtils.getTextCell( getValue( (SimpleNode) property ) ) );
+                            haveContent = true;
+                        }
+                    }
+
+                    if ( haveContent )
+                    {
+                        table.addCell( PDFUtils.getEmptyCell( 2, 15 ) );
+                        haveContent = false;
+                    }
+                }
+
+                document.add( table );
+            }
+        }
+    }
+
+    public String getValue( SimpleNode simpleNode )
+    {
+        if ( simpleNode.getValue() == null )
+        {
+            return "";
+        }
+
+        String value = String.format( "%s", simpleNode.getValue() );
+
+        if ( Date.class.isAssignableFrom( simpleNode.getValue().getClass() ) )
+        {
+            SimpleDateFormat dateFormat = new SimpleDateFormat( "yyyy-MM-dd'T'HH:mm:ss.SSSZ" );
+            dateFormat.setTimeZone( TimeZone.getTimeZone( "UTC" ) );
+            value = dateFormat.format( (Date) simpleNode.getValue() );
+        }
+
+        return value;
     }
 
     @Override
@@ -87,14 +148,7 @@ public class PdfNodeSerializer extends AbstractNodeSerializer
     @Override
     protected void startWriteSimpleNode( SimpleNode simpleNode ) throws Exception
     {
-        String value = String.format( "%s", simpleNode.getValue() );
 
-        if ( Date.class.isAssignableFrom( simpleNode.getValue().getClass() ) )
-        {
-            SimpleDateFormat dateFormat = new SimpleDateFormat( "yyyy-MM-dd'T'HH:mm:ss.SSSZ" );
-            dateFormat.setTimeZone( TimeZone.getTimeZone( "UTC" ) );
-            value = dateFormat.format( (Date) simpleNode.getValue() );
-        }
     }
 
     @Override
@@ -123,6 +177,12 @@ public class PdfNodeSerializer extends AbstractNodeSerializer
 
     @Override
     protected void endWriteCollectionNode( CollectionNode collectionNode ) throws Exception
+    {
+
+    }
+
+    @Override
+    protected void dispatcher( Node node ) throws Exception
     {
 
     }
