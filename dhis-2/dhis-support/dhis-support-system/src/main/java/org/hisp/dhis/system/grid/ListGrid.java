38f1dd4e6af6f52e10fbd2c91085019f6ed5338b
Jason P. Pickering <jason.p.pickering@gmail.com>
2015-04-01 10:37:35 +0200

    revno: 18791 message:   (Translations) Adding Tetum validation rule and import export module.
    committer: Jason P. Pickering <jason.p.pickering@gmail.com>
    branch nick: dhis2
    timestamp: Wed 2015-04-01 10:37:35 +0200

--- dhis-2/dhis-support/dhis-support-system/src/main/java/org/hisp/dhis/system/grid/ListGrid.java
+++ dhis-2/dhis-support/dhis-support-system/src/main/java/org/hisp/dhis/system/grid/ListGrid.java
@@ -331,7 +331,7 @@ public class ListGrid
     }
     
     @Override
-    public Grid addEmptyValues( int number )
+    public Grid addEmptyValues( int number )§
     {
         for ( int i = 0; i < number; i++ )
         {
