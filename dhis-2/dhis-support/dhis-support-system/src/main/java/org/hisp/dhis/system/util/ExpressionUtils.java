30606b813a9f3c8a3366e5ac79e09b7b706e5784
Lars Helge Overland <larshelge@gmail.com>
2015-04-29 13:47:08 +0200

    revno: 19057 message:   Minor
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Wed 2015-04-29 13:47:08 +0200

--- dhis-2/dhis-support/dhis-support-system/src/main/java/org/hisp/dhis/system/util/ExpressionUtils.java
+++ dhis-2/dhis-support/dhis-support-system/src/main/java/org/hisp/dhis/system/util/ExpressionUtils.java
@@ -60,7 +60,7 @@ public class ExpressionUtils
     {
         Expression exp = JEXL.createExpression( expression );
         
-        JexlContext context = vars != null ? new MapContext( vars ) : new MapContext( vars );
+        JexlContext context = vars != null ? new MapContext( vars ) : new MapContext();
                 
         return exp.evaluate( context );
     }
