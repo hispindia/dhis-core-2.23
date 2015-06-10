a470458865f5e5581fb585f2906834df9412fa6f
Abyot Asalefew Gizaw <abyota@gmail.com>
2015-06-10 13:57:37 +0200

    revno: 19340 message:   browser-cache-cleaner: upgrade to latest translate plugin
    committer: Abyot Asalefew Gizaw <abyota@gmail.com>
    branch nick: dhis2
    timestamp: Wed 2015-06-10 13:57:37 +0200

--- dhis-2/dhis-web/dhis-web-apps/src/main/webapp/dhis-web-cache-cleaner/scripts/app.js
+++ dhis-2/dhis-web/dhis-web-apps/src/main/webapp/dhis-web-cache-cleaner/scripts/app.js
@@ -3,22 +3,24 @@
 /* App Module */
 
 var cacheCleaner = angular.module('cacheCleaner',
-		 ['ui.bootstrap', 
-		  'ngRoute', 
-		  'ngCookies', 
-		  'cacheCleanerDirectives', 
-		  'cacheCleanerControllers', 
-		  'cacheCleanerServices',
-          'cacheCleanerFilters',
-          'd2Services',
-          'd2Controllers',
-		  'angularLocalStorage', 
-		  'pascalprecht.translate',
-          'd2HeaderBar'])
+                    ['ui.bootstrap', 
+                     'ngRoute', 
+                     'ngCookies', 
+                     'ngSanitize',
+                     'cacheCleanerDirectives', 
+                     'cacheCleanerControllers', 
+                     'cacheCleanerServices',
+                     'cacheCleanerFilters',
+                     'd2Services',
+                     'd2Controllers',
+                     'angularLocalStorage', 
+                     'pascalprecht.translate',
+                     'd2HeaderBar'])
               
 .value('DHIS2URL', '..')
 
 .config(function($translateProvider) {   
     $translateProvider.preferredLanguage('en');
+    $translateProvider.useSanitizeValueStrategy('escaped');
     $translateProvider.useLoader('i18nLoader');
 });
