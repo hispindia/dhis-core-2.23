32b03ed71733803b25b38420170c20e1ff396153
Lars Helge Overland <larshelge@gmail.com>
2015-08-19 12:21:12 -0400

    revno: 19796 message:   Dashboard filter, clear function, fixed bug
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Wed 2015-08-19 12:21:12 -0400

--- dhis-2/dhis-web/dhis-web-dashboard-integration/src/main/webapp/dhis-web-dashboard-integration/javascript/dashboard.js
+++ dhis-2/dhis-web/dhis-web-dashboard-integration/src/main/webapp/dhis-web-dashboard-integration/javascript/dashboard.js
@@ -1246,7 +1246,7 @@ dhis2.db.viewOrgUnitSelector = function()
 dhis2.db.updateSelectedOrgUnits = function()
 {
 	var ous = selectionTreeSelection.getSelectedUid();
-	dhis2.db.currentUserOrgUnit = ous;	
+	dhis2.db.currentUserOrgUnit = ous;
 	dhis2.db.renderDashboard( dhis2.db.current() );
 	$( "#orgUnitSelectorForm" ).dialog( "destroy" );
 }
@@ -1255,5 +1255,6 @@ dhis2.db.clearSelectedOrgUnits = function()
 {
 	dhis2.db.currentUserOrgUnit = [];
 	selectionTree.clearSelectedOrganisationUnitsAndBuildTree();
-	dhis2.db.updateSelectedOrgUnits();
+	dhis2.db.renderDashboard( dhis2.db.current() );
+	$( "#orgUnitSelectorForm" ).dialog( "destroy" );
 }
