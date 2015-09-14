0434c0640ac3a1721bc5706b34bdeee1b47a660d
Morten Olav Hansen <mortenoh@gmail.com>
2015-09-14 14:26:55 +0700

    revno: 20119 message:   more j8 streams in ModuleManager
    committer: Morten Olav Hansen <mortenoh@gmail.com>
    branch nick: dhis2
    timestamp: Mon 2015-09-14 14:26:55 +0700

--- dhis-2/dhis-web/dhis-web-commons/src/main/java/org/hisp/dhis/webportal/module/DefaultModuleManager.java
+++ dhis-2/dhis-web/dhis-web-commons/src/main/java/org/hisp/dhis/webportal/module/DefaultModuleManager.java
@@ -48,6 +48,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author Torgeir Lorange Ostby
@@ -151,10 +152,7 @@ public class DefaultModuleManager
         List<Module> modules = getAccessibleMenuModules();
         List<App> apps = appManager.getAccessibleApps();
 
-        for ( App app : apps )
-        {
-            modules.add( Module.getModule( app ) );
-        }
+        modules.addAll( apps.stream().map( Module::getModule ).collect( Collectors.toList() ) );
 
         return modules;
     }
@@ -264,15 +262,9 @@ public class DefaultModuleManager
 
     private List<Module> getAccessibleModules( List<Module> modules )
     {
-        List<Module> allowed = new ArrayList<>();
-
-        for ( Module module : modules )
-        {
-            if ( module != null && actionAccessResolver.hasAccess( module.getName(), defaultActionName ) )
-            {
-                allowed.add( module );
-            }
-        }
+        List<Module> allowed = modules.stream()
+            .filter( module -> module != null && actionAccessResolver.hasAccess( module.getName(), defaultActionName ) )
+            .collect( Collectors.toList() );
 
         if ( modules.size() > allowed.size() )
         {
