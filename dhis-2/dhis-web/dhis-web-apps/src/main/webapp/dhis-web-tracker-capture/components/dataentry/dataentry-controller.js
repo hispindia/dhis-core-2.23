4644c7826acee93c07534199619d51b1d134f146
Abyot Asalefew Gizaw <abyota@gmail.com>
2015-09-15 19:16:30 +0200

    revno: 20170 message:   minor
    committer: Abyot Asalefew Gizaw <abyota@gmail.com>
    branch nick: dhis2
    timestamp: Tue 2015-09-15 19:16:30 +0200

--- dhis-2/dhis-web/dhis-web-apps/src/main/webapp/dhis-web-tracker-capture/components/dataentry/dataentry-controller.js
+++ dhis-2/dhis-web/dhis-web-apps/src/main/webapp/dhis-web-tracker-capture/components/dataentry/dataentry-controller.js
@@ -7,6 +7,7 @@ trackerCapture.controller('DataEntryController',
                 $filter,
                 $log,
                 $timeout,
+                $translate,
                 Paginator,
                 DateUtils,
                 EventUtils,
@@ -39,6 +40,11 @@ trackerCapture.controller('DataEntryController',
     $scope.errorMessages = {};
     $scope.warningMessages = {};
     $scope.tableMaxNumberOfDataElements = 10;
+    
+    //Labels
+    $scope.dataElementLabel = $translate.instant('data_element');
+    $scope.valueLabel = $translate.instant('value');
+    $scope.providedElsewhereLabel = $translate.instant('provided_elsewhere');
     
 
     var userProfile = SessionStorageService.get('USER_PROFILE');
