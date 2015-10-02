27ce6ac13a0e4a086b3fa7bcb5f2b63c9cab7b4d
Morten Olav Hansen <mortenoh@gmail.com>
2015-10-02 10:59:43 +0700

    revno: 20457 message:   minor csv mapper fix
    committer: Morten Olav Hansen <mortenoh@gmail.com>
    branch nick: dhis2
    timestamp: Fri 2015-10-02 10:59:43 +0700

--- dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/event/csv/DefaultCsvEventService.java
+++ dhis-2/dhis-services/dhis-service-dxf2/src/main/java/org/hisp/dhis/dxf2/events/event/csv/DefaultCsvEventService.java
@@ -52,14 +52,9 @@ import java.util.List;
  */
 public class DefaultCsvEventService implements CsvEventService
 {
-    private static CsvMapper CSV_MAPPER = new CsvMapper();
+    private static CsvMapper CSV_MAPPER = new CsvMapper().enable( CsvParser.Feature.WRAP_AS_ARRAY );
 
     private static CsvSchema CSV_SCHEMA = CSV_MAPPER.schemaFor( CsvEventDataValue.class ).withLineSeparator( "\n" );
-
-    static
-    {
-        CSV_MAPPER.enable( CsvParser.Feature.WRAP_AS_ARRAY );
-    }
 
     @Override
     public void writeEvents( OutputStream outputStream, Events events, boolean withHeader ) throws IOException
