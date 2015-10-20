e05a35df79d3ac0b799c1416d9f89b4baf99cac0
Lars Helge Overland <larshelge@gmail.com>
2015-10-20 13:02:13 +0200

    revno: 20802 message:   Minor
    committer: Lars Helge Overland <larshelge@gmail.com>
    branch nick: dhis2
    timestamp: Tue 2015-10-20 13:02:13 +0200

--- dhis-2/dhis-support/dhis-support-commons/src/main/java/org/hisp/dhis/commons/util/ExpressionUtils.java
+++ dhis-2/dhis-support/dhis-support-commons/src/main/java/org/hisp/dhis/commons/util/ExpressionUtils.java
@@ -51,7 +51,7 @@ public class ExpressionUtils
     
     private static final Map<String, String> EL_SQL_MAP = new HashMap<>();
     private static final String IGNORED_KEYWORDS_REGEX = 
-        "SUM|sum|AVERAGE|average|COUNT|count|STDDEV|stddev|VARIANCE|variance|MIN|min|MAX|max|NONE|none";
+        "SUM|sum|AVG|avg|COUNT|count|STDDEV|stddev|VARIANCE|variance|MIN|min|MAX|max|NONE|none";
 
     private static final Pattern NUMERIC_PATTERN = Pattern.compile( "^(-?0|-?[1-9]\\d*)(\\.\\d+)?$" );
     
