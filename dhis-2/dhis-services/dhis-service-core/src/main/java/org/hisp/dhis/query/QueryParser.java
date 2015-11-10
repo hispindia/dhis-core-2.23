9a38612628a260b8ec776473d4cde8a59a3be076
Morten Olav Hansen <mortenoh@gmail.com>
2015-11-10 09:29:55 +0700

    revno: 21000 message:   minor javadoc update
    committer: Morten Olav Hansen <mortenoh@gmail.com>
    branch nick: dhis2
    timestamp: Tue 2015-11-10 09:29:55 +0700

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/query/QueryParser.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/query/QueryParser.java
@@ -31,9 +31,23 @@ package org.hisp.dhis.query;
 import java.util.List;
 
 /**
+ * TODO add support for Order
+ *
  * @author Morten Olav Hansen <mortenoh@gmail.com>
  */
 public interface QueryParser
 {
+    /**
+     * Parses filter expressions, need 2 or 3 components depending on operator.
+     * i.e. for null you can use "name:null" which checks to see if property name is null
+     * i.e. for eq you can use "name:eq:ANC" which check to see if property name is equal to ANC
+     * <p>
+     * The general syntax is "propertyName:operatorName:<Value to check against if needed>"
+     *
+     * @param klass   Class type to query for
+     * @param filters List of filters to add to Query
+     * @return Query instance based on Schema of klass and filters list
+     * @throws QueryParserException
+     */
     Query parse( Class<?> klass, List<String> filters ) throws QueryParserException;
 }
