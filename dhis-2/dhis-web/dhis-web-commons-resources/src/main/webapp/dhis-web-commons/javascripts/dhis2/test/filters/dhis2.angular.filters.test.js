bd3049b46ac62f9b2de7111487bbd41c52ba7bdb
Jiju K Jose <jijukjose@gmail.com>
2015-11-27 10:26:14 +0100

    revno: 21211 message:   Added tests for trimvariablequalifiers filters
    committer: Jiju K Jose <jijukjose@gmail.com>
    branch nick: dhis2
    timestamp: Fri 2015-11-27 10:26:14 +0100

--- dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/javascripts/dhis2/test/filters/dhis2.angular.filters.test.js
+++ dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/javascripts/dhis2/test/filters/dhis2.angular.filters.test.js
@@ -4,16 +4,43 @@ describe('Filters: d2Filters', () => {
 
     beforeEach(module('d2Filters'));
 
+    /*TODO: paginate filters */
+
     describe('d2Filter: trimquotesFilter', () => {
         let trimquotesFilter;
+
         beforeEach(inject(($injector) => {
             trimquotesFilter = $injector.get('trimquotesFilter');
         }));
 
         it('should trim the initial single and double quotes', function() {
-            var trimmedString = trimquotesFilter('"testString"');
-            expect(trimmedString).to.equal('testString');
-       });
+            expect(trimquotesFilter('"testString"')).to.equal('testString');
+            expect(trimquotesFilter('{{testString::')).to.equal('{{testString::');
+        });
+        it('should return the same input if the input is not a string', function() {
+            expect(trimquotesFilter(25)).to.equal(25);
+        });
+
+    });
+
+    describe('d2Filter: trimvariablequalifiers', () => {
+        let trimvariablequalifiersFilter;
+
+        beforeEach(inject(($injector) => {
+            trimvariablequalifiersFilter = $injector.get('trimvariablequalifiersFilter');
+        }));
+
+        it('should trim the characters matching /^[#VCAvca]{ in the begnnind and /}', function() {
+            let beginningCharsToFilter = ["V{","C{","A{",,"#{","v{",,"c{",,"a{"];
+            beginningCharsToFilter.forEach(function(beginningString){
+                expect(trimvariablequalifiersFilter(beginningString+'testString}')).to.equal('testString');
+                expect(trimvariablequalifiersFilter(beginningString+'testStringTT')).to.equal('testStringTT');
+            });
+        });
+
+        it('should return the same input if the input is not a string', function() {
+            expect(trimvariablequalifiersFilter(25)).to.equal(25);
+        });
 
     });
 });
