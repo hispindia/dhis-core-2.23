b1a27399c19fd9e026248a764fb33350fad05390
Stian Sandvold <stian.sandvold@gmail.com>
2016-01-05 19:34:10 +0100

    revno: 21603 [merge] message:   Removed unused code from previous commit
    committer: Stian Sandvold <stian.sandvold@gmail.com>
    branch nick: dhis2
    timestamp: Tue 2016-01-05 19:34:10 +0100

--- dhis-2/dhis-web/dhis-web-maintenance/dhis-web-maintenance-program/src/main/java/org/hisp/dhis/trackedentity/action/trackedentityattribute/AddAttributeAction.java
+++ dhis-2/dhis-web/dhis-web-maintenance/dhis-web-maintenance-program/src/main/java/org/hisp/dhis/trackedentity/action/trackedentityattribute/AddAttributeAction.java
@@ -81,9 +81,6 @@ public class AddAttributeAction
     @Autowired
     private AttributeService attributeService;
 
-    @Autowired
-    private DhisConfigurationProvider dhisConfigurationProvider;
-
     // -------------------------------------------------------------------------
     // Input/Output
     // -------------------------------------------------------------------------
