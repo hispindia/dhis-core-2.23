e5c9d66675d34f29960c458f0473e8c577d63d4c
Morten Olav Hansen <morten@dhis2.org>
2016-02-27 10:14:52 +0700

    revno: 22055 message:   minor bugfix
    committer: Morten Olav Hansen <morten@dhis2.org>
    branch nick: dhis2
    timestamp: Sat 2016-02-27 10:14:52 +0700

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/SchemaController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/SchemaController.java
@@ -29,18 +29,18 @@ package org.hisp.dhis.webapi.controller;
  */
 
 import com.google.common.collect.Lists;
-import org.hisp.dhis.render.RenderService;
-import org.hisp.dhis.schema.validation.SchemaValidator;
-import org.hisp.dhis.schema.validation.ValidationViolation;
 import org.hisp.dhis.dxf2.webmessage.WebMessage;
 import org.hisp.dhis.fieldfilter.FieldFilterService;
 import org.hisp.dhis.node.NodeUtils;
 import org.hisp.dhis.node.types.CollectionNode;
 import org.hisp.dhis.node.types.RootNode;
+import org.hisp.dhis.render.RenderService;
 import org.hisp.dhis.schema.Property;
 import org.hisp.dhis.schema.Schema;
 import org.hisp.dhis.schema.SchemaService;
 import org.hisp.dhis.schema.Schemas;
+import org.hisp.dhis.schema.validation.SchemaValidator;
+import org.hisp.dhis.schema.validation.ValidationViolation;
 import org.hisp.dhis.webapi.service.ContextService;
 import org.hisp.dhis.webapi.service.LinkService;
 import org.hisp.dhis.webapi.service.WebMessageService;
@@ -96,7 +96,7 @@ public class SchemaController
 
         if ( fields.isEmpty() )
         {
-            fields.add( "*,properties[*,constants[*]]" );
+            fields.add( "*,authorities[*],properties[*,constants[*]]" );
         }
 
         Schemas schemas = new Schemas( schemaService.getSortedSchemas() );
@@ -117,7 +117,7 @@ public class SchemaController
 
         if ( fields.isEmpty() )
         {
-            fields.add( "*,properties[*,constants[*]]" );
+            fields.add( "*,authorities[*],properties[*,constants[*]]" );
         }
 
         Schema schema = getSchemaFromType( type );
