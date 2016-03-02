4aa97adf0eb1859453fbcba0439f3cdb4c3faebc
Abyot Asalefew Gizaw <abyot@dhis2.org>
2016-03-02 15:10:42 +0100

    revno: 22107 message:   tracker-capture: searching of events in data entry widget - complete
    committer: Abyot Asalefew Gizaw <abyot@dhis2.org>
    branch nick: dhis2
    timestamp: Wed 2016-03-02 15:10:42 +0100

--- dhis-2/dhis-web/dhis-web-apps/src/main/webapp/dhis-web-tracker-capture/scripts/filters.js
+++ dhis-2/dhis-web/dhis-web-apps/src/main/webapp/dhis-web-tracker-capture/scripts/filters.js
@@ -2,4 +2,22 @@
 
 /* Filters */
 
-var trackerCaptureFilters = angular.module('trackerCaptureFilters', []);
+var trackerCaptureFilters = angular.module('trackerCaptureFilters', [])
+
+.filter('eventListFilter', function($filter){    
+    
+    return function(pagedList, fullList, filterText){
+
+        if(!pagedList ){
+            return;
+        }
+        
+        if(!filterText){
+            return pagedList;
+        }        
+           
+        var filteredData = fullList;
+        filteredData = $filter('filter')(filteredData, filterText);
+        return filteredData;
+    }; 
+});
