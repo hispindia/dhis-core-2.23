eb5748fe1f9cd6ff3c6eede18bb75c42fcc4f047
Morten Olav Hansen <morten@dhis2.org>
2016-03-13 13:59:22 +0700

    revno: 22268 message:   minor fix, checkfox .isEmpty in postjson enrollment
    committer: Morten Olav Hansen <morten@dhis2.org>
    branch nick: dhis2
    timestamp: Sun 2016-03-13 13:59:22 +0700

--- dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/event/EnrollmentController.java
+++ dhis-2/dhis-web/dhis-web-api/src/main/java/org/hisp/dhis/webapi/controller/event/EnrollmentController.java
@@ -196,7 +196,7 @@ public class EnrollmentController
         ImportSummaries importSummaries = enrollmentService.addEnrollmentsJson( request.getInputStream(), importOptions );
         response.setContentType( MediaType.APPLICATION_JSON_VALUE );
 
-        if ( importSummaries.getImportSummaries().size() > 1 )
+        if ( importSummaries.getImportSummaries().isEmpty() || importSummaries.getImportSummaries().size() > 1 )
         {
             response.setStatus( HttpServletResponse.SC_CREATED );
             renderService.toJson( response.getOutputStream(), importSummaries );
