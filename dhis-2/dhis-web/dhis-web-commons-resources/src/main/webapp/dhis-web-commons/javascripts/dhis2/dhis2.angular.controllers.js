420d78322880b817a9b94e0f30dd16aed13b3ecf
Abyot Asalefew Gizaw <abyot@dhis2.org>
2016-03-16 22:18:01 +0100

    revno: 22366 message:   enhancement to audit history controller and view
    committer: Abyot Asalefew Gizaw <abyot@dhis2.org>
    branch nick: dhis2
    timestamp: Wed 2016-03-16 22:18:01 +0100

--- dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/javascripts/dhis2/dhis2.angular.controllers.js
+++ dhis-2/dhis-web/dhis-web-commons-resources/src/main/webapp/dhis-web-commons/javascripts/dhis2/dhis2.angular.controllers.js
@@ -50,94 +50,55 @@ var d2Controllers = angular.module('d2Controllers', [])
         $scope.location = CurrentSelection.getLocation();
         $modalInstance.close($scope.location);
     };
-        })
+})
 
-////Controller for audit history
-.controller('AuditHistoryController', function ($scope, $modalInstance, $modal, AuditHistoryDataService, DateUtils,
-                                                    eventId, dataType, nameIdMap) {
+//Controller for audit history
+.controller('AuditHistoryController', function ($scope, $modalInstance, $modal, AuditHistoryDataService, DateUtils, eventId, dataType, nameIdMap) {
 
-    $scope.itemList = {};
+    $scope.itemList = [];
 
     $scope.model = {type: dataType};
 
     $scope.close = function () {
         $modalInstance.close();
     };
-
-    $scope.$watch("itemList", function (itemList) {
-        $scope.listInDisplay = itemList;
-    });
-
-    $scope.searchTheList = function () {
-        var searchStr = $scope.model.searchText;
-        var filteredItemList = {};
-        for (var item in $scope.itemList) {
-            for (var index = 0; index < $scope.itemList[item].length; index++) {
-                var element = $scope.itemList[item][index];
-                if (!isSubString(item, searchStr) && !isSubString(element.created, searchStr) && !isSubString(element.value, searchStr) && !isSubString(element.modifiedBy, searchStr)) {
-                } else {
-                    if (!filteredItemList[item]) {
-                        filteredItemList[item] = [];
-                    }
-                    filteredItemList[item].push(element);
-                }
-            }
-        }
-        $scope.listInDisplay = filteredItemList;
-    }
-    $scope.isListEmpty = function() {
-        return Object.keys($scope.listInDisplay).length === 0;
-    }
-
-    function isSubString(str, subStr) {
-        return str.toLowerCase().indexOf(subStr.toLowerCase()) > -1;
-    }
-
+    
+    $scope.auditColumns = ['name', 'auditType', 'value', 'modifiedBy', 'created'];    
 
     AuditHistoryDataService.getAuditHistoryData(eventId, dataType).then(function (data) {
 
-        $scope.itemList = {};
+        $scope.itemList = [];
 
         var reponseData = data.trackedEntityDataValueAudits ? data.trackedEntityDataValueAudits :
             data.trackedEntityAttributeValueAudits ? data.trackedEntityAttributeValueAudits : null;
+
         if (reponseData) {
             for (var index = 0; index < reponseData.length; index++) {
-
                 var dataValue = reponseData[index];
-
                 /*The true/false values are displayed as Yes/No*/
                 if (dataValue.value === "true") {
                     dataValue.value = "Yes";
                 } else if (dataValue.value === "false") {
                     dataValue.value = "No";
                 }
+                
+                var obj = {};                
+                obj.auditType = dataValue.auditType;                
+                obj.value = dataValue.value;
+                obj.modifiedBy = dataValue.modifiedBy;
+                obj.created = DateUtils.formatToHrsMinsSecs(dataValue.created);
+                
                 if (dataType === "attribute") {
-                    if (nameIdMap[dataValue.trackedEntityAttribute.id] && nameIdMap[dataValue.trackedEntityAttribute.id].displayName) {
-                        var attributeName = nameIdMap[dataValue.trackedEntityAttribute.id].displayName;
-                        if (!$scope.itemList[attributeName]) {
-                            $scope.itemList[attributeName] = [];
-                        }
-                        $scope.itemList[attributeName].push({
-                            created: DateUtils.formatToHrsMinsSecs(dataValue.created), value: dataValue.value,
-                            auditType: dataValue.auditType, modifiedBy: dataValue.modifiedBy
-                        });
+                    if (nameIdMap[dataValue.trackedEntityAttribute.id] && nameIdMap[dataValue.trackedEntityAttribute.id].displayName) {                        
+                        obj.name = nameIdMap[dataValue.trackedEntityAttribute.id].displayName;
                     }
                 } else if (dataType === "dataElement") {
-                    if (nameIdMap[dataValue.dataElement.id] && nameIdMap[dataValue.dataElement.id].dataElement) {
-                        var dataElementName = nameIdMap[dataValue.dataElement.id].dataElement.displayName;
-                        if (!$scope.itemList[dataElementName]) {
-                            $scope.itemList[dataElementName] = [];
-                        }
-                        $scope.itemList[dataElementName].push({
-                            created: DateUtils.formatToHrsMinsSecs(dataValue.created), value: dataValue.value,
-                            auditType: dataValue.auditType, modifiedBy: dataValue.modifiedBy
-                        });
+                    if (nameIdMap[dataValue.dataElement.id] && nameIdMap[dataValue.dataElement.id].dataElement) {                        
+                        obj.name = nameIdMap[dataValue.dataElement.id].dataElement.displayName;
                     }
-                }
+                }                
+                $scope.itemList.push(obj);
             }
         }
     });
 });
-
-
-
