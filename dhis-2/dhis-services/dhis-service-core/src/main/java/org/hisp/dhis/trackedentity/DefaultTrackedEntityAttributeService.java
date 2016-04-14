ff11049aae9e6324eb9df3a46750618acf81ca3b
Morten Olav Hansen <morten@dhis2.org>
2016-04-14 13:58:08 +0700

    revno: 22733 message:   minor fix, validate TEA date/datetime
    committer: Morten Olav Hansen <morten@dhis2.org>
    branch nick: dhis2
    timestamp: Thu 2016-04-14 13:58:08 +0700

--- dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/trackedentity/DefaultTrackedEntityAttributeService.java
+++ dhis-2/dhis-services/dhis-service-core/src/main/java/org/hisp/dhis/trackedentity/DefaultTrackedEntityAttributeService.java
@@ -28,6 +28,7 @@ package org.hisp.dhis.trackedentity;
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import org.apache.commons.lang3.StringUtils;
 import org.hisp.dhis.common.Grid;
 import org.hisp.dhis.common.OrganisationUnitSelectionMode;
 import org.hisp.dhis.common.QueryItem;
@@ -48,8 +49,6 @@ import org.springframework.util.Assert;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import org.apache.commons.lang3.StringUtils;
 
 /**
  * @author Abyot Asalefew
@@ -258,9 +257,9 @@ public class DefaultTrackedEntityAttributeService
         }
 
         // TODO re-factor to avoid circular dependency
-        
+
         TrackedEntityInstanceService trackedEntityInstanceService = (TrackedEntityInstanceService) applicationContext.getBean( TrackedEntityInstanceService.class );
-        
+
         Grid instances = trackedEntityInstanceService.getTrackedEntityInstancesGrid( params );
 
         if ( !(instances.getHeight() == 0) )
@@ -310,9 +309,17 @@ public class DefaultTrackedEntityAttributeService
                 return "Value '" + errorValue + "' is not a valid username for attribute " + trackedEntityAttribute.getUid();
             }
         }
+        else if ( ValueType.DATE == valueType && !DateUtils.dateIsValid( value ) )
+        {
+            return "Value '" + errorValue + "' is not a valid date for attribute " + trackedEntityAttribute.getUid();
+        }
+        else if ( ValueType.DATETIME == valueType && !DateUtils.dateTimeIsValid( value ) )
+        {
+            return "Value '" + errorValue + "' is not a valid datetime for attribute " + trackedEntityAttribute.getUid();
+        }
         else if ( trackedEntityAttribute.hasOptionSet() && !trackedEntityAttribute.isValidOptionValue( value ) )
         {
-            return "Value '" + errorValue + "' is not a valid option for attribute " + 
+            return "Value '" + errorValue + "' is not a valid option for attribute " +
                 trackedEntityAttribute.getUid() + " and option set " + trackedEntityAttribute.getOptionSet().getUid();
         }
 
